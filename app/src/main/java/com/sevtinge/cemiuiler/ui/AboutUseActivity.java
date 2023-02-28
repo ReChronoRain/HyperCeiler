bd347b1bed63eb5212538e9ec4b1d71342092b6d
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2023-02-28 21:11:34 +0800

    新增 关于-引用

---
+++ app/src/main/java/com/sevtinge/cemiuiler/ui/AboutUseActivity.java
@@ -0,0 +1,24 @@
+package com.sevtinge.cemiuiler.ui;
+
+import androidx.fragment.app.Fragment;
+import com.sevtinge.cemiuiler.R;
+import com.sevtinge.cemiuiler.ui.base.BaseAppCompatActivity;
+import com.sevtinge.cemiuiler.ui.base.SubFragment;
+
+public class AboutUseActivity extends BaseAppCompatActivity {
+
+    @Override
+    public Fragment initFragment() {
+        return new com.sevtinge.cemiuiler.ui.AboutUseActivity.AboutUseFragment();
+    }
+
+    public static class AboutUseFragment extends SubFragment {
+
+        @Override
+        public int getContentResId() {
+            return R.xml.prefs_about_use;
+        }
+    }
+
+
+}
