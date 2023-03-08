ff306f87548f3a5598bfab070f2e03ceb8126aa3
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2023-03-08 13:25:46 +0800

    新增 设置-极致模式

--- app/src/main/java/com/sevtinge/cemiuiler/module/SystemSettings.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/SystemSettings.java
@@ -3,6 +3,7 @@ package com.sevtinge.cemiuiler.module;
 import com.sevtinge.cemiuiler.module.base.BaseModule;
 import com.sevtinge.cemiuiler.module.mirror.UnlockMiuiPlus;
 import com.sevtinge.cemiuiler.module.systemsettings.AddMiuiPlusEntry;
+import com.sevtinge.cemiuiler.module.systemsettings.EnableSpeedMode;
 import com.sevtinge.cemiuiler.module.systemsettings.VoipAssistantController;
 
 public class SystemSettings extends BaseModule {
@@ -11,6 +12,7 @@ public class SystemSettings extends BaseModule {
         public void handleLoadPackage() {
             initHook(new VoipAssistantController(), mPrefsMap.getBoolean("system_settings_voip_assistant_controller"));
             initHook(new AddMiuiPlusEntry(), mPrefsMap.getBoolean("mirror_unlock_miui_plus"));
+            initHook(new EnableSpeedMode(), mPrefsMap.getBoolean("system_settings_develop_speed_mode"));
         }
     }
 
