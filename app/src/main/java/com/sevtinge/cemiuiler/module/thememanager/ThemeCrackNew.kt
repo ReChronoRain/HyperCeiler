1592e94a5f142c1d475fb2c12e9b4151c857874c
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-03-09 19:39:30 +0800

    大图标

--- app/src/main/java/com/sevtinge/cemiuiler/module/thememanager/ThemeCrackNew.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/thememanager/ThemeCrackNew.kt
@@ -10,6 +10,7 @@ import de.robv.android.xposed.XposedHelpers
 import io.luckypray.dexkit.DexKitBridge
 import miui.drm.DrmManager
 import java.lang.reflect.Method
+import java.io.File
 
 class ThemeCrackNew : BaseHook() {
     override fun init() {
@@ -19,6 +20,7 @@ class ThemeCrackNew : BaseHook() {
             DexKitBridge.create(lpparam.appInfo.sourceDir)?.use { bridge ->
                 val map = mapOf(
                     "DrmResult" to setOf("theme", "ThemeManagerTag", "/system"),
+                    "LargeIcon" to setOf("apply failed", "/data/system/theme/large_icons/", "default_large_icon_product_id"),
                 )
                 val resultMap = bridge.batchFindMethodsUsingStrings {
                     queryMap(map)
@@ -29,6 +31,17 @@ class ThemeCrackNew : BaseHook() {
                 val drmResultMethod: Method = drmResultDescriptor.getMethodInstance(lpparam.classLoader)
                 drmResultMethod.hookAfter {
                     it.result = DrmManager.DrmResult.DRM_SUCCESS
+                }
+                val largeIcon = resultMap["LargeIcon"]!!
+                assert(largeIcon.size == 1)
+                val largeIconDescriptor = largeIcon.first()
+                val largeIconMethod: Method = largeIconDescriptor.getMethodInstance(lpparam.classLoader)
+                largeIconMethod.hookBefore {
+                    val resource = findField(it.thisObject.javaClass) {
+                        type == loadClass("com.android.thememanager.basemodule.resource.model.Resource", lpparam.classLoader)
+                    }
+                    val productId = it.thisObject.getObject(resource.name).invokeMethod("getProductId").toString()
+                    File("/storage/emulated/0/Android/data/com.android.thememanager/files/MIUI/theme/.data/rights/theme/${productId}-largeicons.mra").createNewFile()
                 }
             }
         } catch (t: Throwable) {
