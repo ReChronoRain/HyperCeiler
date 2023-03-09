ecf835464f0b39b3c153304e3ef7800351cb1493
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2023-03-09 16:23:57 +0800

    Update UseThirdPartyBrowser.java

--- app/src/main/java/com/sevtinge/cemiuiler/module/voiceassist/UseThirdPartyBrowser.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/voiceassist/UseThirdPartyBrowser.java
@@ -5,18 +5,52 @@ import android.content.Intent;
 import android.net.Uri;
 
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import io.luckypray.dexkit.DexKitBridge;
+import io.luckypray.dexkit.builder.BatchFindArgs;
+import io.luckypray.dexkit.descriptor.member.DexMethodDescriptor;
+import io.luckypray.dexkit.enums.MatchType;
+
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class UseThirdPartyBrowser extends BaseHook {
 
         public void init() {
 
+            Method browserActivityWithIntent = null;
+
             //XposedBridge.log("Hook到小爱同学进程！");
             try {
-                Class<?> clazz = XposedHelpers.findClass("e.D.L.pa.Wa", lpparam.classLoader);
-                XposedHelpers.findAndHookMethod(clazz,"startActivityWithIntent", Intent.class, boolean.class,int.class, new XC_MethodHook() {
+                System.loadLibrary("dexkit");
+                String apkPath = lpparam.appInfo.sourceDir;
+                try (DexKitBridge bridge = DexKitBridge.create(apkPath)) {
+                    if (bridge == null) {
+                        return;
+                    }
+                    Map<String, List<DexMethodDescriptor>> resultMap =
+                            bridge.batchFindMethodsUsingStrings(
+                                    BatchFindArgs.builder()
+                                            .addQuery("BrowserActivityWithIntent", List.of("IntentUtils", "permission click No Application can handle your intent"))
+                                            .matchType(MatchType.CONTAINS)
+                                            .build()
+                            );
+
+                    List<DexMethodDescriptor> result = Objects.requireNonNull(resultMap.get("BrowserActivityWithIntent"));
+                    for (DexMethodDescriptor descriptor : result) {
+                        browserActivityWithIntent = descriptor.getMethodInstance(lpparam.classLoader);
+                    }
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
+                XposedBridge.log("Cemiuiler: com.miui.voiceassist browserActivityWithIntent method is "+browserActivityWithIntent);
+                //Class<?> clazz = XposedHelpers.findClass("e.D.L.pa.Wa", lpparam.classLoader);
+                XposedBridge.hookMethod(browserActivityWithIntent, new XC_MethodHook() {
                     @Override
                     protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                         super.beforeHookedMethod(param);
