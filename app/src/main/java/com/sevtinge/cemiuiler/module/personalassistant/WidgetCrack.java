6fbc72198ba8509fb2d3b9a6362dd2a095c358c1
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-03-09 19:44:02 +0800

    小组件 1/2

--- app/src/main/java/com/sevtinge/cemiuiler/module/personalassistant/WidgetCrack.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/personalassistant/WidgetCrack.java
@@ -15,104 +15,60 @@ public class WidgetCrack extends BaseHook {
 
     @Override
     public void init() {
-        mPickerDetailResponseWrapper = findClassIfExists("com.miui.personalassistant.picker.business.detail.bean.PickerDetailResponseWrapper");
-        mPickerDetailResponse = findClassIfExists("com.miui.personalassistant.picker.business.detail.bean.PickerDetailResponse");
-        mPickerDetailViewModel = findClassIfExists("com.miui.personalassistant.picker.business.detail.PickerDetailViewModel");
-        mPickerDetailUtil = findClassIfExists("com.miui.personalassistant.picker.business.detail.utils.PickerDetailUtil");
-        mPickerDetailActionController = findClassIfExists("com.miui.personalassistant.picker.business.detail.utils.PickerDetailActionController");
-        mPickerDetailDownloadManager = findClassIfExists("com.miui.personalassistant.picker.business.detail.utils.PickerDetailDownloadManager$Companion");
-
-
-        findAndHookMethod(mPickerDetailViewModel, "isCanDirectAddMaMl", int.class, new MethodHook() {
-            @Override
-            protected void after(MethodHookParam param) throws Throwable {
-                param.setResult(true);
-            }
-        });
-
-        findAndHookMethod(mPickerDetailViewModel, "mamlDownloading", String.class, new MethodHook() {
-            @Override
-            protected void after(MethodHookParam param) throws Throwable {
-
-            }
-        });
-
-        hookAllMethods(mPickerDetailDownloadManager, "isCanDownload", new MethodHook() {
-            @Override
-            protected void before(MethodHookParam param) throws Throwable {
-                param.setResult(true);
-            }
-        });
-
-        hookAllMethods(mPickerDetailUtil, "isCanAutoDownloadMaMl", new MethodHook() {
-            @Override
-            protected void before(MethodHookParam param) throws Throwable {
-                param.setResult(true);
-            }
-        });
-
-        hookAllMethods(mPickerDetailResponse, "isPay", new MethodHook() {
-            @Override
-            protected void before(MethodHookParam param) throws Throwable {
-                param.setResult(false);
-            }
-        });
-
-        hookAllMethods(mPickerDetailResponse, "isBought", new MethodHook() {
-            @Override
-            protected void before(MethodHookParam param) throws Throwable {
-                param.setResult(true);
-            }
-        });
-
-
-        hookAllMethods(mPickerDetailResponseWrapper, "isPay", new MethodHook() {
-            @Override
-            protected void before(MethodHookParam param) throws Throwable {
-                param.setResult(false);
-            }
-        });
-
-        hookAllMethods(mPickerDetailResponseWrapper, "isBought", new MethodHook() {
-            @Override
-            protected void before(MethodHookParam param) throws Throwable {
-                param.setResult(true);
-            }
-        });
-
-        findAndHookMethod(mPickerDetailViewModel, "shouldCheckMamlBoughtState", int.class, new MethodHook() {
-            @Override
-            protected void after(MethodHookParam param) throws Throwable {
-                param.setResult(false);
-            }
-        });
-
-        findAndHookMethod(mPickerDetailViewModel, "isTargetPositionMamlPayAndDownloading", int.class, new MethodHook() {
-            @Override
-            protected void after(MethodHookParam param) throws Throwable {
-                param.setResult(false);
-            }
-        });
-
-        findAndHookMethod(mPickerDetailViewModel, "checkIsIndependentProcessWidgetForPosition", int.class, new MethodHook() {
-            @Override
-            protected void after(MethodHookParam param) throws Throwable {
-                param.setResult(true);
-            }
-        });
-
-        findAndHookMethod("com.miui.personalassistant.picker.bean.cards.SuitEntity", "isShowPayLogo", new MethodHook() {
-            @Override
-            protected void after(MethodHookParam param) throws Throwable {
-                param.setResult(false);
-            }
-        });
-
-        hookAllMethods("com.miui.maml.widget.edit.MamlutilKt", "themeManagerSupportPaidWidget", new MethodHook() {
-            @Override
-            protected void after(MethodHookParam param) throws Throwable {
-                param.setResult(false);
-            }
-        });
+        findMethod("com.miui.maml.widget.edit.MamlutilKt") {
+                    name == "themeManagerSupportPaidWidget"
+                }.hookAfter {
+                    it.result = false
+                }
+                findMethod("com.miui.personalassistant.picker.business.detail.PickerDetailViewModel") {
+                    name == "isCanDirectAddMaMl"
+                }.hookAfter {
+                    it.result = true
+                }
+                findMethod("com.miui.personalassistant.picker.business.detail.utils.PickerDetailDownloadManager\$Companion") {
+                    name == "isCanDownload"
+                }.hookBefore {
+                    it.result = true
+                }
+                findMethod("com.miui.personalassistant.picker.business.detail.utils.PickerDetailUtil") {
+                    name == "isCanAutoDownloadMaMl"
+                }.hookBefore {
+                    it.result = true
+                }
+                findMethod("com.miui.personalassistant.picker.business.detail.bean.PickerDetailResponse") {
+                    name == "isPay"
+                }.hookBefore {
+                    it.result = false
+                }
+                findMethod("com.miui.personalassistant.picker.business.detail.bean.PickerDetailResponse") {
+                    name == "isBought"
+                }.hookBefore {
+                    it.result = true
+                }
+                findMethod("com.miui.personalassistant.picker.business.detail.bean.PickerDetailResponseWrapper") {
+                    name == "isPay"
+                }.hookBefore {
+                    it.result = false
+                }
+                findMethod("com.miui.personalassistant.picker.business.detail.bean.PickerDetailResponseWrapper") {
+                    name == "isBought"
+                }.hookBefore {
+                    it.result = true
+                }
+                findMethod("com.miui.personalassistant.picker.business.detail.PickerDetailViewModel") {
+                    name == "shouldCheckMamlBoughtState"
+                }.hookAfter {
+                    it.result = false
+                }
+                findMethod("com.miui.personalassistant.picker.business.detail.PickerDetailViewModel") {
+                    name == "isTargetPositionMamlPayAndDownloading"
+                }.hookAfter {
+                    it.result = false
+                }
+                findMethod("com.miui.personalassistant.picker.business.detail.PickerDetailViewModel") {
+                    name == "checkIsIndependentProcessWidgetForPosition"
+                }.hookAfter {
+                    it.result = true
+                }
     }
 }
