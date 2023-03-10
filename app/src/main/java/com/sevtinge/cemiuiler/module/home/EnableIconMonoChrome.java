7e8d0130c5b08dc1e31fdf540ee117f9ffa88196
mu7220 <linqiqi233@qq.com>
2023-03-10 22:16:12 +0800

    同步上游

--- app/src/main/java/com/sevtinge/cemiuiler/module/home/EnableIconMonoChrome.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/home/EnableIconMonoChrome.java
@@ -6,7 +6,7 @@ import com.sevtinge.cemiuiler.module.base.BaseHook;
 public class EnableIconMonoChrome extends BaseHook {
         @Override
         public void init() {
-            findAndHookMethod("com.miui.home.launcher.graphics.MonoChromeUtils", "isSupportMonoChrome", new MethodHook() {
+            findAndHookMethod("com.miui.home.launcher.graphics.MonochromeUtils", "isSupportMonochrome", new MethodHook() {
                 @Override
                 protected void before(MethodHookParam param) throws Throwable {
                     param.setResult(true);
