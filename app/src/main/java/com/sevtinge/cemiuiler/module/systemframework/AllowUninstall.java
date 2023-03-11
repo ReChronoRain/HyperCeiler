e5aecac68bfb4d5c0d10d7c1cd34a54639aaacc0
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2023-03-11 18:45:58 +0800

    新增 系统框架-其他-允许卸载部分预装APP

---
+++ app/src/main/java/com/sevtinge/cemiuiler/module/systemframework/AllowUninstall.java
@@ -0,0 +1,58 @@
+package com.sevtinge.cemiuiler.module.systemframework;
+
+import dalvik.system.PathClassLoader;
+import de.robv.android.xposed.*;
+
+import static com.sevtinge.cemiuiler.module.base.BaseHook.mPrefsMap;
+
+public class AllowUninstall implements IXposedHookZygoteInit {
+
+    private final String SecurityManagerServiceName = "com.miui.server.SecurityManagerService$1";
+
+    private PathClassLoader servicesClassLoader = null;
+
+    private Class<?> SecurityManagerServiceClazz = null;
+
+    private java.util.Set<XC_MethodHook.Unhook> pathClassLoaderHook = null;
+
+    @Override
+    public void initZygote(StartupParam startupParam) {
+        if (mPrefsMap.getBoolean("system_framework_allow_uninstall")) {
+            XposedBridge.log("Cemiuiler: hook all PathClassLoader Constructors");
+            pathClassLoaderHook =
+                    XposedBridge.hookAllConstructors(PathClassLoader.class, new XC_MethodHook() {
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) {
+                            String path = param.args[0].toString();
+                            if (path.contains("/system/framework/services.jar")) {
+                                XposedBridge.log("Cemiuiler: find services.jar ClassLoader");
+                                try {
+                                    servicesClassLoader = (PathClassLoader) param.thisObject;
+                                    SecurityManagerServiceClazz = XposedHelpers.findClass(
+                                            SecurityManagerServiceName,
+                                            servicesClassLoader);
+                                    XposedBridge.log("Cemiuiler: findClass SecurityManagerService$1");
+                                    XposedHelpers.findAndHookMethod(SecurityManagerServiceClazz,
+                                            "run", new XC_MethodReplacement() {
+                                                @Override
+                                                protected Object replaceHookedMethod(MethodHookParam unused) {
+                                                    XposedBridge.log("Cemiuiler: hooked checkSystemSelfProtection invoke");
+                                                    return null;
+                                                }
+                                            });
+                                    XposedBridge.log("Cemiuiler: hook method 'SecurityManagerService$1.run()'");
+                                } catch (Exception e) {
+                                    XposedBridge.log("Cemiuiler: AllowUninstall Exception!");
+                                    e.printStackTrace();
+                                } finally {
+                                    for (Unhook hook : pathClassLoaderHook) {
+                                        hook.unhook();
+                                    }
+                                    XposedBridge.log("Cemiuiler: unhook all PathClassLoader Constructors");
+                                }
+                            }
+                        }
+                    });
+        }
+    }
+}
