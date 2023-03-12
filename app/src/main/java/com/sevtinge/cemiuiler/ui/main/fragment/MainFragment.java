51098c247b2d03e41522f2aa3d11178ac72c29a4
mu7220 <linqiqi233@qq.com>
2023-03-12 17:21:45 +0800

    优化显示电量与性能 - 自定义高刷

--- app/src/main/java/com/sevtinge/cemiuiler/ui/main/fragment/MainFragment.java
+++ app/src/main/java/com/sevtinge/cemiuiler/ui/main/fragment/MainFragment.java
@@ -5,7 +5,12 @@ import android.os.Bundle;
 import com.sevtinge.cemiuiler.R;
 import com.sevtinge.cemiuiler.ui.base.SubFragment;
 
+import moralnorm.os.SdkVersion;
+import moralnorm.preference.Preference;
+
 public class MainFragment extends SubFragment {
+
+    Preference mPowerSetting;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -15,5 +20,11 @@ public class MainFragment extends SubFragment {
     @Override
     public int getContentResId() {
         return R.xml.prefs_main;
+    }
+
+    @Override
+    public void initPrefs() {
+        mPowerSetting = findPreference("prefs_key_powerkeeper");
+        mPowerSetting.setVisible(SdkVersion.isAndroidT);
     }
 }
