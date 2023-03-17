bdecdc600a47e32455e0e734bc79486aa09c65e5
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2023-03-18 07:45:55 +0800

    优化 MIUI安全组件-阻止上传已安装APP列表

--- app/src/main/java/com/sevtinge/cemiuiler/module/GuardProvider.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/GuardProvider.java
@@ -2,10 +2,12 @@ package com.sevtinge.cemiuiler.module;
 
 import com.sevtinge.cemiuiler.module.base.BaseModule;
 import com.sevtinge.cemiuiler.module.guardprovider.DisableUploadAppList;
+import com.sevtinge.cemiuiler.module.guardprovider.DisableUploadAppListNew;
 
 public class GuardProvider extends BaseModule {
     @Override
     public void handleLoadPackage() {
-        initHook(new DisableUploadAppList(), mPrefsMap.getBoolean("disable_upload_applist"));
+        //initHook(new DisableUploadAppList(), mPrefsMap.getBoolean("disable_upload_applist"));
+        initHook(new DisableUploadAppListNew(), mPrefsMap.getBoolean("disable_upload_applist"));
     }
 }
---
+++ app/src/main/java/com/sevtinge/cemiuiler/module/guardprovider/DisableUploadAppListNew.kt
@@ -0,0 +1,35 @@
+package com.sevtinge.cemiuiler.module.guardprovider
+
+import com.sevtinge.cemiuiler.module.base.BaseHook
+import com.sevtinge.cemiuiler.utils.replaceMethod
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+import io.luckypray.dexkit.DexKitBridge
+import java.lang.reflect.Method
+
+class DisableUploadAppListNew : BaseHook() {
+
+    @Throws(NoSuchMethodException::class)
+    override fun init() {
+        System.loadLibrary("dexkit")
+        DexKitBridge.create(lpparam.appInfo.sourceDir)?.use { bridge ->
+
+            val map = mapOf(
+                "AntiDefraudAppManager" to setOf("AntiDefraudAppManager", "https://flash.sec.miui.com/detect/app"),
+            )
+
+            val resultMap = bridge.batchFindMethodsUsingStrings {
+                queryMap(map)
+            }
+
+            val antiDefraudAppManager = resultMap["AntiDefraudAppManager"]!!
+            assert(antiDefraudAppManager.size == 1)
+            val antiDefraudAppManagerDescriptor = antiDefraudAppManager.first()
+            val antiDefraudAppManagerMethod: Method = antiDefraudAppManagerDescriptor.getMethodInstance(lpparam.classLoader)
+            antiDefraudAppManagerMethod.replaceMethod {
+                return@replaceMethod null
+            }
+        }
+    }
+
+}
