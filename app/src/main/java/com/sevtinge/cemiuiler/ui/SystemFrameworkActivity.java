60af5a9d9569dd1d96212d9007c069a44503975b
mu7220 <linqiqi233@qq.com>
2023-03-20 14:00:43 +0800

    隐藏 Android13 过渡动画使用原生样式

--- app/src/main/java/com/sevtinge/cemiuiler/ui/SystemFrameworkActivity.java
+++ app/src/main/java/com/sevtinge/cemiuiler/ui/SystemFrameworkActivity.java
@@ -4,12 +4,32 @@ import androidx.fragment.app.Fragment;
 
 import com.sevtinge.cemiuiler.R;
 import com.sevtinge.cemiuiler.ui.base.BaseAppCompatActivity;
-import com.sevtinge.cemiuiler.ui.base.PreferenceFragment;
+
+import com.sevtinge.cemiuiler.ui.base.SubFragment;
+
+import moralnorm.os.SdkVersion;
+import moralnorm.preference.SwitchPreference;
+
 
 public class SystemFrameworkActivity extends BaseAppCompatActivity {
 
     @Override
     public Fragment initFragment() {
-        return new PreferenceFragment(R.xml.system_framework);
+        return new SystemFrameworkActivity.SystemFrameworkFragment();
+    }
+
+    public static class SystemFrameworkFragment extends SubFragment {
+        @Override
+        public int getContentResId() {
+            return R.xml.system_framework;
+        }
+
+        @Override
+        public void initPrefs() {
+            SwitchPreference mOriginalAnimation;
+
+            mOriginalAnimation = findPreference("prefs_key_system_framework_other_use_original_animation");
+            mOriginalAnimation.setVisible(SdkVersion.isAndroidS || SdkVersion.isAndroidR);
+        }
     }
 }
