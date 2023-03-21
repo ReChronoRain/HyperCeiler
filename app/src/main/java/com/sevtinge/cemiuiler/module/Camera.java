17e87e9ce477bd14d8c02dc65ac302d4cd7c9583
绀漓丨Sevtinge <sevtinge@gmail.com>
2023-03-21 16:16:42 +0800

    新增 相机-解锁魔法分身

--- app/src/main/java/com/sevtinge/cemiuiler/module/Camera.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/Camera.java
@@ -1,8 +1,7 @@
 package com.sevtinge.cemiuiler.module;
 
 import com.sevtinge.cemiuiler.module.base.BaseModule;
-import com.sevtinge.cemiuiler.module.camera.UnlockAiWatermark;
-import com.sevtinge.cemiuiler.module.camera.UnlockAmbilight;
+import com.sevtinge.cemiuiler.module.camera.*;
 
 public class Camera extends BaseModule {
 
@@ -10,5 +9,6 @@ public class Camera extends BaseModule {
     public void handleLoadPackage() {
         initHook(new UnlockAiWatermark(), mPrefsMap.getBoolean("camera_features_unlock_aiwatermark"));
         initHook(new UnlockAmbilight(), mPrefsMap.getBoolean("camera_features_unlock_ambilight"));
+        initHook(new UnlockClone(), mPrefsMap.getBoolean("camera_features_unlock_clone"));
     }
 }
