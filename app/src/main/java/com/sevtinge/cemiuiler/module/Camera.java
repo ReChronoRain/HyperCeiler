dece7d5ad640738308c73a2d6c76df6106d9db47
绀漓丨Sevtinge <sevtinge@gmail.com>
2023-03-21 16:13:59 +0800

    新增 相机-解锁长曝光

--- app/src/main/java/com/sevtinge/cemiuiler/module/Camera.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/Camera.java
@@ -2,12 +2,13 @@ package com.sevtinge.cemiuiler.module;
 
 import com.sevtinge.cemiuiler.module.base.BaseModule;
 import com.sevtinge.cemiuiler.module.camera.UnlockAiWatermark;
+import com.sevtinge.cemiuiler.module.camera.UnlockAmbilight;
 
 public class Camera extends BaseModule {
 
     @Override
     public void handleLoadPackage() {
         initHook(new UnlockAiWatermark(), mPrefsMap.getBoolean("camera_features_unlock_aiwatermark"));
-
+        initHook(new UnlockAmbilight(), mPrefsMap.getBoolean("camera_features_unlock_ambilight"));
     }
 }
--- app/src/main/java/com/sevtinge/cemiuiler/module/camera/UnlockAmbilight.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/camera/UnlockAmbilight.java
@@ -2,10 +2,10 @@ package com.sevtinge.cemiuiler.module.camera;
 
 import com.sevtinge.cemiuiler.module.base.BaseHook;
 
-public class UnlockAiWatermark extends BaseHook {
+public class UnlockAmbilight extends BaseHook {
     @Override
     public void init() {
-        findAndHookMethod("com.android.camera.features.mode.aiwatermark.AIWaterModuleEntry", "support", new MethodHook() {
+        findAndHookMethod("com.android.camera.features.mode.ambilight.AmbilightModuleEntry", "support", new BaseHook.MethodHook() {
             @Override
             protected void before(MethodHookParam param) throws Throwable {
                 param.setResult(true);
