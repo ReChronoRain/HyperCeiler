0b06f009b6d92c72aade833c70a1ca792efe1b10
MoralNorm <732388462@qq.com>
2023-03-22 22:27:04 +0800

    Update Gradle to 7.4.1

--- app/src/main/java/com/sevtinge/cemiuiler/ui/systemui/statusbar/ClockIndicatorActivity.java
+++ app/src/main/java/com/sevtinge/cemiuiler/ui/systemui/statusbar/ClockIndicatorActivity.java
@@ -5,53 +5,54 @@ import androidx.fragment.app.Fragment;
 import com.sevtinge.cemiuiler.R;
 import com.sevtinge.cemiuiler.ui.base.SubFragment;
 import com.sevtinge.cemiuiler.ui.systemui.base.BaseSystemUIActivity;
+import com.sevtinge.cemiuiler.utils.PrefsUtils;
+
+import moralnorm.preference.DropDownPreference;
+import moralnorm.preference.Preference;
+import moralnorm.preference.PreferenceCategory;
 
 public class ClockIndicatorActivity extends BaseSystemUIActivity {
+
     @Override
     public Fragment initFragment() {
         return new ClockIndicatorActivity.ClockIndicatorFragment();
     }
 
-    public static class ClockIndicatorFragment extends SubFragment {
+    public static class ClockIndicatorFragment extends SubFragment implements Preference.OnPreferenceChangeListener {
 
-/*
+        DropDownPreference mClockModePreference;
         PreferenceCategory mDefault;
         PreferenceCategory mGeek;
-*/
 
         @Override
         public int getContentResId() {
             return R.xml.system_ui_status_bar_clock_indicator;
         }
 
-        /*public void onPreferenceChange(Preference preference, Object newValue) {
-            preference.setOnPreferenceChangeListener((Preference.OnPreferenceChangeListener) newValue);
-        }*/
-
-        /*@Override
+        @Override
         public void initPrefs() {
-            int getMode = mPrefsMap.getInt("system_ui_statusbar_clock_mode", 1);
+            int mClockMode = Integer.parseInt(PrefsUtils.getSharedStringPrefs(getContext(), "prefs_key_system_ui_statusbar_clock_mode", "1"));
+            mClockModePreference = findPreference("prefs_key_system_ui_statusbar_clock_mode");
             mDefault = findPreference("prefs_key_system_ui_statusbar_clock_default");
             mGeek = findPreference("prefs_key_system_ui_statusbar_clock_geek");
 
-            switch (getMode) {
-                case 1:
-                    if (mDefault != null) {
-                        mDefault.setVisible(true);
-                    }
-                    if (mGeek != null) {
-                        mGeek.setVisible(false);
-                    }
-                    break;
-                case 2:
-                    if (mDefault != null) {
-                        mDefault.setVisible(false);
-                    }
-                    if (mGeek != null) {
-                        mGeek.setVisible(true);
-                    }
-                    break;
+            setClockMode(mClockMode);
+            mClockModePreference.setOnPreferenceChangeListener(this);
+
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object o) {
+            if (preference == mClockModePreference) {
+                setClockMode(Integer.parseInt((String)o));
             }
-        }*/
+            return true;
+        }
+
+        private void setClockMode(int mode) {
+            mDefault.setVisible(mode != 1);
+            mGeek.setVisible(mode != 2);
+        }
+
     }
 }
