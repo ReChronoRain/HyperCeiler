f98041fc5bc49def872a5aaf82c052cea6c8ac2f
mu7220 <linqiqi233@qq.com>
2023-03-23 21:04:01 +0800

    新增 设置-权限-上层权限免翻应用列表

--- app/src/main/java/com/sevtinge/cemiuiler/module/SystemSettings.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/SystemSettings.java
@@ -1,19 +1,20 @@
 package com.sevtinge.cemiuiler.module;
 
 import com.sevtinge.cemiuiler.module.base.BaseModule;
-import com.sevtinge.cemiuiler.module.mirror.UnlockMiuiPlus;
 import com.sevtinge.cemiuiler.module.systemsettings.AddMiuiPlusEntry;
 import com.sevtinge.cemiuiler.module.systemsettings.EnableSpeedMode;
+import com.sevtinge.cemiuiler.module.systemsettings.PermissionTopOfApp;
 import com.sevtinge.cemiuiler.module.systemsettings.VoipAssistantController;
 
 public class SystemSettings extends BaseModule {
 
-        @Override
-        public void handleLoadPackage() {
-            initHook(new VoipAssistantController(), mPrefsMap.getBoolean("system_settings_voip_assistant_controller"));
-            initHook(new AddMiuiPlusEntry(), mPrefsMap.getBoolean("mirror_unlock_miui_plus"));
-            initHook(new EnableSpeedMode(), mPrefsMap.getBoolean("system_settings_develop_speed_mode"));
-        }
+    @Override
+    public void handleLoadPackage() {
+        initHook(new VoipAssistantController(), mPrefsMap.getBoolean("system_settings_voip_assistant_controller"));
+        initHook(new AddMiuiPlusEntry(), mPrefsMap.getBoolean("mirror_unlock_miui_plus"));
+        initHook(new EnableSpeedMode(), mPrefsMap.getBoolean("system_settings_develop_speed_mode"));
+        initHook(PermissionTopOfApp.INSTANCE, mPrefsMap.getBoolean("system_settings_permission_show_app_up"));
     }
+}
 
 
