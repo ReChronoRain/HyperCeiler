077d6ed0a44a1bd1a5cd143c197bcda53ee9d555
mu7220 <linqiqi233@qq.com>
2023-03-23 23:58:07 +0800

    新增 系统界面-状态栏-网速指示器-常规-网速分隔符

---
+++ app/src/main/java/com/sevtinge/cemiuiler/module/systemui/statusbar/StatusBarNoNetSpeedSep.kt
@@ -0,0 +1,24 @@
+package com.sevtinge.cemiuiler.module.systemui.statusbar
+
+import android.view.View
+import com.sevtinge.cemiuiler.module.base.BaseHook
+import com.sevtinge.cemiuiler.utils.Helpers
+import de.robv.android.xposed.XposedHelpers
+
+
+object StatusBarNoNetSpeedSep : BaseHook(){
+    override fun init() {
+        Helpers.findAndHookMethod("com.android.systemui.statusbar.views.NetworkSpeedSplitter", lpparam.classLoader, "updateVisibility",
+            object : MethodHook() {
+                @Throws(Throwable::class)
+                override fun before(param: MethodHookParam) {
+                    XposedHelpers.setObjectField(
+                        param.thisObject,
+                        "mNetworkSpeedVisibility",
+                        View.GONE
+                    )
+                }
+            }
+        )
+    }
+}
