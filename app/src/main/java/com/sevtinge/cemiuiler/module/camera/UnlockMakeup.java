830262a421024d79641fb496e63c8b161c18e51f
绀漓丨Sevtinge <sevtinge@gmail.com>
2023-03-23 18:23:51 +0800

    Update UnlockMakeup.java

--- app/src/main/java/com/sevtinge/cemiuiler/module/camera/UnlockMakeup.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/camera/UnlockMakeup.java
@@ -5,6 +5,18 @@ import com.sevtinge.cemiuiler.module.base.BaseHook;
 public class UnlockMakeup extends BaseHook {
     @Override
     public void init() {
+        findAndHookMethod("com.android.camera2.CameraCapabilities", "isSupportMakeups", new BaseHook.MethodHook() {
+            @Override
+            protected void before(MethodHookParam param) throws Throwable {
+                param.setResult(true);
+            }
+        });
+        findAndHookMethod("com.android.camera2.CameraCapabilities", "isSupportMakeups2", new BaseHook.MethodHook() {
+            @Override
+            protected void before(MethodHookParam param) throws Throwable {
+                param.setResult(true);
+            }
+        });
         findAndHookMethod("com.android.camera2.CameraCapabilitiesUtil", "isSupportMakeups", new BaseHook.MethodHook() {
             @Override
             protected void before(MethodHookParam param) throws Throwable {
