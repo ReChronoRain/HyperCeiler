0d3a7dcb3ff44b5ccf68f40fa8f5bf898ee20333
mu7220 <linqiqi233@qq.com>
2023-03-25 17:51:27 +0800

    优化 代码

--- app/src/main/java/com/sevtinge/cemiuiler/module/mishare/NoAutoTurnOff.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/mishare/NoAutoTurnOff.kt
@@ -4,36 +4,47 @@ import com.github.kyuubiran.ezxhelper.utils.hookBefore
 import com.sevtinge.cemiuiler.module.base.BaseHook
 import de.robv.android.xposed.XposedBridge
 import io.luckypray.dexkit.DexKitBridge
-import io.luckypray.dexkit.enums.MatchType
 import java.lang.reflect.Method
 
 class NoAutoTurnOff : BaseHook() {
 
     @Throws(NoSuchMethodException::class)
     override fun init() {
+        //仅对 2.16.0 生效
         System.loadLibrary("dexkit")
         DexKitBridge.create(lpparam.appInfo.sourceDir)?.use { bridge ->
-            listOf("EnabledState").forEach { usingString ->
-                val resultList = bridge.findMethodUsingString {
-                    this.usingString = usingString
-                    matchType = MatchType.SIMILAR_REGEX
+            val map = mapOf(
+                "EnabledState" to setOf("MiShareService", "EnabledState"),
+            )
+            val resultMap = bridge.batchFindMethodsUsingStrings { queryMap(map) }
+            val enabledManager = resultMap["EnabledState"]!!
+            assert(enabledManager.size == 1)
+            val anEnabled = enabledManager.first()
+            val enabledReturnMethod: Method = anEnabled.getMethodInstance(lpparam.classLoader)
+            try {
+                enabledReturnMethod.hookBefore {
+                    it.result = true
                 }
-                assert(resultList.size == 1)
-                val enabledReturn = resultList.first()
-                val enabledReturnMethod: Method =
-                    enabledReturn.getMethodInstance(lpparam.classLoader) //getMethodInstance 使用需定位到方法
-                    // Lcom/miui/mishare/connectivity/MiShareService$j$g;->a()V 小米互传 2.16.0 定位方法名
-                try {
-                    enabledReturnMethod.hookBefore {
-                        it.result = true
-                    }
-                    XposedBridge.log("Cemiuiler: $enabledReturn")
-                    XposedBridge.log("Cemiuiler: NoAutoTurnOff com.miui.mishare.connectivity.MiShareService success!")
-                } catch (e: Throwable) {
-                    XposedBridge.log("Cemiuiler: NoAutoTurnOff com.miui.mishare.connectivity.MiShareService failed!")
-                    XposedBridge.log(e)
-                }
+                XposedBridge.log("Cemiuiler: Found hook class is $anEnabled")
+                XposedBridge.log("Cemiuiler: NoAutoTurnOff com.miui.mishare.connectivity.MiShareService success!")
+            } catch (e: Throwable) {
+                XposedBridge.log("Cemiuiler: NoAutoTurnOff com.miui.mishare.connectivity.MiShareService failed!")
+                XposedBridge.log(e)
             }
+
+//            listOf("MiShareService", "EnabledState").forEach { usingString ->
+//                val resultList = bridge.findMethodUsingString {
+//                    this.usingString = usingString
+//                    matchType = MatchType.SIMILAR_REGEX
+//                }
+//                assert(resultList.size == 2)
+//                val enabledReturn = resultList.first()
+//                val enabledReturnMethod: Method =
+//                    enabledReturn.getMethodInstance(lpparam.classLoader)
+//                // Lcom/miui/mishare/connectivity/MiShareService$d$g;->b()V 小米互传 2.15.0 定位方法名
+//                // Lcom/miui/mishare/connectivity/MiShareService$j$g;->a()V 小米互传 2.16.0 定位方法名
+//
+//            }
         }
 //        try {
 //            findMethod("com.miui.mishare.connectivity.MiShareService\$d\$g") {
