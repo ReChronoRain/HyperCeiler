a8223c195496b3b7b98431b789d2cabee4c48ef6
绀漓丨Sevtinge <sevtinge@gmail.com>
2023-03-27 02:06:25 +0800

    Update UpdaterActivity.java

--- app/src/main/java/com/sevtinge/cemiuiler/ui/UpdaterActivity.java
+++ app/src/main/java/com/sevtinge/cemiuiler/ui/UpdaterActivity.java
@@ -4,39 +4,26 @@ import androidx.fragment.app.Fragment;
 import com.sevtinge.cemiuiler.R;
 import com.sevtinge.cemiuiler.ui.base.BaseAppCompatActivity;
 import com.sevtinge.cemiuiler.ui.base.SubFragment;
-import moralnorm.preference.EditTextPreference;
 
 public class UpdaterActivity extends BaseAppCompatActivity {
 
+    @Override
+    public Fragment initFragment() {
+        return new com.sevtinge.cemiuiler.ui.UpdaterActivity.UpdaterFragment();
+    }
+
+    public static class UpdaterFragment extends SubFragment {
+
         @Override
-        public Fragment initFragment() {
-            return new com.sevtinge.cemiuiler.ui.UpdaterActivity.UpdaterFragment();
+        public int getContentResId() {
+            return R.xml.updater;
         }
-
-        public static class UpdaterFragment extends SubFragment {
-
-            EditTextPreference mDeviceModify;
-
-            @Override
-            public int getContentResId() {
-                return R.xml.updater;
-            }
-
-            @Override
-            public void initPrefs() {
-                mDeviceModify = findPreference("prefs_key_updater_device");
-
-                if (!getSharedPreferences().getBoolean("prefs_key_hidden_function",false)) {
-                    mDeviceModify.setVisible(false);
-                }
-            }
-
-        }
-
-
 
 
     }
+
+
+}
 
 
 
