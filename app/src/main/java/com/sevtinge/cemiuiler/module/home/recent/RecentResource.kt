c81b8105f960bc5096058d54ad5f56c35bb52cc3
绀漓丨Sevtinge <sevtinge@gmail.com>
2023-03-28 23:17:30 +0800

    修复 部分hook失败

--- app/src/main/java/com/sevtinge/cemiuiler/module/home/recent/RecentResource.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/home/recent/RecentResource.kt
@@ -1,7 +1,10 @@
 package com.sevtinge.cemiuiler.module.home.recent
 
+import android.app.Application
+import android.content.Context
 import com.sevtinge.cemiuiler.module.base.BaseHook
 import android.content.res.Resources
+import com.github.kyuubiran.ezxhelper.init.EzXHelperInit
 import com.github.kyuubiran.ezxhelper.init.InitFields.appContext
 import com.sevtinge.cemiuiler.utils.ResourcesHookData
 import com.sevtinge.cemiuiler.utils.ResourcesHookMap
@@ -23,21 +26,28 @@ object RecentResource : BaseHook() {
     }
 
     override fun init() {
+        Application::class.java.hookBeforeMethod("attach", Context::class.java) {
+            EzXHelperInit.initHandleLoadPackage(lpparam)
+            EzXHelperInit.setLogTag(TAG)
+            EzXHelperInit.setToastTag(TAG)
+            EzXHelperInit.initAppContext(it.args[0] as Context)
 
-        Resources::class.java.hookBeforeMethod("getBoolean", Int::class.javaPrimitiveType) { hook(it) }
-        Resources::class.java.hookBeforeMethod("getDimension", Int::class.javaPrimitiveType) { hook(it) }
-        Resources::class.java.hookBeforeMethod("getDimensionPixelOffset", Int::class.javaPrimitiveType) { hook(it) }
-        Resources::class.java.hookBeforeMethod("getDimensionPixelSize", Int::class.javaPrimitiveType) { hook(it) }
-        Resources::class.java.hookBeforeMethod("getInteger", Int::class.javaPrimitiveType) { hook(it) }
-        Resources::class.java.hookBeforeMethod("getText", Int::class.javaPrimitiveType) { hook(it) }
+            Resources::class.java.hookBeforeMethod("getBoolean", Int::class.javaPrimitiveType) { hook(it) }
+            Resources::class.java.hookBeforeMethod("getDimension", Int::class.javaPrimitiveType) { hook(it) }
+            Resources::class.java.hookBeforeMethod("getDimensionPixelOffset", Int::class.javaPrimitiveType) { hook(it) }
+            Resources::class.java.hookBeforeMethod("getDimensionPixelSize", Int::class.javaPrimitiveType) { hook(it) }
+            Resources::class.java.hookBeforeMethod("getInteger", Int::class.javaPrimitiveType) { hook(it) }
+            Resources::class.java.hookBeforeMethod("getText", Int::class.javaPrimitiveType) { hook(it) }
 
-        val value = mPrefsMap.getInt("task_view_corners", -1).toFloat()
-        val value1 = mPrefsMap.getInt("task_view_header_height", -1).toFloat()
-        if (value != -1f && value != 20f) {
-            hookMap["recents_task_view_rounded_corners_radius_min"] = ResourcesHookData("dimen", dp2px(value))
-            hookMap["recents_task_view_rounded_corners_radius_max"] = ResourcesHookData("dimen", dp2px(value))
+            val value = mPrefsMap.getInt("task_view_corners", -1).toFloat()
+            val value1 = mPrefsMap.getInt("task_view_header_height", -1).toFloat()
+            if (value != -1f && value != 20f) {
+                hookMap["recents_task_view_rounded_corners_radius_min"] = ResourcesHookData("dimen", dp2px(value))
+                hookMap["recents_task_view_rounded_corners_radius_max"] = ResourcesHookData("dimen", dp2px(value))
+            }
+            if (value1 != -1f && value != 40f) hookMap["recents_task_view_header_height"] =
+                ResourcesHookData("dimen", dp2px(value1))
         }
-        if (value1 != -1f && value != 40f) hookMap["recents_task_view_header_height"] = ResourcesHookData("dimen", dp2px(value1))
     }
 
 }
