da4f222124f7b55d9506e7ca77e7fa5043d2cd76
绀漓丨Sevtinge <sevtinge@gmail.com>
2023-03-28 23:41:45 +0800

    修复 部分hook失败

--- app/src/main/java/com/sevtinge/cemiuiler/module/home/drawer/AllAppsContainerViewBlur.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/home/drawer/AllAppsContainerViewBlur.kt
@@ -1,49 +1,55 @@
 package com.sevtinge.cemiuiler.module.home.drawer
 
+import android.app.Application
+import android.content.Context
 import android.os.Build
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.RelativeLayout
 import android.widget.ViewSwitcher
 import androidx.annotation.RequiresApi
+import com.github.kyuubiran.ezxhelper.init.EzXHelperInit
 import com.github.kyuubiran.ezxhelper.utils.findMethod
 import com.github.kyuubiran.ezxhelper.utils.hookAfter
 import com.sevtinge.cemiuiler.module.base.BaseHook
 import com.sevtinge.cemiuiler.utils.findClass
 import com.sevtinge.cemiuiler.utils.getObjectField
-//import com.yuk.miuiHomeR.mPrefsMap
-//import com.yuk.miuiHomeR.utils.ktx.findClass
 import com.sevtinge.cemiuiler.utils.getCornerRadiusTop
-//import com.yuk.miuiHomeR.utils.ktx.getObjectField
+import com.sevtinge.cemiuiler.utils.hookBeforeMethod
 import com.zhenxiang.blur.BlurFrameLayout
 import com.zhenxiang.blur.model.CornersRadius
 
 @RequiresApi(Build.VERSION_CODES.S)
 object AllAppsContainerViewBlur : BaseHook() {
     override fun init() {
+        Application::class.java.hookBeforeMethod("attach", Context::class.java) {
+            EzXHelperInit.initHandleLoadPackage(lpparam)
+            EzXHelperInit.setLogTag(TAG)
+            EzXHelperInit.setToastTag(TAG)
+            EzXHelperInit.initAppContext(it.args[0] as Context)
 
-        //if (!mPrefsMap.getBoolean("home_all_apps_blur")) return
-        findMethod("com.miui.home.launcher.allapps.BaseAllAppsContainerView".findClass(), true) {
-            name == "onFinishInflate"
-        }.hookAfter { hookParam ->
-            val mCategoryContainer = hookParam.thisObject.getObjectField("mCategoryContainer") as ViewSwitcher
-            val appsView = mCategoryContainer.parent as RelativeLayout
-            val blur = BlurFrameLayout(mCategoryContainer.context)
-            val radius = getCornerRadiusTop().toFloat()
-            blur.blurController.apply {
-                cornerRadius = CornersRadius.custom(radius, radius, 0f, 0f)
-            }
-            val view = View(mCategoryContainer.context)
-            blur.addView(view)
-            (view.layoutParams as FrameLayout.LayoutParams).apply {
-                width = FrameLayout.LayoutParams.MATCH_PARENT
-                height = FrameLayout.LayoutParams.MATCH_PARENT
-            }
-            appsView.addView(blur, 0)
             findMethod("com.miui.home.launcher.allapps.BaseAllAppsContainerView".findClass(), true) {
-                name == "onResume"
-            }.hookAfter {
-                blur.refreshDrawableState()
+                name == "onFinishInflate"
+            }.hookAfter { hookParam ->
+                val mCategoryContainer = hookParam.thisObject.getObjectField("mCategoryContainer") as ViewSwitcher
+                val appsView = mCategoryContainer.parent as RelativeLayout
+                val blur = BlurFrameLayout(mCategoryContainer.context)
+                val radius = getCornerRadiusTop().toFloat()
+                blur.blurController.apply {
+                    cornerRadius = CornersRadius.custom(radius, radius, 0f, 0f)
+                }
+                val view = View(mCategoryContainer.context)
+                blur.addView(view)
+                (view.layoutParams as FrameLayout.LayoutParams).apply {
+                    width = FrameLayout.LayoutParams.MATCH_PARENT
+                    height = FrameLayout.LayoutParams.MATCH_PARENT
+                }
+                appsView.addView(blur, 0)
+                findMethod("com.miui.home.launcher.allapps.BaseAllAppsContainerView".findClass(), true) {
+                    name == "onResume"
+                }.hookAfter {
+                    blur.refreshDrawableState()
+                }
             }
         }
 
