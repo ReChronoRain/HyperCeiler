4f013f1aaa4c8670e194ede788c5bcdd9cb263d4
绀漓丨Sevtinge <sevtinge@gmail.com>
2023-04-03 13:06:10 +0800

    新增 设置-快速打开安装未知应用

--- app/src/main/java/com/sevtinge/cemiuiler/module/SystemSettings.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/SystemSettings.java
@@ -1,10 +1,7 @@
 package com.sevtinge.cemiuiler.module;
 
 import com.sevtinge.cemiuiler.module.base.BaseModule;
-import com.sevtinge.cemiuiler.module.systemsettings.AddMiuiPlusEntry;
-import com.sevtinge.cemiuiler.module.systemsettings.EnableSpeedMode;
-import com.sevtinge.cemiuiler.module.systemsettings.PermissionTopOfApp;
-import com.sevtinge.cemiuiler.module.systemsettings.VoipAssistantController;
+import com.sevtinge.cemiuiler.module.systemsettings.*;
 
 public class SystemSettings extends BaseModule {
 
@@ -14,6 +11,7 @@ public class SystemSettings extends BaseModule {
         initHook(new AddMiuiPlusEntry(), mPrefsMap.getBoolean("mirror_unlock_miui_plus"));
         initHook(new EnableSpeedMode(), mPrefsMap.getBoolean("system_settings_develop_speed_mode"));
         initHook(PermissionTopOfApp.INSTANCE, mPrefsMap.getBoolean("system_settings_permission_show_app_up"));
+        initHook(new QuickInstallPermission(), mPrefsMap.getBoolean("system_settings_app_fast_install_permission"));
     }
 }
 
