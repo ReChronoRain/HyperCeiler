bdd53914891ad711d7daa4ad9a9101f93d7ba24c
绀漓丨Sevtinge <sevtinge@gmail.com>
2023-04-13 00:17:13 +0800

    回退jdk至11

--- hidden-api/build.gradle.kts
+++ hidden-api/build.gradle.kts
@@ -25,11 +25,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.majorVersion
+        jvmTarget = JavaVersion.VERSION_11.majorVersion
     }
 }
 
