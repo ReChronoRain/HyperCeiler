e800af55a47510e104e92f64b9ed1339ccfdcfc3
绀漓丨Sevtinge <sevtinge@gmail.com>
2023-04-14 12:09:48 +0800

    优化 项目结构

--- app/src/main/java/com/sevtinge/cemiuiler/module/Home.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/Home.java
@@ -46,7 +46,8 @@ public class Home extends BaseModule {
                 mPrefsMap.getInt("home_gesture_down_swipe_action", 0) > 0 ||
                 mPrefsMap.getInt("home_gesture_up_swipe2_action", 0) > 0 ||
                 mPrefsMap.getInt("home_gesture_down_swipe2_action", 0) > 0);
-        initHook(new HotSeatSwipe(), mPrefsMap.getInt("home_gesture_left_swipe_action", 0) > 0 || mPrefsMap.getInt("home_gesture_right_swipe_action", 0) > 0);
+        initHook(new HotSeatSwipe(), mPrefsMap.getInt("home_gesture_left_swipe_action", 0) > 0
+                || mPrefsMap.getInt("home_gesture_right_swipe_action", 0) > 0);
         initHook(new ShakeDevice(), mPrefsMap.getInt("home_gesture_shake_action", 0) > 0);
 
         //布局
@@ -57,13 +58,15 @@ public class Home extends BaseModule {
         initHook(new HotSeatsHeight(), mPrefsMap.getBoolean("home_layout_hotseats_height_enable"));
         initHook(new HotSeatsMarginTop(), mPrefsMap.getBoolean("home_layout_hotseats_margin_top_enable"));
         initHook(new HotSeatsMarginBottom(), mPrefsMap.getBoolean("home_layout_hotseats_margin_bottom_enable"));
-        initHook(new SearchBarMarginBottom(), (mPrefsMap.getInt("home_layout_searchbar_margin_bottom", 0) > 0) && mPrefsMap.getBoolean("home_layout_searchbar_margin_bottom_enable"));
+        initHook(new SearchBarMarginBottom(), (mPrefsMap.getInt("home_layout_searchbar_margin_bottom", 0) > 0) &&
+                mPrefsMap.getBoolean("home_layout_searchbar_margin_bottom_enable"));
 
 
         //文件夹
         initHook(FolderAutoClose.INSTANCE, mPrefsMap.getBoolean("home_folder_auto_close"));
         initHook(new FolderShade(), mPrefsMap.getStringAsInt("home_folder_shade", 1) > 0);
-        initHook(FolderColumns.INSTANCE, mPrefsMap.getInt("home_folder_columns", 3) != 3 || mPrefsMap.getBoolean("home_folder_width"));
+        initHook(FolderColumns.INSTANCE, mPrefsMap.getInt("home_folder_columns", 3) != 3 ||
+                mPrefsMap.getBoolean("home_folder_width"));
         initHook(FolderBlur.INSTANCE, mPrefsMap.getBoolean("home_folder_blur"));
         initHook(new FolderAnimation(), mPrefsMap.getBoolean("home_folder_animation"));
         initHook(new SmallFolderIconBlur(), mPrefsMap.getBoolean("home_small_folder_icon_bg"));
@@ -75,7 +78,8 @@ public class Home extends BaseModule {
         initHook(new BigFolderItemMaxCount(), mPrefsMap.getBoolean("home_big_folder_item_max_count"));
 
         //抽屉
-        initHook(AppDrawer.INSTANCE, mPrefsMap.getBoolean("home_drawer_all") && mPrefsMap.getBoolean("home_drawer_editor"));
+        initHook(AppDrawer.INSTANCE, mPrefsMap.getBoolean("home_drawer_all") &&
+                mPrefsMap.getBoolean("home_drawer_editor"));
         initHook(AllAppsContainerViewBlur.INSTANCE, mPrefsMap.getBoolean("home_drawer_blur"));
 
         //最近任务
@@ -127,7 +131,6 @@ public class Home extends BaseModule {
         initHook(new UserPresentAnimation(), mPrefsMap.getBoolean("home_other_user_present_animation"));
         initHook(new PerfectIcon(), mPrefsMap.getBoolean("home_other_perfect_icon"));
         initHook(new EnableIconMonoChrome(), mPrefsMap.getBoolean("home_other_icon_mono_chrome"));
-        //initHook(EnableIconMonetColor.INSTANCE, mPrefsMap.getBoolean("home_other_icon_monet_color"));
         initHook(new HomePortraitReverse(), mPrefsMap.getBoolean("home_other_portrait_reverse"));
         initHook(AlwaysBlurWallpaper.INSTANCE, mPrefsMap.getBoolean("home_other_always_blur_launcher_wallpaper"));
         initHook(BlurRadius.INSTANCE, mPrefsMap.getInt("home_other_blur_radius", 100) != 100);
@@ -157,32 +160,4 @@ public class Home extends BaseModule {
         initHook(SetDeviceLevel.INSTANCE, mPrefsMap.getBoolean("home_other_high_models"));
     }
 
-
-    /*public static void CleanShareMenuHook() {
-        Helpers.hookAllMethods("miui.securityspace.XSpaceResolverActivityHelper.ResolverActivityRunner", null, "run", new Helpers.MethodHook() {
-            @Override
-            protected void after(XC_MethodHook.MethodHookParam param) throws Throwable {
-                Intent mOriginalIntent = (Intent) XposedHelpers.getObjectField(param.thisObject, "mOriginalIntent");
-                if (mOriginalIntent == null) return;
-                String action = mOriginalIntent.getAction();
-                if (action == null) return;
-                if (!action.equals(Intent.ACTION_SEND) && !action.equals(Intent.ACTION_SENDTO) && !action.equals(Intent.ACTION_SEND_MULTIPLE)) return;
-                if (mOriginalIntent.getDataString() != null && mOriginalIntent.getDataString().contains(":")) return;
-
-                Context mContext = (Context)XposedHelpers.getObjectField(param.thisObject, "mContext");
-                String mAimPackageName = (String)XposedHelpers.getObjectField(param.thisObject, "mAimPackageName");
-                if (mContext == null || mAimPackageName == null) return;
-                Set<String> selectedApps = PrefsUtils.getSharedStringSetPrefs(mContext, "prefs_key_system_clean_share_apps");
-                View mRootView = (View)XposedHelpers.getObjectField(param.thisObject, "mRootView");
-                int appResId1 = mContext.getResources().getIdentifier("app1", "id", "android.miui");
-                int appResId2 = mContext.getResources().getIdentifier("app2", "id", "android.miui");
-                boolean removeOriginal = selectedApps.contains(mAimPackageName) || selectedApps.contains(mAimPackageName + "|0");
-                boolean removeDual = selectedApps.contains(mAimPackageName + "|999");
-                View originalApp = mRootView.findViewById(appResId1);
-                View dualApp = mRootView.findViewById(appResId2);
-                if (removeOriginal)dualApp.performClick();
-                else if (removeDual) originalApp.performClick();
-            }
-        });
-    }*/
 }
