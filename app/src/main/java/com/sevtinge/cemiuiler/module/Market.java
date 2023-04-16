56987763c32fba655d736621e7e2bf7e395c1294
绀漓丨Sevtinge <sevtinge@gmail.com>
2023-04-16 11:25:45 +0800

    优化 应用商店-机型伪装

--- app/src/main/java/com/sevtinge/cemiuiler/module/Market.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/Market.java
@@ -9,7 +9,7 @@ public class Market extends BaseModule {
 
     @Override
     public void handleLoadPackage() {
-        initHook(new DeviceModify(), mPrefsMap.getBoolean("market_device_modify"));
+        initHook(new DeviceModify(), mPrefsMap.getStringAsInt("market_device_modify_new", 0) != 0);
         initHook(new NewIcon(), mPrefsMap.getBoolean("market_disable_new_icon"));
     }
 }
--- app/src/main/java/com/sevtinge/cemiuiler/module/market/DeviceModify.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/market/DeviceModify.java
@@ -8,14 +8,51 @@ import de.robv.android.xposed.XposedHelpers;
 
 public class DeviceModify extends BaseHook {
 
+    String mDevice;
+    String mModel;
+    String mManufacturer;
+
     @Override
     public void init() {
+        if (mPrefsMap.getStringAsInt("market_device_modify_new", 0) == 1) {//13u
+            mDevice = "ishtar";
+            mModel = "2304FPN6DC";
+            mManufacturer = "Xiaomi";
+        } else if (mPrefsMap.getStringAsInt("market_device_modify_new", 0) == 2) {//13p
+            mDevice = "nuwa";
+            mModel = "2210132C";
+            mManufacturer = "Xiaomi";
+        } else if (mPrefsMap.getStringAsInt("market_device_modify_new", 0) == 3) {//f2
+            mDevice = "zizhan";
+            mModel = "22061218C";
+            mManufacturer = "Xiaomi";
+        } else if (mPrefsMap.getStringAsInt("market_device_modify_new", 0) == 4) {//alpha
+            mDevice = "avenger";
+            mModel = "MIX Alpha";
+            mManufacturer = "Xiaomi";
+        } else if (mPrefsMap.getStringAsInt("market_device_modify_new", 0) == 5) {//alpha
+            mDevice = "draco";
+            mModel = "MIX Alpha";
+            mManufacturer = "Xiaomi";
+        } else if (mPrefsMap.getStringAsInt("market_device_modify_new", 0) == 6) {//pad6p
+            mDevice = "liuqin";
+            mModel = "23046RP50C";
+            mManufacturer = "Xiaomi";
+        } else if (mPrefsMap.getStringAsInt("market_device_modify_new", 0) == 7) {//k60p
+            mDevice = "socrates";
+            mModel = "22127RK46C";
+            mManufacturer = "Redmi";
+        } else if (mPrefsMap.getStringAsInt("market_device_modify_new", 0) == 8) {//n12t
+            mDevice = "marble";
+            mModel = "23049RAD8C";
+            mManufacturer = "Redmi";
+        }
         findAndHookConstructor("com.xiaomi.market.MarketApp", new MethodHook() {
             @Override
             protected void before(MethodHookParam param) throws Throwable {
-                XposedHelpers.setStaticObjectField(Build.class, "DEVICE", "ishtar");
-                XposedHelpers.setStaticObjectField(Build.class, "MODEL", "2304FPN6DC");
-                XposedHelpers.setStaticObjectField(Build.class, "MANUFACTURER", "Xiaomi");
+                XposedHelpers.setStaticObjectField(Build.class, "DEVICE", mDevice);
+                XposedHelpers.setStaticObjectField(Build.class, "MODEL", mModel);
+                XposedHelpers.setStaticObjectField(Build.class, "MANUFACTURER", mManufacturer);
             }
         });
     }
