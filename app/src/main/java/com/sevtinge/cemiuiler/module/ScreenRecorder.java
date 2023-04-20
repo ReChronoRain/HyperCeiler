0f345813c737547556e8442bdfad70e5bc033b00
绀漓丨Sevtinge <sevtinge@gmail.com>
2023-04-21 03:26:02 +0800

    新增 屏幕录制-添加更多帧率与码率选项

--- app/src/main/java/com/sevtinge/cemiuiler/module/ScreenRecorder.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/ScreenRecorder.java
@@ -2,12 +2,14 @@ package com.sevtinge.cemiuiler.module;
 
 import com.sevtinge.cemiuiler.module.base.BaseModule;
 import com.sevtinge.cemiuiler.module.screenrecorder.ForceSupportPlaybackCapture;
+import com.sevtinge.cemiuiler.module.screenrecorder.ScreenRecorderConfig;
 
 public class ScreenRecorder extends BaseModule {
 
     @Override
     public void handleLoadPackage() {
         initHook(new ForceSupportPlaybackCapture(), mPrefsMap.getBoolean("screenrecorder_force_support_playback_capture"));
+        initHook(ScreenRecorderConfig.INSTANCE, mPrefsMap.getBoolean("screenrecorder_config"));
     }
 }
 
