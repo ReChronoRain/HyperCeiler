87448c8a8d402756101b54a93e084135635960d8
绀漓丨Sevtinge <sevtinge@gmail.com>
2023-04-21 04:13:07 +0800

    新增 屏幕录制-解锁更多声音来源选项

--- app/src/main/java/com/sevtinge/cemiuiler/module/ScreenRecorder.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/ScreenRecorder.java
@@ -3,12 +3,14 @@ package com.sevtinge.cemiuiler.module;
 import com.sevtinge.cemiuiler.module.base.BaseModule;
 import com.sevtinge.cemiuiler.module.screenrecorder.ForceSupportPlaybackCapture;
 import com.sevtinge.cemiuiler.module.screenrecorder.ScreenRecorderConfig;
+import com.sevtinge.cemiuiler.module.screenrecorder.UnlockMoreVolumeFrom;
 
 public class ScreenRecorder extends BaseModule {
 
     @Override
     public void handleLoadPackage() {
         initHook(new ForceSupportPlaybackCapture(), mPrefsMap.getBoolean("screenrecorder_force_support_playback_capture"));
+        initHook(new UnlockMoreVolumeFrom(), mPrefsMap.getBoolean("screenrecorder_more_volume"));
         initHook(ScreenRecorderConfig.INSTANCE, mPrefsMap.getBoolean("screenrecorder_config"));
     }
 }
