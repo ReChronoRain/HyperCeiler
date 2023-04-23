647312314e4c59c0ba37d240d44bca3a1b7aa619
绀漓丨Sevtinge <sevtinge@gmail.com>
2023-04-24 00:32:31 +0800

    优化 hook逻辑

--- app/src/main/java/com/sevtinge/cemiuiler/module/VoiceAssist.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/VoiceAssist.java
@@ -1,13 +1,13 @@
 package com.sevtinge.cemiuiler.module;
 
 import com.sevtinge.cemiuiler.module.base.BaseModule;
-import com.sevtinge.cemiuiler.module.clock.EnableHourGlass;
-import com.sevtinge.cemiuiler.module.voiceassist.UseThirdPartyBrowser;
+import com.sevtinge.cemiuiler.module.voiceassist.*;
 
 public class VoiceAssist extends BaseModule {
 
     @Override
     public void handleLoadPackage() {
+        initHook(new VoiceAssistDexKit());
         initHook(new UseThirdPartyBrowser(), mPrefsMap.getBoolean("content_extension_browser"));
     }
 }
--- app/src/main/java/com/sevtinge/cemiuiler/module/voiceassist/UseThirdPartyBrowser.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/voiceassist/UseThirdPartyBrowser.java
@@ -19,62 +19,47 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+import static com.sevtinge.cemiuiler.module.voiceassist.VoiceAssistDexKit.mVoiceAssistResultMethodsMap;
+
 public class UseThirdPartyBrowser extends BaseHook {
 
-        public void init() {
+    public void init() {
 
-            Method browserActivityWithIntent = null;
+        Method browserActivityWithIntent = null;
 
-            //XposedBridge.log("Hook到小爱同学进程！");
-            try {
-                System.loadLibrary("dexkit");
-                String apkPath = lpparam.appInfo.sourceDir;
-                try (DexKitBridge bridge = DexKitBridge.create(apkPath)) {
-                    if (bridge == null) {
-                        return;
-                    }
-                    Map<String, List<DexMethodDescriptor>> resultMap =
-                            bridge.batchFindMethodsUsingStrings(
-                                    BatchFindArgs.builder()
-                                            .addQuery("BrowserActivityWithIntent", List.of("IntentUtils", "permission click No Application can handle your intent"))
-                                            .matchType(MatchType.CONTAINS)
-                                            .build()
-                            );
-
-                    List<DexMethodDescriptor> result = Objects.requireNonNull(resultMap.get("BrowserActivityWithIntent"));
-                    for (DexMethodDescriptor descriptor : result) {
-                        browserActivityWithIntent = descriptor.getMethodInstance(lpparam.classLoader);
-                    }
-                } catch (Throwable e) {
-                    e.printStackTrace();
-                }
-                XposedBridge.log("Cemiuiler: com.miui.voiceassist browserActivityWithIntent method is "+browserActivityWithIntent);
-                //Class<?> clazz = XposedHelpers.findClass("e.D.L.pa.Wa", lpparam.classLoader);
-                XposedBridge.hookMethod(browserActivityWithIntent, new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        super.beforeHookedMethod(param);
-                        //XposedBridge.log("0)Hook到Activity启动，开始判断");
-                        Intent intent = (Intent) param.args[0];
-                        XposedBridge.log(intent.toString());
-                        try {
-                            if (intent.getPackage().equals("com.android.browser")) {
-                                XposedBridge.log("Cemiuiler: com.miui.voiceassist get browser intent");
-                                XposedBridge.log("Cemiuiler: com.miui.voiceassist get URL " + intent.getDataString());
-                                Uri uri = Uri.parse(intent.getDataString());
-                                Intent newIntent = new Intent();
-                                newIntent.setAction("android.intent.action.VIEW");
-                                newIntent.setData(uri);
-                                param.args[0] = newIntent;
-                            }
-                        } catch (Exception e) {
-                            XposedBridge.log(e);
-                        }
-                    }
-                });
-            } catch (Exception e) {
-                XposedBridge.log(e);
+        //XposedBridge.log("Hook到小爱同学进程！");
+        try {
+            List<DexMethodDescriptor> result = Objects.requireNonNull(mVoiceAssistResultMethodsMap.get("BrowserActivityWithIntent"));
+            for (DexMethodDescriptor descriptor : result) {
+                browserActivityWithIntent = descriptor.getMethodInstance(lpparam.classLoader);
             }
+        } catch (Throwable e) {
+            e.printStackTrace();
         }
+        XposedBridge.log("Cemiuiler: com.miui.voiceassist browserActivityWithIntent method is " + browserActivityWithIntent);
+        //Class<?> clazz = XposedHelpers.findClass("e.D.L.pa.Wa", lpparam.classLoader);
+        XposedBridge.hookMethod(browserActivityWithIntent, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                super.beforeHookedMethod(param);
+                //XposedBridge.log("0)Hook到Activity启动，开始判断");
+                Intent intent = (Intent) param.args[0];
+                XposedBridge.log(intent.toString());
+                try {
+                    if (intent.getPackage().equals("com.android.browser")) {
+                        XposedBridge.log("Cemiuiler: com.miui.voiceassist get browser intent");
+                        XposedBridge.log("Cemiuiler: com.miui.voiceassist get URL " + intent.getDataString());
+                        Uri uri = Uri.parse(intent.getDataString());
+                        Intent newIntent = new Intent();
+                        newIntent.setAction("android.intent.action.VIEW");
+                        newIntent.setData(uri);
+                        param.args[0] = newIntent;
+                    }
+                } catch (Exception e) {
+                    XposedBridge.log(e);
+                }
+            }
+        });
     }
+}
 
