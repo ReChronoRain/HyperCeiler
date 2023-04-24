a07a76634ea2098a0f96d529b7204ee33b6ce1f1
绀漓丨Sevtinge <sevtinge@gmail.com>
2023-04-25 02:06:33 +0800

    新增 系统桌面-文件夹-回弹动画 支持调节
    iss#28

---
+++ app/src/main/java/com/sevtinge/cemiuiler/module/home/folder/FolderAnimation.kt
@@ -0,0 +1,66 @@
+package com.sevtinge.cemiuiler.module.home.folder
+
+import com.github.kyuubiran.ezxhelper.utils.findMethod
+import com.github.kyuubiran.ezxhelper.utils.hookMethod
+import com.sevtinge.cemiuiler.module.base.BaseHook
+import com.sevtinge.cemiuiler.utils.hookAfterMethod
+import com.sevtinge.cemiuiler.utils.hookBeforeMethod
+import de.robv.android.xposed.XC_MethodHook
+import kotlin.math.abs
+
+class FolderAnimation : BaseHook() {
+    var mLauncher: Class<*>? = null
+    private var value1: Float? = null
+    private var value2: Float? = null
+    private var value3: Float? = null
+    private var value4: Float? = null
+    override fun init() {//|x-200|    50-150
+        value1 = abs(mPrefsMap.getInt("home_folder_anim_1", 90).toFloat() - 200) / 100
+        value2 = mPrefsMap.getInt("home_folder_anim_2", 30).toFloat() / 100
+        value3 = abs(mPrefsMap.getInt("home_folder_anim_3", 99).toFloat() - 200) / 100
+        value4 = mPrefsMap.getInt("home_folder_anim_4", 24).toFloat() / 100
+        val mSpringAnimator = findClassIfExists("com.miui.home.launcher.animate.SpringAnimator")
+        var hook1: XC_MethodHook.Unhook? = null
+        var hook2: XC_MethodHook.Unhook? = null
+        for (i in 47..60) {
+            val launcherClass = findClassIfExists("com.miui.home.launcher.Launcher$$i")
+            if (launcherClass != null) {
+                for (field in launcherClass.declaredFields) {
+                    if (field.name == "val\$folderInfo") {
+                        findMethod(launcherClass) {
+                            name == "run"
+                        }.hookMethod {
+                            before {
+                                hook1 = mSpringAnimator.hookBeforeMethod(
+                                    "setDampingResponse", Float::class.javaPrimitiveType, Float::class.javaPrimitiveType
+                                ) {
+                                    it.args[0] = value1
+                                    it.args[1] = value2
+                                }
+                            }
+                            after {
+                                hook1?.unhook()
+                            }
+                        }
+                        break
+                    }
+                }
+            }
+        }
+
+        "com.miui.home.launcher.Launcher".hookBeforeMethod("closeFolder", Boolean::class.java) {
+            if (it.args[0] == true) {
+                hook2 = mSpringAnimator.hookBeforeMethod(
+                    "setDampingResponse", Float::class.javaPrimitiveType, Float::class.javaPrimitiveType
+                ) { hookParam ->
+                    hookParam.args[0] = value3
+                    hookParam.args[1] = value4
+                }
+            }
+        }
+        "com.miui.home.launcher.Launcher".hookAfterMethod("closeFolder", Boolean::class.java) {
+            hook2?.unhook()
+        }
+
+    }
+}
