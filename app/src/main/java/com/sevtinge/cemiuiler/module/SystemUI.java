dbeb1a74bb0acac6db379d0a50f45ddf9e467f25
mu7220 <80700814+mu7220@users.noreply.github.com>
2023-04-27 14:54:36 +0800

    修复 上下行网速默认启用的问题

--- app/src/main/java/com/sevtinge/cemiuiler/module/SystemUI.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/SystemUI.java
@@ -49,7 +49,9 @@ public class SystemUI extends BaseModule {
 
         //网速指示器
         if (mPrefsMap.getBoolean("system_ui_statusbar_network_speed_enable_custom")) {
-            initHook(NetworkSpeed.INSTANCE);
+            if(mPrefsMap.getBoolean("system_ui_statusbar_network_speed_detailed")  || mPrefsMap.getBoolean("system_ui_statusbar_network_speed_fakedualrow")) {
+                initHook(NetworkSpeed.INSTANCE);
+            }
             initHook(NetworkSpeedUnit.INSTANCE);
             initHook(NetworkSpeedWidth.INSTANCE, mPrefsMap.getInt("system_ui_statusbar_network_speed_fixedcontent_width", 10) > 10);
         }
