2679aa2e39c1964cfe05dbf7ea3f0d00778722c0
绀漓丨Sevtinge <sevtinge@gmail.com>
2023-05-02 03:12:35 +0800

    修复 文件管理-* hook失败

--- app/src/main/java/com/sevtinge/cemiuiler/module/FileExplorer.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/FileExplorer.java
@@ -7,7 +7,6 @@ public class FileExplorer extends BaseModule {
 
     @Override
     public void handleLoadPackage() {
-        initHook(SelectName.INSTANCE, mPrefsMap.getBoolean("file_explorer_can_selectable") ||
-                mPrefsMap.getBoolean("file_explorer_is_single_line"));
+        initHook(SelectName.INSTANCE, mPrefsMap.getBoolean("file_explorer_can_selectable") || mPrefsMap.getBoolean("file_explorer_is_single_line"));
     }
 }
--- app/src/main/java/com/sevtinge/cemiuiler/module/fileexplorer/SelectName.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/fileexplorer/SelectName.kt
@@ -5,14 +5,13 @@ import com.github.kyuubiran.ezxhelper.utils.findMethod
 import com.github.kyuubiran.ezxhelper.utils.getObject
 import com.github.kyuubiran.ezxhelper.utils.hookAfter
 import com.sevtinge.cemiuiler.module.base.BaseHook
-import com.sevtinge.cemiuiler.utils.XSPUtils.getBoolean
 
 object SelectName : BaseHook() {
     override fun init() {
         findMethod("com.android.fileexplorer.view.FileListItem") { name == "onFinishInflate" }.hookAfter {
             (it.thisObject.getObject("mFileNameTextView") as TextView).apply {
-                setTextIsSelectable(getBoolean("file_explorer_can_selectable", false))
-                isSingleLine = getBoolean("file_explorer_is_single_line", false)
+                setTextIsSelectable(mPrefsMap.getBoolean("file_explorer_can_selectable"))
+                isSingleLine = mPrefsMap.getBoolean("file_explorer_is_single_line")
             }
         }
     }
