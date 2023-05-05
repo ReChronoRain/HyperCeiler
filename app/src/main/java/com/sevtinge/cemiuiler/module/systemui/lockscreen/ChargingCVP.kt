dd99a9a471f49ef68127e06ec5ad8c8b7e8a8073
YifePlayte <yife-playte@foxmail.com>
2023-05-05 18:54:37 +0800

    修复 锁屏充电显示电流、电压、功率

--- app/src/main/java/com/sevtinge/cemiuiler/module/systemui/lockscreen/ChargingCVP.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/systemui/lockscreen/ChargingCVP.kt
@@ -5,6 +5,7 @@ import android.app.AndroidAppHelper
 import android.content.Context
 import android.os.BatteryManager
 import android.widget.TextView
+import com.github.kyuubiran.ezxhelper.utils.Log
 import com.github.kyuubiran.ezxhelper.utils.findMethod
 import com.github.kyuubiran.ezxhelper.utils.hookAfter
 import com.github.kyuubiran.ezxhelper.utils.hookAllConstructorAfter
@@ -23,7 +24,9 @@ object ChargingCVP : BaseHook() {
         findMethod("com.android.keyguard.charge.ChargeUtils") {
             name == "getChargingHintText" && parameterCount == 3
         }.hookAfter {
-            it.result = it.result as String + "\n" + getCVP()
+            if (it.result != null) {
+                it.result = it.result as String + "\n" + getCVP()
+            }
         }
 
         findClassIfExists("com.android.systemui.statusbar.phone.KeyguardIndicationTextView").hookAllConstructorAfter {
@@ -32,15 +35,23 @@ object ChargingCVP : BaseHook() {
     }
 
     private fun getCVP(): String {
-        val batteryManager = AndroidAppHelper.currentApplication().getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+        val batteryManager =
+            AndroidAppHelper.currentApplication().getSystemService(Context.BATTERY_SERVICE) as BatteryManager
         val current = abs(batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW) / 1000)
-        val wirelessSupport = File("/sys/class/power_supply/wireless/signal_strength").exists()
-        val usb = BufferedReader(FileReader("/sys/class/power_supply/usb/voltage_now"))
-        val wireless = BufferedReader(FileReader("/sys/class/power_supply/wireless/voltage_now"))
-        val usbVoltage = BigDecimal(usb.readLine().toDouble() / 1000000.0).setScale(1, BigDecimal.ROUND_HALF_UP).toDouble()
-        val wirelessVoltage = if (wirelessSupport) {
-            BigDecimal(wireless.readLine().toDouble() / 1000000.0).setScale(1, BigDecimal.ROUND_HALF_UP).toDouble()
-        } else 0.0
+        var usbVoltage = 0.0
+        var wirelessVoltage = 0.0
+        kotlin.runCatching {
+            val usb = BufferedReader(FileReader("/sys/class/power_supply/usb/voltage_now"))
+            usbVoltage =
+                BigDecimal(usb.readLine().toDouble() / 1000000.0).setScale(1, BigDecimal.ROUND_HALF_UP).toDouble()
+        }
+        kotlin.runCatching {
+            val wirelessSupport = File("/sys/class/power_supply/wireless/signal_strength").exists()
+            val wireless = BufferedReader(FileReader("/sys/class/power_supply/wireless/voltage_now"))
+            wirelessVoltage = if (wirelessSupport) {
+                BigDecimal(wireless.readLine().toDouble() / 1000000.0).setScale(1, BigDecimal.ROUND_HALF_UP).toDouble()
+            } else 0.0
+        }
         val voltage = if (usbVoltage >= wirelessVoltage) usbVoltage else wirelessVoltage
         val powerAll = abs((current * voltage) / 1000f)
         val power = String.format("%.2f", powerAll)
