667c34ed17482660eabc07dcb86f02c341e61a43
mu7220 <80700814+mu7220@users.noreply.github.com>
2023-05-09 01:18:57 +0800

    新增 系统界面-状态栏-图标-双排信号栏

--- app/src/main/java/com/sevtinge/cemiuiler/module/lbe/DisableClipboardTip.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/lbe/DisableClipboardTip.kt
@@ -48,8 +48,6 @@ object DisableClipboardTip : BaseHook() {
                         val appName = getAppName(context, packageName)
                         val modRes = Helpers.getModuleRes(context)
 
-                        // 对于 mPrefsMap.getBoolean 的判定是否为 false 不会写，所以禁用剪切板读取提示暂时坏了，QAQ
-                        // 某不知名绀漓前来尝试修理
                         if (!mDisableClipboardTip) {
                             Toast.makeText(context, "$appName ${modRes.getString(R.string.lbe_clipboard_tip)}", Toast.LENGTH_SHORT)
                                 .show()
