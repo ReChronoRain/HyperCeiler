84b319eb2f110ff5c26db8f5af9ee21f11466c7c
mu7220 <80700814+mu7220@users.noreply.github.com>
2023-05-08 20:46:37 +0800

    新增 系统界面-通知与控制中心-亮屏时静音

--- app/src/main/java/com/sevtinge/cemiuiler/module/SystemUI.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/SystemUI.java
@@ -92,6 +92,7 @@ public class SystemUI extends BaseModule {
         initHook(new QSFiveGTile(), mPrefsMap.getBoolean("system_control_center_5g_tile"));
         initHook(new QSTileLabel(), mPrefsMap.getBoolean("system_control_center_qs_tile_label"));
         initHook(new QSGrid(), mPrefsMap.getInt("system_control_center_qs_rows", 4) > 4 || mPrefsMap.getInt("system_control_center_qs_columns", 4) > 4);
+        initHook(new MuteVisibleNotifications(), mPrefsMap.getBoolean("system_ui_control_center_mute_visible_notice"));
         initHook(HideMiPlayEntry.INSTANCE, mPrefsMap.getBoolean("system_ui_control_center_hide_mi_play_entry"));
         initHook(QSControlDetailBackgroundAlpha.INSTANCE, mPrefsMap.getInt("system_ui_control_center_control_detail_background_alpha", 255) != 255);
         initHook(FixMediaControlPanel.INSTANCE, mPrefsMap.getBoolean("system_ui_control_center_fix_media_control_panel"));
