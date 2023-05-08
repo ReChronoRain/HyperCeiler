41f64a091ce67bc771432f3b42eeab039a6cade4
绀漓丨Sevtinge <sevtinge@gmail.com>
2023-05-09 00:27:04 +0800

    修复 权限管理服务-禁用提示读取剪贴板

--- app/src/main/java/com/sevtinge/cemiuiler/module/lbe/DisableClipboardTip.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/lbe/DisableClipboardTip.kt
@@ -26,7 +26,7 @@ object DisableClipboardTip : BaseHook() {
                 lpparam.classLoader
             )
 
-        val mDisableClipboardTip = mPrefsMap.getBoolean("prefs_key_lbe_disable_clipboard_tip")
+        val mDisableClipboardTip = mPrefsMap.getBoolean("lbe_disable_clipboard_tip")
 
         XposedHelpers.findAndHookMethod(
             "com.lbe.security.ui.SecurityPromptHandler",
@@ -49,6 +49,7 @@ object DisableClipboardTip : BaseHook() {
                         val modRes = Helpers.getModuleRes(context)
 
                         // 对于 mPrefsMap.getBoolean 的判定是否为 false 不会写，所以禁用剪切板读取提示暂时坏了，QAQ
+                        // 某不知名绀漓前来尝试修理
                         if (!mDisableClipboardTip) {
                             Toast.makeText(context, "$appName ${modRes.getString(R.string.lbe_clipboard_tip)}", Toast.LENGTH_SHORT)
                                 .show()
