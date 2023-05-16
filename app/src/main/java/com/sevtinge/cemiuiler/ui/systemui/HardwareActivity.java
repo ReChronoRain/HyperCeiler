2734438dfa9f9f3d73496a74a80c41c0ca10322e
Neko Qiqi <80700814+mu7220@users.noreply.github.com>
2023-05-17 00:34:18 +0800

    补充提交（我是笨蛋）
    优化描述，并调整部分功能位置

---
+++ app/src/main/java/com/sevtinge/cemiuiler/ui/systemui/HardwareActivity.java
@@ -0,0 +1,24 @@
+package com.sevtinge.cemiuiler.ui.systemui; 
+  
+ import android.os.Build; 
+ import androidx.fragment.app.Fragment; 
+  
+ import com.sevtinge.cemiuiler.R; 
+ import com.sevtinge.cemiuiler.ui.base.SubFragment; 
+ import com.sevtinge.cemiuiler.ui.systemui.base.BaseSystemUIActivity; 
+  
+ public class HardwareActivity extends BaseSystemUIActivity { 
+     @Override 
+     public Fragment initFragment() { 
+         return new SystemUIHardwareFragment(); 
+     } 
+  
+     public static class SystemUIHardwareFragment extends SubFragment { 
+  
+         @Override 
+         public int getContentResId() { 
+             return R.xml.system_ui_hardware; 
+         } 
+
+     } 
+ }
