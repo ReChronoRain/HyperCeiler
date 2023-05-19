29ffe47ab9c4a9b27892f1da8abfaa950b5b2945
mu7220 <80700814+mu7220@users.noreply.github.com>
2023-05-20 01:05:50 +0800

    HideBatteryIcon 去 EzXposed，并修复部分异常问题

--- app/src/main/java/com/sevtinge/cemiuiler/module/systemui/statusbar/HideBatteryIcon.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/systemui/statusbar/HideBatteryIcon.kt
@@ -4,49 +4,57 @@ import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
-import com.github.kyuubiran.ezxhelper.utils.findMethod
-import com.github.kyuubiran.ezxhelper.utils.getObject
-import com.github.kyuubiran.ezxhelper.utils.getObjectAs
-import com.github.kyuubiran.ezxhelper.utils.hookAfter
 import com.sevtinge.cemiuiler.module.base.BaseHook
+import com.sevtinge.cemiuiler.utils.getObjectField
+import com.sevtinge.cemiuiler.utils.getObjectFieldAs
 
 object HideBatteryIcon : BaseHook() {
     override fun init() {
-        findMethod("com.android.systemui.statusbar.views.MiuiBatteryMeterView") {
-            name == "updateResources"
-        }.hookAfter {
-            //隐藏电池图标
-            if (mPrefsMap.getBoolean("system_ui_status_bar_battery_icon")) {
-                (it.thisObject.getObjectAs<ImageView>("mBatteryIconView")).visibility = View.GONE
-                if (it.thisObject.getObject("mBatteryStyle") == 1) {
-                    (it.thisObject.getObjectAs<FrameLayout>("mBatteryDigitalView")).visibility =
-                        View.GONE
+        findAndHookMethod("com.android.systemui.statusbar.views.MiuiBatteryMeterView",
+            "updateResources",
+            object : MethodHook() {
+                override fun after(param: MethodHookParam?) {
+                    //隐藏电池图标
+                    if (mPrefsMap.getBoolean("system_ui_status_bar_battery_icon")) {
+                        (param?.thisObject?.getObjectFieldAs<ImageView>("mBatteryIconView"))?.visibility =
+                            View.GONE
+                        if (param?.thisObject?.getObjectField("mBatteryStyle") == 1) {
+                            (param.thisObject?.getObjectFieldAs<FrameLayout>("mBatteryDigitalView"))?.visibility =
+                                View.GONE
+                        }
+                    }
+                    //隐藏电池百分号
+                    if (mPrefsMap.getBoolean("system_ui_status_bar_battery_percent") || mPrefsMap.getBoolean(
+                            "system_ui_status_bar_battery_percent_mark"
+                        )
+                    ) {
+                        (param?.thisObject?.getObjectFieldAs<TextView>("mBatteryPercentMarkView"))?.textSize =
+                            0F
+                    }
+                    //隐藏电池内的百分比
+                    if (mPrefsMap.getBoolean("system_ui_status_bar_battery_percent")) {
+                        (param?.thisObject?.getObjectFieldAs<TextView>("mBatteryPercentView"))?.textSize =
+                            0F
+                        (param?.thisObject?.getObjectFieldAs<TextView>("mBatteryTextDigitView"))?.textSize =
+                            0F
+                    }
                 }
             }
+        )
 
-            if (!mPrefsMap.getBoolean("system_ui_status_bar_battery_percent") || mPrefsMap.getBoolean(
-                    "system_ui_status_bar_battery_percent_mark"
-                )
-            ) { //隐藏电池百分号
-                (it.thisObject.getObjectAs<TextView>("mBatteryPercentMarkView")).textSize = 0F
-            } else if (mPrefsMap.getBoolean("system_ui_status_bar_battery_percent")) { //隐藏电池内的百分比
-                (it.thisObject.getObjectAs<TextView>("mBatteryPercentView")).textSize = 0F
-                (it.thisObject.getObjectAs<TextView>("mBatteryTextDigitView")).textSize = 0F
-                (it.thisObject.getObjectAs<TextView>("mBatteryPercentMarkView")).textSize = 0F
-            }
-        }
-
-        findMethod("com.android.systemui.statusbar.views.MiuiBatteryMeterView") {
-            name == "updateChargeAndText"
-        }.hookAfter {
-            //隐藏电池充电图标
-            if (mPrefsMap.getBoolean("system_ui_status_bar_battery_charging")) {
-                (it.thisObject.getObjectAs<ImageView>("mBatteryChargingInView")).visibility =
-                    View.GONE
-                (it.thisObject.getObjectAs<ImageView>("mBatteryChargingView")).visibility =
-                    View.GONE
-            }
-        }
+        findAndHookMethod("com.android.systemui.statusbar.views.MiuiBatteryMeterView",
+            "updateChargeAndText",
+            object : MethodHook() {
+                override fun after(param: MethodHookParam?) {
+                    //隐藏电池充电图标
+                    if (mPrefsMap.getBoolean("system_ui_status_bar_battery_charging")) {
+                        (param?.thisObject?.getObjectFieldAs<ImageView>("mBatteryChargingInView"))?.visibility =
+                            View.GONE
+                        (param?.thisObject?.getObjectFieldAs<ImageView>("mBatteryChargingView"))?.visibility =
+                            View.GONE
+                    }
+                }
+            })
     }
 
 }
