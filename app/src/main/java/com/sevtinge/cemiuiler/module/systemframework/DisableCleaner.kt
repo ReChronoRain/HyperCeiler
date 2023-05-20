3a662d46c6044e0a9b66234d274e12b1dd7b113e
nakixii <61456408+nakixii@users.noreply.github.com>
2023-05-21 07:57:13 +0800

    优化 系统框架-其他-禁止杀后台应用 (#109)

--- app/src/main/java/com/sevtinge/cemiuiler/module/systemframework/DisableCleaner.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/systemframework/DisableCleaner.kt
@@ -5,53 +5,22 @@ import com.sevtinge.cemiuiler.module.base.BaseHook
 object DisableCleaner : BaseHook() {
     override fun init() {
         hookAllMethods("com.android.server.am.ActivityManagerService", "checkExcessivePowerUsage",
-            object : MethodHook() {
-                override fun before(param: MethodHookParam?) {
-                    param?.result = null
-                }
-            }
+            object : MethodHook() { override fun before(param: MethodHookParam?) { param?.result = null }}
         )
         hookAllMethods("com.android.server.am.OomAdjuster", "shouldKillExcessiveProcesses",
-            object : MethodHook() {
-                override fun before(param: MethodHookParam?) {
-                    param?.result = false
-                }
-            }
-        )
-        hookAllMethods("com.android.server.am.OomAdjuster", "updateAndTrimProcessLSP",
-            object : MethodHook() {
-                override fun before(param: MethodHookParam?) {
-                    param?.args?.set(2, 0)
-                }
-            }
+            object : MethodHook() { override fun before(param: MethodHookParam?) { param?.result = false }}
         )
         hookAllMethods("com.android.server.am.PhantomProcessList", "trimPhantomProcessesIfNecessary",
-            object : MethodHook() {
-                override fun before(param: MethodHookParam?) {
-                    param?.result = null
-                }
-            }
+            object : MethodHook() { override fun before(param: MethodHookParam?) { param?.result = null }}
         )
         hookAllMethods("com.android.server.am.ProcessMemoryCleaner", "checkBackgroundProcCompact",
-            object : MethodHook() {
-                override fun before(param: MethodHookParam?) {
-                    param?.result = null
-                }
-            }
+            object : MethodHook() { override fun before(param: MethodHookParam?) { param?.result = null }}
         )
         hookAllMethods("com.android.server.am.SystemPressureController", "nStartPressureMonitor",
-            object : MethodHook() {
-                override fun before(param: MethodHookParam?) {
-                    param?.result = null
-                }
-            }
+            object : MethodHook() { override fun before(param: MethodHookParam?) { param?.result = null }}
         )
         hookAllMethods("com.android.server.wm.RecentTasks", "trimInactiveRecentTasks",
-            object : MethodHook() {
-                override fun before(param: MethodHookParam?) {
-                    param?.result = null
-                }
-            }
+            object : MethodHook() { override fun before(param: MethodHookParam?) { param?.result = null }}
         )
     }
 }
