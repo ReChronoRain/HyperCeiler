38e2b28ee735fac378bfd474c2ca258566e4735f
mu7220 <80700814+mu7220@users.noreply.github.com>
2023-05-21 00:19:52 +0800

    还原 BypassSignCheckForT 的提交

--- app/src/main/java/com/sevtinge/cemiuiler/module/systemframework/corepatch/BypassSignCheckForT.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/systemframework/corepatch/BypassSignCheckForT.kt
@@ -1,11 +1,20 @@
 package com.sevtinge.cemiuiler.module.systemframework.corepatch
 
+import com.github.kyuubiran.ezxhelper.utils.findMethod
+import com.github.kyuubiran.ezxhelper.utils.hookMethod
 import com.sevtinge.cemiuiler.module.base.BaseHook
 
 object BypassSignCheckForT : BaseHook() {
     override fun init() {
         try {
-            findAndHookMethod(
+            findMethod("android.util.apk.ApkSignatureVerifier") {
+                name == "getMinimumSignatureSchemeVersionForTargetSdk"
+            }.hookMethod {
+                after { param ->
+                    param.result = 1
+                }
+            }
+            /*findAndHookMethod(
                 "android.util.apk.ApkSignatureVerifier",
                 "getMinimumSignatureSchemeVersionForTargetSdk",
                 object : MethodHook() {
@@ -13,7 +22,7 @@ object BypassSignCheckForT : BaseHook() {
                         param?.result = 1
                     }
                 }
-            )
+            )*/
         } catch (e: Throwable) {
             log("hook failed by $e")
         }
