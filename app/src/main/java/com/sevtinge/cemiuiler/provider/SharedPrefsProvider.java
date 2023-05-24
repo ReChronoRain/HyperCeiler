b22c6fc35ea708376b1762a89698ceaad934ec5b
mu7220 <80700814+mu7220@users.noreply.github.com>
2023-05-24 23:59:17 +0800

    部分优化

--- app/src/main/java/com/sevtinge/cemiuiler/provider/SharedPrefsProvider.java
+++ app/src/main/java/com/sevtinge/cemiuiler/provider/SharedPrefsProvider.java
@@ -54,26 +54,25 @@ public class SharedPrefsProvider extends ContentProvider {
         MatrixCursor cursor = new MatrixCursor(new String[]{"data"});
 
         switch (uriMatcher.match(uri)) {
-            case 0: {
+            case 0 -> {
                 cursor.newRow().add("data", prefs.getString(parts.get(1), ""));
                 return cursor;
             }
-            case 1: {
+            case 1 -> {
                 cursor.newRow().add("data", prefs.getString(parts.get(1), parts.get(2)));
                 return cursor;
             }
-            case 2: {
+            case 2 -> {
                 cursor.newRow().add("data", prefs.getInt(parts.get(1), Integer.parseInt(parts.get(2))));
                 return cursor;
             }
-            case 3: {
+            case 3 -> {
                 cursor.newRow().add("data", prefs.getBoolean(parts.get(1), Integer.parseInt(parts.get(2)) == 1) ? 1 : 0);
                 return cursor;
             }
-            case 4: {
-                Set<String> strings = prefs.getStringSet(parts.get(1), new LinkedHashSet<String>());
-                if (strings != null)
-                    for (String str: strings) cursor.newRow().add("data", str);
+            case 4 -> {
+                Set<String> strings = prefs.getStringSet(parts.get(1), new LinkedHashSet<>());
+                for (String str : strings) cursor.newRow().add("data", str);
                 return cursor;
             }
         }
