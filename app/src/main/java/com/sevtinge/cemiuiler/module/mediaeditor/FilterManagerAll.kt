fcb08385ead0ad80fa05157a326cdddca877ba8d
绀漓丨Sevtinge <sevtinge@gmail.com>
2023-05-27 03:29:15 +0800

    优化 小米相册-编辑-解锁图片裁切大小限制

--- app/src/main/java/com/sevtinge/cemiuiler/module/mediaeditor/FilterManagerAll.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/mediaeditor/FilterManagerAll.kt
@@ -1,36 +1,35 @@
 package com.sevtinge.cemiuiler.module.mediaeditor
 
+import android.os.Bundle
 import com.github.kyuubiran.ezxhelper.utils.field
 import com.github.kyuubiran.ezxhelper.utils.hookBefore
 import com.github.kyuubiran.ezxhelper.utils.loadClass
 import com.sevtinge.cemiuiler.module.base.BaseHook
-import com.sevtinge.cemiuiler.utils.DexKit
-import com.sevtinge.cemiuiler.utils.DexKit.dexKitBridge
-import io.luckypray.dexkit.enums.MatchType
+import com.sevtinge.cemiuiler.module.mediaeditor.MediaEditorDexKit.mMediaEditorResultMethodsMap
+import java.lang.reflect.Method
+import java.util.Objects
 
 object FilterManagerAll : BaseHook() {
     override fun init() {
-        DexKit.hostDir = lpparam.appInfo.sourceDir
-        DexKit.loadDexKit()
         try {
-            dexKitBridge.findMethodUsingString {
-                usingString = "wayne"
-                methodReturnType = "Ljava/util/List;"
-                matchType = MatchType.FULL
-            }.single().getMethodInstance(lpparam.classLoader).hookBefore {
-                loadClass("android.os.Build").field("DEVICE", true, String::class.java)
-                    .set(null, "wayne")
+            val result = mMediaEditorResultMethodsMap["FilterManager"]!!
+            for (descriptor in result) {
+                try {
+                    //val filterManager: Method = descriptor.getMethodInstance(lpparam.classLoader)
+                    log("descriptor method is $descriptor")
+                    val filterManager = descriptor.getMethodInstance(lpparam.classLoader)
+                    log("filterManager method is $filterManager")
+                    if (filterManager.returnType == List::class.java || filterManager.returnType == Bundle::class.java) {
+                        filterManager.hookBefore {
+                            loadClass("android.os.Build").field("DEVICE", true, String::class.java)
+                                .set(null, "wayne")
+                        }
+                    }
+                } catch (_: Throwable) {
+                }
             }
-        } catch (_: Throwable) {
-            dexKitBridge.findMethodUsingString {
-                usingString = "wayne"
-                methodParamTypes = arrayOf("android.os.Bundle")
-                matchType = MatchType.FULL
-            }.single().getMethodInstance(lpparam.classLoader).hookBefore {
-                loadClass("android.os.Build").field("DEVICE", true, String::class.java)
-                    .set(null, "wayne")
-            }
+        } catch (e: Throwable) {
+            logE(e)
         }
-        DexKit.closeDexKit()
     }
 }
