a480eb1f0882d538901a6efc90918c4d6ece44de
hosizoraru <hosizoraru@gmail.com>
2023-05-27 02:16:43 +0800

    使用DexKit适配禁止小米互传10min后自动关闭并且隐藏10min后自动关闭的Toast

--- app/src/main/java/com/sevtinge/cemiuiler/module/mishare/NoAutoTurnOff.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/mishare/NoAutoTurnOff.kt
@@ -1,31 +1,47 @@
 package com.sevtinge.cemiuiler.module.mishare
 
-import com.github.kyuubiran.ezxhelper.utils.findMethod
+import android.content.Context
+import com.github.kyuubiran.ezxhelper.utils.findAllMethods
 import com.github.kyuubiran.ezxhelper.utils.hookBefore
+import com.github.kyuubiran.ezxhelper.utils.paramCount
 import com.sevtinge.cemiuiler.module.base.BaseHook
-import com.sevtinge.cemiuiler.utils.Helpers
+import com.sevtinge.cemiuiler.utils.DexKit.closeDexKit
+import com.sevtinge.cemiuiler.utils.DexKit.dexKitBridge
+import com.sevtinge.cemiuiler.utils.DexKit.hostDir
+import com.sevtinge.cemiuiler.utils.DexKit.loadDexKit
+import io.luckypray.dexkit.enums.MatchType
 
 class NoAutoTurnOff : BaseHook() {
 
     override fun init() {
-        when (val version = Helpers.getPackageVersionCode(lpparam)) {
-            21500 -> {
-                findMethod("com.miui.mishare.connectivity.MiShareService\$d\$g") {
-                    name == "b"
-                }.hookBefore {
-                    it.result = null
-                }
+        hostDir = lpparam.appInfo.sourceDir
+        loadDexKit()
+        dexKitBridge.batchFindMethodsUsingStrings {
+            addQuery("qwq", listOf("EnabledState", "mishare_enabled"))
+            matchType = MatchType.FULL
+        }.forEach { (_, classes) ->
+            classes.map {
+                it.getMethodInstance(lpparam.classLoader)
+            }.hookBefore {
+                it.result = null
             }
-
-            21600 -> {
-                findMethod("com.miui.mishare.connectivity.MiShareService\$j\$g") {
-                    name == "a"
-                }.hookBefore {
-                    it.result = null
-                }
-            }
-
-            else ->  log("Your MiShare version is $version, NoAutoTurnOff doesn't work")
         }
+
+        dexKitBridge.batchFindClassesUsingStrings {
+            addQuery("qwq", listOf("null context", "cta_agree"))
+            matchType = MatchType.FULL
+        }.forEach { (_, classes) ->
+            classes.map {
+                val qaq = it.getClassInstance(lpparam.classLoader)
+                findAllMethods(qaq) {
+                    returnType == Boolean::class.java && paramCount == 2 && parameterTypes[0] == Context::class.java && parameterTypes[1] == String::class.java
+                }.hookBefore { param ->
+                    if (param.args[1].equals("security_agree")) {
+                        param.result = false
+                    }
+                }
+            }
+        }
+        closeDexKit()
     }
 }
