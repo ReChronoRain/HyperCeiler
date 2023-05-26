50e16794acabc4ee49f317968d1d47e35908accd
hosizoraru <hosizoraru@gmail.com>
2023-05-27 02:16:13 +0800

    使用DexKit适配多版本大师滤镜

--- app/src/main/java/com/sevtinge/cemiuiler/module/mediaeditor/FilterManagerAll.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/mediaeditor/FilterManagerAll.kt
@@ -1,32 +1,35 @@
 package com.sevtinge.cemiuiler.module.mediaeditor
 
-import com.github.kyuubiran.ezxhelper.utils.Log
 import com.github.kyuubiran.ezxhelper.utils.field
-import com.github.kyuubiran.ezxhelper.utils.findMethod
 import com.github.kyuubiran.ezxhelper.utils.hookBefore
 import com.github.kyuubiran.ezxhelper.utils.loadClass
 import com.sevtinge.cemiuiler.module.base.BaseHook
-import de.robv.android.xposed.XposedBridge
+import com.sevtinge.cemiuiler.utils.DexKit
+import com.sevtinge.cemiuiler.utils.DexKit.dexKitBridge
+import io.luckypray.dexkit.enums.MatchType
 
 object FilterManagerAll : BaseHook() {
     override fun init() {
-
-        log("Rules Hook success!")
-        // 1.0.3.2.1
-//        "b6.b".hookBeforeMethod(
-//            getDefaultClassLoader(), "g"
-//        ) {
-//            val field = findField("android.os.Build") { type == String::class.java && name == "DEVICE" }
-//            it.thisObject.putObject(field, "wayne")
-//            XposedBridge.log("Cemiuiler: HookBeforeMethod Hook success!")
-//        }
-        findMethod("b6.b") {
-            name == "g"
-        }.hookBefore {
-//            param.thisObject.javaClass.field("DEVICE",true).setBoolean(param.thisObject, true)
-            loadClass("android.os.Build").field("DEVICE", true, String::class.java)
-                .set(null, "wayne")
-            Log.ix("Cemiuiler: HookBeforeMethod Hook success!")
+        DexKit.hostDir = lpparam.appInfo.sourceDir
+        DexKit.loadDexKit()
+        try {
+            dexKitBridge.findMethodUsingString {
+                usingString = "wayne"
+                methodReturnType = "Ljava/util/List;"
+                matchType = MatchType.FULL
+            }.single().getMethodInstance(lpparam.classLoader).hookBefore {
+                loadClass("android.os.Build").field("DEVICE", true, String::class.java)
+                    .set(null, "wayne")
+            }
+        } catch (_: Throwable) {
+            dexKitBridge.findMethodUsingString {
+                usingString = "wayne"
+                methodParamTypes = arrayOf("android.os.Bundle")
+                matchType = MatchType.FULL
+            }.single().getMethodInstance(lpparam.classLoader).hookBefore {
+                loadClass("android.os.Build").field("DEVICE", true, String::class.java)
+                    .set(null, "wayne")
+            }
         }
     }
 }
