e6c99f5023640a4fa7a4f19a41133695109e5dff
nakixii <61456408+nakixii@users.noreply.github.com>
2023-05-29 20:07:41 +0800

    修复 禁用签名验证逻辑 (#149)

--- app/src/main/java/com/sevtinge/cemiuiler/module/systemframework/corepatch/BypassSignCheckForT.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/systemframework/corepatch/BypassSignCheckForT.kt
@@ -7,8 +7,5 @@ object BypassSignCheckForT : BaseHook() {
         hookAllMethods("android.util.apk.ApkSignatureVerifier", "getMinimumSignatureSchemeVersionForTargetSdk",
             object : MethodHook() { override fun after(param: MethodHookParam?) { param?.result = 1 }}
         )
-        hookAllMethods("com.android.server.pm.InstallPackageHelper ", "doesSignatureMatchForPermissions",
-            object : MethodHook() { override fun before(param: MethodHookParam?) { param?.result = true }}
-        )
     }
 }
--- app/src/main/java/com/sevtinge/cemiuiler/module/systemframework/corepatch/CorePatchForT.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/systemframework/corepatch/CorePatchForT.java
@@ -10,18 +10,6 @@ import java.lang.reflect.InvocationTargetException;
 
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-import static com.sevtinge.cemiuiler.module.base.BaseHook.mPrefsMap;
-import static com.sevtinge.cemiuiler.utils.Helpers.log;
-
-import android.util.Log;
-
-import java.lang.reflect.InvocationTargetException;
-
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedBridge;
-import de.robv.android.xposed.XposedHelpers;
-import de.robv.android.xposed.callbacks.XC_LoadPackage;
-
 public class CorePatchForT extends CorePatchForSv2 {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws IllegalAccessException, InvocationTargetException, InstantiationException {
@@ -42,7 +30,7 @@ public class CorePatchForT extends CorePatchForSv2 {
                 // Don't handle PERMISSION (grant SIGNATURE permissions to pkgs with this cert)
                 // Or applications will have all privileged permissions
                 // https://cs.android.com/android/platform/superproject/+/master:frameworks/base/core/java/android/content/pm/PackageParser.java;l=5947?q=CertCapabilities
-                if (((Integer) param.args[1] != 4) && prefs.getBoolean("prefs_key_system_framework_core_patch_digest_creak", true)) {
+                if (prefs.getBoolean("prefs_key_system_framework_core_patch_digest_creak", true)) {
                     param.setResult(true);
                 }
             }
@@ -66,8 +54,7 @@ public class CorePatchForT extends CorePatchForSv2 {
             }
         });
 
-
-        if (prefs.getBoolean("prefs_key_system_framework_core_patch_digest_creak", true) && prefs.getBoolean("prefs_key_system_framework_core_patch_use_pre_signature", false)) {
+        if (prefs.getBoolean("prefs_key_system_framework_core_patch_digest_creak", true)) {
             findAndHookMethod("com.android.server.pm.InstallPackageHelper", loadPackageParam.classLoader, "doesSignatureMatchForPermissions", String.class, "com.android.server.pm.parsing.pkg.ParsedPackage", int.class, new XC_MethodHook() {
                 @Override
                 protected void afterHookedMethod(MethodHookParam param) {
@@ -81,14 +68,16 @@ public class CorePatchForT extends CorePatchForSv2 {
                 }
             });
         }
+
         findAndHookMethod("com.android.server.pm.ScanPackageUtils", loadPackageParam.classLoader, "assertMinSignatureSchemeIsValid", "com.android.server.pm.parsing.pkg.AndroidPackage", int.class, new XC_MethodHook() {
             @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+            protected void afterHookedMethod(MethodHookParam param) {
                 if (prefs.getBoolean("prefs_key_system_framework_core_patch_auth_creak", true)) {
                     param.setResult(null);
                 }
             }
         });
+
         Class<?> strictJarVerifier = findClass("android.util.jar.StrictJarVerifier", loadPackageParam.classLoader);
         if (strictJarVerifier != null) {
             XposedBridge.hookAllConstructors(strictJarVerifier, new XC_MethodHook() {
