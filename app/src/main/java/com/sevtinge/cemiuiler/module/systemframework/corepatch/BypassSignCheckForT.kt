02694f9c514b02be5f91bb9376f9e031bed6f83f
nakixii <nakixii@gmail.com>
2023-05-29 18:39:19 +0800

    修复 禁用签名验证有时失效

--- app/src/main/java/com/sevtinge/cemiuiler/module/systemframework/corepatch/BypassSignCheckForT.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/systemframework/corepatch/BypassSignCheckForT.kt
@@ -1,32 +1,14 @@
 package com.sevtinge.cemiuiler.module.systemframework.corepatch
 
-import com.github.kyuubiran.ezxhelper.utils.findMethod
-import com.github.kyuubiran.ezxhelper.utils.hookMethod
 import com.sevtinge.cemiuiler.module.base.BaseHook
 
 object BypassSignCheckForT : BaseHook() {
     override fun init() {
-        try {
-            findMethod("android.util.apk.ApkSignatureVerifier") {
-                name == "getMinimumSignatureSchemeVersionForTargetSdk"
-            }.hookMethod {
-                after { param ->
-                    param.result = 1
-                }
-            }
-            /*findAndHookMethod(
-                "android.util.apk.ApkSignatureVerifier",
-                "getMinimumSignatureSchemeVersionForTargetSdk",
-                object : MethodHook() {
-                    override fun after(param: MethodHookParam?) {
-                        param?.result = 1
-                    }
-                }
-            )*/
-        } catch (e: Throwable) {
-            logE(e)
-        }
-
+        hookAllMethods("android.util.apk.ApkSignatureVerifier", "getMinimumSignatureSchemeVersionForTargetSdk",
+            object : MethodHook() { override fun after(param: MethodHookParam?) { param?.result = 1 }}
+        )
+        hookAllMethods("com.android.server.pm.InstallPackageHelper ", "doesSignatureMatchForPermissions",
+            object : MethodHook() { override fun before(param: MethodHookParam?) { param?.result = true }}
+        )
     }
-
 }
