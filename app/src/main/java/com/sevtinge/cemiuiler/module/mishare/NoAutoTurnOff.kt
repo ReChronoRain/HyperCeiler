fe728b707a622494988bd4304c86dcc5780bbc49
mu7220 <80700814+mu7220@users.noreply.github.com>
2023-06-01 16:56:42 +0800

    修修补补，给 Release 发包用

--- app/src/main/java/com/sevtinge/cemiuiler/module/mishare/NoAutoTurnOff.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/mishare/NoAutoTurnOff.kt
@@ -1,44 +1,47 @@
 package com.sevtinge.cemiuiler.module.mishare
 
+import android.content.Context
+import com.github.kyuubiran.ezxhelper.utils.findAllMethods
 import com.github.kyuubiran.ezxhelper.utils.hookBefore
+import com.github.kyuubiran.ezxhelper.utils.paramCount
 import com.sevtinge.cemiuiler.module.base.BaseHook
-import com.sevtinge.cemiuiler.module.mishare.MiShareDexKit.mMiShareResultMethodsMap
+import com.sevtinge.cemiuiler.utils.DexKit.closeDexKit
+import com.sevtinge.cemiuiler.utils.DexKit.dexKitBridge
+import com.sevtinge.cemiuiler.utils.DexKit.hostDir
+import com.sevtinge.cemiuiler.utils.DexKit.loadDexKit
+import io.luckypray.dexkit.enums.MatchType
 
 class NoAutoTurnOff : BaseHook() {
-
     override fun init() {
-        try {
-            val result = mMiShareResultMethodsMap["qwq"]!!
-            for (descriptor in result) {
-                try {
-                    log("descriptor method is $descriptor")
-                    val autoOff = descriptor.getMethodInstance(lpparam.classLoader)
-                    log("autoOff method is $autoOff")
-                    autoOff.hookBefore {
-                        it.result = null
+        // 暂时先这样发版，等后面再改
+        hostDir = lpparam.appInfo.sourceDir
+        loadDexKit()
+        dexKitBridge.batchFindMethodsUsingStrings {
+            addQuery("qwq", listOf("EnabledState", "mishare_enabled"))
+            matchType = MatchType.FULL
+        }.forEach { (_, classes) ->
+            classes.map {
+                it.getMethodInstance(lpparam.classLoader)
+            }.hookBefore {
+                it.result = null
+            }
+        }
+
+        dexKitBridge.batchFindClassesUsingStrings {
+            addQuery("qwq", listOf("null context", "cta_agree"))
+            matchType = MatchType.FULL
+        }.forEach { (_, classes) ->
+            classes.map {
+                val qaq = it.getClassInstance(lpparam.classLoader)
+                findAllMethods(qaq) {
+                    returnType == Boolean::class.java && paramCount == 2 && parameterTypes[0] == Context::class.java && parameterTypes[1] == String::class.java
+                }.hookBefore { param ->
+                    if (param.args[1].equals("security_agree")) {
+                        param.result = false
                     }
-                } catch (_: Throwable) {
                 }
             }
-        } catch (e: Throwable) {
-            logE(e)
         }
-        try {
-            val result = mMiShareResultMethodsMap["qwq2"]!!
-            for (descriptor in result) {
-                try {
-                    val securityAgree = descriptor.getMethodInstance(lpparam.classLoader)
-                    log("securityAgree method is $securityAgree")
-                    securityAgree.hookBefore { param ->
-                        if (param.args[1].equals("security_agree")) {
-                            param.result = false
-                        }
-                    }
-                } catch (_: Throwable) {
-                }
-            }
-        } catch (e: Throwable) {
-            logE(e)
-        }
+        closeDexKit()
     }
 }
