58595662ee08d66035de8afd40e650aef53f88f2
柚稚的孩纸 <30484319+zjw2017@users.noreply.github.com>
2023-06-07 23:22:36 +0800

    新增 设置-连接与共享-新版 NFC 页面 (#168)

--- app/src/main/java/com/sevtinge/cemiuiler/module/SystemSettings.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/SystemSettings.java
@@ -7,6 +7,7 @@ import com.sevtinge.cemiuiler.module.miinput.UnlockKnuckleFunction;
 import com.sevtinge.cemiuiler.module.systemsettings.AddMiuiPlusEntry;
 import com.sevtinge.cemiuiler.module.systemsettings.EnableSpeedMode;
 import com.sevtinge.cemiuiler.module.systemsettings.InternationalBuild;
+import com.sevtinge.cemiuiler.module.systemsettings.NewNFCPage;
 import com.sevtinge.cemiuiler.module.systemsettings.PermissionTopOfApp;
 import com.sevtinge.cemiuiler.module.systemsettings.QuickInstallPermission;
 import com.sevtinge.cemiuiler.module.systemsettings.UnLockAreaScreenshot;
@@ -27,6 +28,7 @@ public class SystemSettings extends BaseModule {
         initHook(PermissionTopOfApp.INSTANCE, mPrefsMap.getBoolean("system_settings_permission_show_app_up"));
         initHook(new QuickInstallPermission(), mPrefsMap.getBoolean("system_settings_permission_unknown_origin_app"));
         initHook(new InternationalBuild(), mPrefsMap.getBoolean("system_settings_international_build"));
+        initHook(new NewNFCPage(), mPrefsMap.getBoolean("system_settings_new_nfc_page"));
 
         initHook(new UnlockSuperResolution(), mPrefsMap.getBoolean("system_settings_ai_image_unlock_sr"));
         initHook(new UnlockAi(), mPrefsMap.getBoolean("system_settings_ai_image_unlock_ai"));
