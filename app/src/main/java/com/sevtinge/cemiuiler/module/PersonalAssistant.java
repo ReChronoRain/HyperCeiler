7c17b547e960983fd1930270fc969f7456b1cc54
绀漓丨Sevtinge <sevtinge@gmail.com>
2023-06-08 17:55:18 +0800

    优化 智能助理-* hook逻辑

--- app/src/main/java/com/sevtinge/cemiuiler/module/PersonalAssistant.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/PersonalAssistant.java
@@ -1,9 +1,11 @@
 package com.sevtinge.cemiuiler.module;
 
 import com.sevtinge.cemiuiler.module.base.BaseModule;
+import com.sevtinge.cemiuiler.module.packageinstaller.PackageInstallerDexKit;
 import com.sevtinge.cemiuiler.module.personalassistant.BlurOverlay;
 import com.sevtinge.cemiuiler.module.personalassistant.BlurPersonalAssistant;
 import com.sevtinge.cemiuiler.module.personalassistant.EnableFoldWidget;
+import com.sevtinge.cemiuiler.module.personalassistant.PersonalAssistantDexKit;
 import com.sevtinge.cemiuiler.module.personalassistant.WidgetCrack;
 
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
@@ -12,6 +14,7 @@ public class PersonalAssistant extends BaseModule {
 
     @Override
     public void handleLoadPackage() {
+        initHook(new PersonalAssistantDexKit());
         initHook(new BlurOverlay(), false);
         initHook(new EnableFoldWidget(), mPrefsMap.getBoolean("personal_assistant_fold_widget_enable"));
         initHook(BlurPersonalAssistant.INSTANCE, mPrefsMap.getBoolean("pa_enable"));
--- app/src/main/java/com/sevtinge/cemiuiler/module/personalassistant/BlurPersonalAssistant.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/personalassistant/BlurPersonalAssistant.kt
@@ -5,9 +5,11 @@ import android.os.Build
 import android.view.Window
 import androidx.annotation.RequiresApi
 import com.sevtinge.cemiuiler.module.base.BaseHook
+import com.sevtinge.cemiuiler.module.personalassistant.PersonalAssistantDexKit.mPersonalAssistantResultMethodsMap
+import com.sevtinge.cemiuiler.utils.Helpers
 import com.sevtinge.cemiuiler.utils.HookUtils
 import de.robv.android.xposed.XC_MethodHook
-import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.XposedBridge
 import kotlin.math.abs
 
 object BlurPersonalAssistant : BaseHook() {
@@ -19,9 +21,49 @@ object BlurPersonalAssistant : BaseHook() {
             "com.miui.personalassistant.core.overlay.AssistantOverlayWindow"
         ) ?: return
 
+        val appVersionName = Helpers.getPackageVersionName(lpparam)
+
         var lastBlurRadius = -1
 
-        XposedHelpers.findAndHookMethod(
+        val mScrollStateManager = mPersonalAssistantResultMethodsMap["ScrollStateManager"]!!
+        for (descriptor in mScrollStateManager) {
+            try {
+                val mScrollStateManagerMethod = descriptor.getMethodInstance(lpparam.classLoader)
+                log("mScrollStateManager method is $mScrollStateManagerMethod")
+                XposedBridge.hookMethod(
+                    mScrollStateManagerMethod,
+                    object : XC_MethodHook() {
+                        @RequiresApi(Build.VERSION_CODES.S)
+                        override fun afterHookedMethod(param: MethodHookParam) {
+                            val scrollX = param.args[0] as Float
+                            val window: Any?
+                            window = if(appVersionName.contains("FOR-DESIGNER")) {
+                                log("is designer ver.")
+                                HookUtils.getValueByField(param.thisObject, "e") ?: return
+                            } else {
+                                HookUtils.getValueByField(param.thisObject, "b") ?: return
+                            }
+                            if (window.javaClass.name.contains("Window")) {
+                                try {
+                                    window as Window
+                                    val blurRadius = (scrollX * blurRadius).toInt()
+                                    if (abs(blurRadius - lastBlurRadius) > 2) {
+                                        window.setBackgroundBlurRadius(blurRadius)
+                                        lastBlurRadius = blurRadius
+                                    }
+                                    val backgroundColorDrawable = ColorDrawable(backgroundColor)
+                                    backgroundColorDrawable.alpha = (scrollX * 255).toInt()
+                                    window.setBackgroundDrawable(backgroundColorDrawable)
+                                } catch (_: Throwable) {
+                                }
+                            }
+                        }
+                    })
+            } catch (_: Throwable) {
+            }
+        }
+
+        /*XposedHelpers.findAndHookMethod(
             AssistantOverlayWindowClass,
             "a",
             Float::class.java,
@@ -48,5 +90,7 @@ object BlurPersonalAssistant : BaseHook() {
                     }
                 }
             })
+
+         */
     }
 }
