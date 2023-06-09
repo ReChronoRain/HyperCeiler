7507a07f2bbf5111c23b5f45cf6ab539643f6d47
绀漓丨Sevtinge <sevtinge@gmail.com>
2023-06-09 14:17:46 +0800

    修复 系统桌面-图标与标题-下载动画 失效

--- app/src/main/java/com/sevtinge/cemiuiler/module/home/title/DownloadAnimation.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/home/title/DownloadAnimation.java
@@ -5,11 +5,26 @@ import com.sevtinge.cemiuiler.module.base.BaseHook;
 public class DownloadAnimation extends BaseHook {
     @Override
     public void init() {
-        hookAllMethods("com.miui.home.launcher.common.CpuLevelUtils", "needMamlDownload", new MethodHook() {
-            @Override
-            protected void before(MethodHookParam param) throws Throwable {
-                param.setResult(true);
-            }
-        });
+        try{
+            hookAllMethods("com.miui.home.launcher.common.DeviceLevelUtils", "needMamlProgressIcon", new MethodHook() {
+                @Override
+                protected void before(MethodHookParam param) throws Throwable {
+                    param.setResult(true);
+                }
+            });
+            hookAllMethods("com.miui.home.launcher.common.DeviceLevelUtils", "needRemoveDownloadAnimationDevice", new MethodHook() {
+                @Override
+                protected void before(MethodHookParam param) throws Throwable {
+                    param.setResult(false);
+                }
+            });
+        } catch (Exception e) {
+            hookAllMethods("com.miui.home.launcher.common.CpuLevelUtils", "needMamlDownload", new MethodHook() {
+                @Override
+                protected void before(MethodHookParam param) throws Throwable {
+                    param.setResult(true);
+                }
+            });
+        }
     }
 }
