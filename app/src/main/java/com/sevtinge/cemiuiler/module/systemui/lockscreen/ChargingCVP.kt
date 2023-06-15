fc3b4687817573f2b45f9609897d0e7ad21581b9
mu7220 <80700814+mu7220@users.noreply.github.com>
2023-06-16 02:05:25 +0800

    新增 系统界面-锁屏-充电底部显示充电信息-显示刷新间隔（感谢 yife

--- app/src/main/java/com/sevtinge/cemiuiler/module/systemui/lockscreen/ChargingCVP.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/systemui/lockscreen/ChargingCVP.kt
@@ -2,13 +2,20 @@ package com.sevtinge.cemiuiler.module.systemui.lockscreen
 
 import android.annotation.SuppressLint
 import android.app.AndroidAppHelper
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.BatteryManager
+import android.os.Handler
+import android.os.PowerManager
 import android.widget.TextView
+import com.github.kyuubiran.ezxhelper.ClassUtils.invokeStaticMethodBestMatch
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClassOrNull
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHooks
+import com.github.kyuubiran.ezxhelper.ObjectUtils.invokeMethodBestMatch
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 import com.sevtinge.cemiuiler.module.base.BaseHook
 import java.io.BufferedReader
@@ -18,26 +25,58 @@ import java.math.RoundingMode
 import kotlin.math.abs
 
 object ChargingCVP : BaseHook() {
-
     @SuppressLint("SetTextI18n")
     override fun init() {
+        val clazzDependency = loadClass("com.android.systemui.Dependency")
+        val clazzKeyguardIndicationController = loadClass("com.android.systemui.statusbar.KeyguardIndicationController")
+        loadClassOrNull("com.android.systemui.statusbar.phone.KeyguardIndicationTextView")?.constructors?.createHooks {
+            after { param ->
+                (param.thisObject as TextView).isSingleLine = false
+                val screenOnOffReceiver = @SuppressLint("ServiceCast")
+                object : BroadcastReceiver() {
+                    val keyguardIndicationController = invokeStaticMethodBestMatch(
+                        clazzDependency, "get", null, clazzKeyguardIndicationController
+                    )!!
+                    val handler = Handler((param.thisObject as TextView).context.mainLooper)
+                    val runnable = object : Runnable {
+                        override fun run() {
+                            invokeMethodBestMatch(keyguardIndicationController, "updatePowerIndication")
+                            handler.postDelayed(this, mPrefsMap.getInt("system_ui_statusbar_lock_screen_show_spacing", 3) * 1000L)
+                        }
+                    }
 
-        loadClass("com.android.keyguard.charge.ChargeUtils")
-            .methodFinder()
-            .filterByName("getChargingHintText")
-            .filterByParamCount(3)
-            .first().createHook {
-                after {
-                    if (it.result != null) {
-                        it.result = it.result as String + "\n" + getCVP()
+                    init {
+                        if (((param.thisObject as TextView).context.getSystemService(Context.POWER_SERVICE) as PowerManager).isInteractive) {
+                            handler.post(runnable)
+                        }
+                    }
+
+                    override fun onReceive(context: Context, intent: Intent) {
+                        when (intent.action) {
+                            Intent.ACTION_SCREEN_ON -> {
+                                handler.post(runnable)
+                            }
+
+                            Intent.ACTION_SCREEN_OFF -> {
+                                handler.removeCallbacks(runnable)
+                            }
+                        }
                     }
                 }
-            }
 
-        loadClassOrNull("com.android.systemui.statusbar.phone.KeyguardIndicationTextView")?.constructors?.createHooks {
-            after {
-                (it.thisObject as TextView).isSingleLine = false
+                val filter = IntentFilter().apply {
+                    addAction(Intent.ACTION_SCREEN_ON)
+                    addAction(Intent.ACTION_SCREEN_OFF)
+                }
+                (param.thisObject as TextView).context.registerReceiver(screenOnOffReceiver, filter)
             }
+        }
+        loadClass("com.android.keyguard.charge.ChargeUtils").methodFinder().filterByName("getChargingHintText")
+            .filterByParamTypes(Context::class.java, Boolean::class.javaPrimitiveType, Int::class.javaPrimitiveType)
+            .first().createHook {
+                after { param ->
+                    param.result = param.result?.let { "$it\n${getCVP()}" }
+                }
         }
     }
 
