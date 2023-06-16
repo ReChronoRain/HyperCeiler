59a935d92a84ebb1450bfde627b67b41c5cf4445
mu7220 <80700814+mu7220@users.noreply.github.com>
2023-06-17 01:59:06 +0800

    补全搜索范围

--- app/src/main/java/com/sevtinge/cemiuiler/utils/SearchHelper.java
+++ app/src/main/java/com/sevtinge/cemiuiler/utils/SearchHelper.java
@@ -1,5 +1,7 @@
 package com.sevtinge.cemiuiler.utils;
 
+import static com.sevtinge.cemiuiler.utils.api.VoyagerApisKt.isPad;
+
 import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
@@ -113,6 +115,250 @@ public class SearchHelper {
             R.string.system_ui,
             R.string.system_ui_other_title,
             "com.sevtinge.cemiuiler.ui.fragment.systemui.SystemUIOtherSettings");
+
+        // 系统桌面相关
+        parsePrefXml(context, R.xml.home_gesture,
+            R.string.home,
+            R.string.home_gesture,
+            "com.sevtinge.cemiuiler.ui.fragment.home.HomeGestureSettings");
+
+        parsePrefXml(context, R.xml.home_layout,
+            R.string.home,
+            R.string.home_layout,
+            "com.sevtinge.cemiuiler.ui.fragment.home.HomeLayoutSettings");
+
+        parsePrefXml(context, R.xml.home_folder,
+            R.string.home,
+            R.string.home_folder,
+            "com.sevtinge.cemiuiler.ui.fragment.home.HomeFolderSettings");
+
+        parsePrefXml(context, R.xml.home_drawer,
+            R.string.home,
+            R.string.home_drawer,
+            "com.sevtinge.cemiuiler.ui.fragment.home.HomeDrawerSettings");
+
+        parsePrefXml(context, R.xml.home_title,
+            R.string.home,
+            R.string.home_title,
+            "com.sevtinge.cemiuiler.ui.fragment.home.HomeTitleSettings");
+
+        parsePrefXml(context, R.xml.home_recent,
+            R.string.home,
+            R.string.home_recent,
+            "com.sevtinge.cemiuiler.ui.fragment.home.HomeRecentSettings");
+
+        parsePrefXml(context, R.xml.home_widget,
+            R.string.home,
+            R.string.home_widget,
+            "com.sevtinge.cemiuiler.ui.fragment.home.HomeWidgetSettings");
+
+        parsePrefXml(context, R.xml.home_dock,
+            R.string.home,
+            R.string.home_dock,
+            "com.sevtinge.cemiuiler.ui.fragment.home.HomeDockSettings");
+
+        parsePrefXml(context, R.xml.home_other,
+            R.string.home,
+            R.string.home_other,
+            "com.sevtinge.cemiuiler.ui.fragment.home.HomeOtherSettings");
+
+        // 设置相关
+        parsePrefXml(context, R.xml.system_settings,
+            R.string.system_settings,
+            R.string.system_settings,
+            "com.sevtinge.cemiuiler.ui.fragment.SystemSettingsFragment");
+
+        // 其他杂项
+        parsePrefXml(context, R.xml.browser,
+            R.string.browser,
+            R.string.browser,
+            "com.sevtinge.cemiuiler.ui.fragment.BrowserFragment");
+
+        parsePrefXml(context, R.xml.camera,
+            R.string.camera,
+            R.string.camera,
+            "com.sevtinge.cemiuiler.ui.fragment.CameraFragment");
+
+        parsePrefXml(context, R.xml.clock,
+            R.string.clock,
+            R.string.clock,
+            "com.sevtinge.cemiuiler.ui.fragment.ClockFragment");
+
+        parsePrefXml(context, R.xml.fileexplorer,
+            R.string.fileexplorer,
+            R.string.fileexplorer,
+            "com.sevtinge.cemiuiler.ui.fragment.FileExplorerFragment");
+
+        parsePrefXml(context, R.xml.incallui,
+            R.string.incallui,
+            R.string.incallui,
+            "com.sevtinge.cemiuiler.ui.fragment.InCallUiFragment");
+
+        parsePrefXml(context, R.xml.mms,
+            R.string.mms,
+            R.string.mms,
+            "com.sevtinge.cemiuiler.ui.fragment.MmsFragment");
+
+        parsePrefXml(context, R.xml.phone,
+            R.string.phone,
+            R.string.phone,
+            "com.sevtinge.cemiuiler.ui.fragment.PhoneFragment");
+
+        parsePrefXml(context, R.xml.downloads,
+            R.string.downloads,
+            R.string.downloads,
+            "com.sevtinge.cemiuiler.ui.fragment.DownloadsFragment");
+
+        parsePrefXml(context, R.xml.updater,
+            R.string.updater,
+            R.string.updater,
+            "com.sevtinge.cemiuiler.ui.fragment.UpdaterFragment");
+
+        parsePrefXml(context, R.xml.lbe_security,
+            R.string.lbe,
+            R.string.lbe,
+            "com.sevtinge.cemiuiler.ui.fragment.LbeFragment");
+
+        parsePrefXml(context, R.xml.milink,
+            R.string.milink,
+            R.string.milink,
+            "com.sevtinge.cemiuiler.ui.fragment.MiLinkFragment");
+
+        parsePrefXml(context, R.xml.aod,
+            R.string.aod,
+            R.string.aod,
+            "com.sevtinge.cemiuiler.ui.fragment.AodFragment");
+
+        parsePrefXml(context, R.xml.content_extension,
+            R.string.content_extension,
+            R.string.content_extension,
+            "com.sevtinge.cemiuiler.ui.fragment.ContentExtensionFragment");
+
+        parsePrefXml(context, R.xml.gallery,
+            R.string.gallery,
+            R.string.gallery,
+            "com.sevtinge.cemiuiler.ui.fragment.GalleryFragment");
+
+        parsePrefXml(context, R.xml.guard_provider,
+            R.string.guard_provider,
+            R.string.guard_provider,
+            "com.sevtinge.cemiuiler.ui.fragment.GuardProviderFragment");
+
+        parsePrefXml(context, R.xml.mediaeditor,
+            R.string.mediaeditor,
+            R.string.mediaeditor,
+            "com.sevtinge.cemiuiler.ui.fragment.MediaEditorFragment");
+
+        parsePrefXml(context, R.xml.mishare,
+            R.string.mishare,
+            R.string.mishare,
+            "com.sevtinge.cemiuiler.ui.fragment.MiShareFragment");
+
+        parsePrefXml(context, R.xml.miwallpaper,
+            R.string.miwallpaper,
+            R.string.miwallpaper,
+            "com.sevtinge.cemiuiler.ui.fragment.MiWallpaperFragment");
+
+        parsePrefXml(context, R.xml.package_installer,
+            R.string.package_installer,
+            R.string.package_installer,
+            "com.sevtinge.cemiuiler.ui.fragment.MiuiPackageInstallerFragment");
+
+        parsePrefXml(context, R.xml.music,
+            R.string.music,
+            R.string.music,
+            "com.sevtinge.cemiuiler.ui.fragment.MusicFragment");
+
+        parsePrefXml(context, R.xml.powerkeeper,
+            R.string.powerkeeper,
+            R.string.powerkeeper,
+            "com.sevtinge.cemiuiler.ui.fragment.PowerKeeperFragment");
+
+        parsePrefXml(context, R.xml.screenrecorder,
+            R.string.screenrecorder,
+            R.string.screenrecorder,
+            "com.sevtinge.cemiuiler.ui.fragment.ScreenRecorderFragment");
+
+        parsePrefXml(context, R.xml.screenshot,
+            R.string.screenshot,
+            R.string.screenshot,
+            "com.sevtinge.cemiuiler.ui.fragment.ScreenShotFragment");
+
+        parsePrefXml(context, R.xml.security_center,
+            !isPad() ? R.string.security_center : R.string.security_center_pad,
+            !isPad() ? R.string.security_center : R.string.security_center_pad,
+            "com.sevtinge.cemiuiler.ui.fragment.SecurityCenterFragment");
+
+        parsePrefXml(context, R.xml.tsmclient,
+            R.string.tsmclient,
+            R.string.tsmclient,
+            "com.sevtinge.cemiuiler.ui.fragment.TsmClientFragment");
+
+        parsePrefXml(context, R.xml.weather,
+            R.string.weather,
+            R.string.weather,
+            "com.sevtinge.cemiuiler.ui.fragment.WeatherFragment");
+
+        parsePrefXml(context, R.xml.aiasst,
+            R.string.aiasst,
+            R.string.aiasst,
+            "com.sevtinge.cemiuiler.ui.fragment.AiAsstFragment");
+
+        parsePrefXml(context, R.xml.tsmclient,
+            R.string.tsmclient,
+            R.string.tsmclient,
+            "com.sevtinge.cemiuiler.ui.fragment.TsmClientFragment");
+
+        parsePrefXml(context, R.xml.aireco,
+            R.string.aireco,
+            R.string.aireco,
+            "com.sevtinge.cemiuiler.ui.fragment.AirecoFragment");
+
+        parsePrefXml(context, R.xml.barrage,
+            R.string.barrage,
+            R.string.barrage,
+            "com.sevtinge.cemiuiler.ui.fragment.BarrageFragment");
+
+        parsePrefXml(context, R.xml.joyose,
+            R.string.joyose,
+            R.string.joyose,
+            "com.sevtinge.cemiuiler.ui.fragment.JoyoseFragment");
+
+        parsePrefXml(context, R.xml.market,
+            R.string.market,
+            R.string.market,
+            "com.sevtinge.cemiuiler.ui.fragment.MarketFragment");
+
+        parsePrefXml(context, R.xml.mirror,
+            R.string.mirror,
+            R.string.mirror,
+            "com.sevtinge.cemiuiler.ui.fragment.MirrorFragment");
+
+        parsePrefXml(context, R.xml.mtb,
+            R.string.mtb,
+            R.string.mtb,
+            "com.sevtinge.cemiuiler.ui.fragment.MtbFragment");
+
+        parsePrefXml(context, R.xml.scanner,
+            R.string.scanner,
+            R.string.scanner,
+            "com.sevtinge.cemiuiler.ui.fragment.ScannerFragment");
+
+        parsePrefXml(context, R.xml.various,
+            R.string.various,
+            R.string.various,
+            "com.sevtinge.cemiuiler.ui.fragment.VariousFragment");
+
+        // 实验性
+        /*parsePrefXml(context, R.xml.theme_manager,
+            R.string.theme_manager,
+            R.string.theme_manager,
+            "com.sevtinge.cemiuiler.ui.fragment.ThemeManagerFragment");
+
+        parsePrefXml(context, R.xml.personal_assistant,
+            R.string.personal_assistant,
+            R.string.personal_assistant,
+            "com.sevtinge.cemiuiler.ui.fragment.PersonalAssistantFragment");*/
     }
 
     private static void parsePrefXml(Context context, int xmlResId, int catResId, int catSubResId, String catPrefsFragment) {
@@ -126,7 +372,11 @@ public class SearchHelper {
                         ModData modData = new ModData();
                         modData.title = getModTitle(res, xml.getAttributeValue(ANDROID_NS, "title"));
                         if (!TextUtils.isEmpty(modData.title)) {
-                            modData.breadcrumbs = res.getString(catResId) + "/" + res.getString(catSubResId);
+                            if (!res.getString(catResId).equals(res.getString(catSubResId))) {
+                                modData.breadcrumbs = res.getString(catResId) + "/" + res.getString(catSubResId);
+                            } else {
+                                modData.breadcrumbs = res.getString(catResId);
+                            }
                             modData.key = xml.getAttributeValue(ANDROID_NS, "key");
                             modData.order = order;
                             modData.catTitleResId = catSubResId;
