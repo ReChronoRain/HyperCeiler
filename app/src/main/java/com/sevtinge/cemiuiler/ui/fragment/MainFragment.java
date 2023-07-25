9c1bbc46c2a74c345c093e8a40ab68797b4efab0
绀漓丨Sevtinge <sevtinge@gmail.com>
2023-07-26 06:08:41 +0800

    Update Tips

--- app/src/main/java/com/sevtinge/cemiuiler/ui/fragment/MainFragment.java
+++ app/src/main/java/com/sevtinge/cemiuiler/ui/fragment/MainFragment.java
@@ -42,22 +42,22 @@ public class MainFragment extends SettingsPreferenceFragment {
         mSecurityCenter.setVisible(!isPad());
         mSecurityCenterPad.setVisible(isPad());
 
-        if (randomTip == 1) mTip.setSummary(getString(R.string.tip_a) + " " + getString(R.string.tip_1));
-        else if (randomTip == 2) mTip.setSummary(getString(R.string.tip_a) + " " + getString(R.string.tip_2));
-        else if (randomTip == 3) mTip.setSummary(getString(R.string.tip_a) + " " + getString(R.string.tip_3));
-        else if (randomTip == 4) mTip.setSummary(getString(R.string.tip_a) + " " + getString(R.string.tip_4));
-        else if (randomTip == 5) mTip.setSummary(getString(R.string.tip_a) + " " + getString(R.string.tip_5));
-        else if (randomTip == 6) mTip.setSummary(getString(R.string.tip_a) + " " + getString(R.string.tip_6));
-        else if (randomTip == 7) mTip.setSummary(getString(R.string.tip_a) + " " + getString(R.string.tip_7));
-        else if (randomTip == 8) mTip.setSummary(getString(R.string.tip_a) + " " + getString(R.string.tip_8));
-        else if (randomTip == 9) mTip.setSummary(getString(R.string.tip_a) + " " + getString(R.string.tip_9));
-        else if (randomTip == 10) mTip.setSummary(getString(R.string.tip_a) + " " + getString(R.string.tip_10));
-        else if (randomTip == 11) mTip.setSummary(getString(R.string.tip_a) + " " + getString(R.string.tip_11));
-        else if (randomTip == 12) mTip.setSummary(getString(R.string.tip_a) + " " + getString(R.string.tip_12));
-        else if (randomTip == 13) mTip.setSummary(getString(R.string.tip_a) + " " + getString(R.string.tip_13));
-        else if (randomTip == 14) mTip.setSummary(getString(R.string.tip_a) + " " + getString(R.string.tip_14));
-        else if (randomTip == 15) mTip.setSummary(getString(R.string.tip_a) + " " + getString(R.string.tip_15));
-        else if (randomTip == 16) mTip.setSummary(getString(R.string.tip_a) + " " + getString(R.string.tip_16));
-        else mTip.setSummary(getString(R.string.tip_a) + " " + R.string.tip_0);
+        if (randomTip == 1) mTip.setSummary(R.string.tip_1);
+        else if (randomTip == 2) mTip.setSummary(R.string.tip_2);
+        else if (randomTip == 3) mTip.setSummary(R.string.tip_3);
+        else if (randomTip == 4) mTip.setSummary(R.string.tip_4);
+        else if (randomTip == 5) mTip.setSummary(R.string.tip_5);
+        else if (randomTip == 6) mTip.setSummary(R.string.tip_6);
+        else if (randomTip == 7) mTip.setSummary(R.string.tip_7);
+        else if (randomTip == 8) mTip.setSummary(R.string.tip_8);
+        else if (randomTip == 9) mTip.setSummary(R.string.tip_9);
+        else if (randomTip == 10) mTip.setSummary(R.string.tip_10);
+        else if (randomTip == 11) mTip.setSummary(R.string.tip_11);
+        else if (randomTip == 12) mTip.setSummary(R.string.tip_12);
+        else if (randomTip == 13) mTip.setSummary(R.string.tip_13);
+        else if (randomTip == 14) mTip.setSummary(R.string.tip_14);
+        else if (randomTip == 15) mTip.setSummary(R.string.tip_15);
+        else if (randomTip == 16) mTip.setSummary(R.string.tip_16);
+        else mTip.setSummary(R.string.tip_0);
     }
 }
