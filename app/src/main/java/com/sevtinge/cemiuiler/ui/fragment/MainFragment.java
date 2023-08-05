580d696861140ead816b17ec629be1a532cba2ce
MoralNorm <732388462@qq.com>
2023-08-05 15:51:06 +0800

    修复背景自定义页面FC问题

--- app/src/main/java/com/sevtinge/cemiuiler/ui/fragment/MainFragment.java
+++ app/src/main/java/com/sevtinge/cemiuiler/ui/fragment/MainFragment.java
@@ -19,6 +19,30 @@ public class MainFragment extends SettingsPreferenceFragment {
     Preference mTip;
     Random r = new Random();
 
+    int[] tips = new int[] {
+        R.string.tip_0,
+        R.string.tip_1,
+        R.string.tip_2,
+        R.string.tip_3,
+        R.string.tip_4,
+        R.string.tip_5,
+        R.string.tip_6,
+        R.string.tip_7,
+        R.string.tip_8,
+        R.string.tip_9,
+        R.string.tip_10,
+        R.string.tip_11,
+        R.string.tip_12,
+        R.string.tip_13,
+        R.string.tip_14,
+        R.string.tip_15,
+        R.string.tip_16,
+        R.string.tip_17,
+        R.string.tip_18,
+        R.string.tip_19,
+        R.string.tip_20,
+    };
+
     @Override
     public int getContentResId() {
         return R.xml.prefs_main;
@@ -41,26 +65,6 @@ public class MainFragment extends SettingsPreferenceFragment {
         mSecurityCenter.setVisible(!isPad());
         mSecurityCenterPad.setVisible(isPad());
 
-        if (randomTip == 1) mTip.setSummary(R.string.tip_1);
-        else if (randomTip == 2) mTip.setSummary(R.string.tip_2);
-        else if (randomTip == 3) mTip.setSummary(R.string.tip_3);
-        else if (randomTip == 4) mTip.setSummary(R.string.tip_4);
-        else if (randomTip == 5) mTip.setSummary(R.string.tip_5);
-        else if (randomTip == 6) mTip.setSummary(R.string.tip_6);
-        else if (randomTip == 7) mTip.setSummary(R.string.tip_7);
-        else if (randomTip == 8) mTip.setSummary(R.string.tip_8);
-        else if (randomTip == 9) mTip.setSummary(R.string.tip_9);
-        else if (randomTip == 10) mTip.setSummary(R.string.tip_10);
-        else if (randomTip == 11) mTip.setSummary(R.string.tip_11);
-        else if (randomTip == 12) mTip.setSummary(R.string.tip_12);
-        else if (randomTip == 13) mTip.setSummary(R.string.tip_13);
-        else if (randomTip == 14) mTip.setSummary(R.string.tip_14);
-        else if (randomTip == 15) mTip.setSummary(R.string.tip_15);
-        else if (randomTip == 16) mTip.setSummary(R.string.tip_16);
-        else if (randomTip == 17) mTip.setSummary(R.string.tip_17);
-        else if (randomTip == 18) mTip.setSummary(R.string.tip_18);
-        else if (randomTip == 19) mTip.setSummary(R.string.tip_19);
-        else if (randomTip == 20) mTip.setSummary(R.string.tip_20);
-        else mTip.setSummary(R.string.tip_0);
+        mTip.setSummary(tips[randomTip]);
     }
 }
--- app/src/main/java/com/sevtinge/cemiuiler/ui/fragment/sub/CustomBackgroundSettings.java
+++ app/src/main/java/com/sevtinge/cemiuiler/ui/fragment/sub/CustomBackgroundSettings.java
@@ -8,113 +8,125 @@ import com.sevtinge.cemiuiler.utils.PrefsUtils;
 
 import moralnorm.preference.ColorPickerPreference;
 import moralnorm.preference.Preference;
-import moralnorm.preference.SeekBarPreferenceEx;
+import moralnorm.preference.SeekBarPreference;
 import moralnorm.preference.SwitchPreference;
 
 public class CustomBackgroundSettings extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
 
-    private SeekBarPreferenceEx mBlurRadius;
-    private SwitchPreference mBlurEnabled;
-
-    private ColorPickerPreference mColor;
-    private SeekBarPreferenceEx mColorAlpha;
-    private SeekBarPreferenceEx mCornerRadius;
-
-    private String mBlurRadiusKey;
-    private String mBlurEnabledKey;
-
+    private String mKey = "";
+    private String mCustomBackgroundEnabledKey;
     private String mColorKey;
     private String mColorAlphaKey;
     private String mCornerRadiusKey;
 
+    private String mBlurEnabledKey;
+    private String mBlurRadiusKey;
+
+    private SwitchPreference mCustomEnabledPreference;
+
+    private ColorPickerPreference mColorPickerPreference;
+    private SeekBarPreference mColorAlphaPreference;
+    private SeekBarPreference mCornerRadiusPreference;
+
+    private SwitchPreference mBlurEnabledPreference;
+    private SeekBarPreference mBlurRadiusPreference;
+
     @Override
     public int getContentResId() {
-        return R.xml.various_background;
+        return R.xml.custom_background;
     }
 
     @Override
     public void initPrefs() {
         Bundle args = getArguments();
-        String mKey = args.getString("key");
 
-        mBlurRadius = findPreference("prefs_key_custom_background_blur_radius");
-        mBlurEnabled = findPreference("prefs_key_custom_background_blur_enabled");
+        if (args != null) {
+            mKey = args.getString("key");
 
-        mColor = findPreference("prefs_key_custom_background_color");
-        mColorAlpha = findPreference("prefs_key_custom_background_color_alpha");
-        mCornerRadius = findPreference("prefs_key_custom_background_corner_radius");
+            mCustomBackgroundEnabledKey = mKey + "_custom_enable";
 
-        mBlurEnabledKey = mKey + "_blur_enabled";
-        mBlurRadiusKey = mKey + "_blur_radius";
+            mColorKey = mKey + "_color";
+            mColorAlphaKey = mKey + "_color_alpha";
+            mCornerRadiusKey = mKey + "_corner_radius";
 
-        mColorKey = mKey + "_color";
-        mColorAlphaKey = mKey + "_color_alpha";
-        mCornerRadiusKey = mKey + "_corner_radius";
+            mBlurEnabledKey = mKey + "_blur_enabled";
+            mBlurRadiusKey = mKey + "_blur_radius";
+        } else {
+            finish();
+        }
 
-        mBlurEnabled.setChecked(isBackgroundBlurEnabled());
+        mCustomEnabledPreference = findPreference("prefs_key_custom_background_enabled");
 
-        mColorAlpha.setVisible(false);
-        mColor.saveValue(getBackgroundColor());
-        setSeekBarPreferenceValue(mColorAlpha, mColorAlphaKey, 120);
-        setSeekBarPreferenceValue(mCornerRadius, mCornerRadiusKey, 18);
-        setSeekBarPreferenceValue(mBlurRadius, mBlurRadiusKey, 60);
+        mColorPickerPreference = findPreference("prefs_key_custom_background_color");
+        mColorAlphaPreference = findPreference("prefs_key_custom_background_color_alpha");
+        mCornerRadiusPreference = findPreference("prefs_key_custom_background_corner_radius");
 
-        mBlurRadius.setOnPreferenceChangeListener(this);
-        mBlurEnabled.setOnPreferenceChangeListener(this);
+        mBlurEnabledPreference = findPreference("prefs_key_custom_background_blur_enabled");
+        mBlurRadiusPreference = findPreference("prefs_key_custom_background_blur_radius");
 
-        mColor.setOnPreferenceChangeListener(this);
-        mColorAlpha.setOnPreferenceChangeListener(this);
-        mCornerRadius.setOnPreferenceChangeListener(this);
+        loadData();
+
+        mCustomEnabledPreference.setOnPreferenceChangeListener(this);
+
+        mColorPickerPreference.setOnPreferenceChangeListener(this);
+        mColorAlphaPreference.setOnPreferenceChangeListener(this);
+        mCornerRadiusPreference.setOnPreferenceChangeListener(this);
+
+        mBlurEnabledPreference.setOnPreferenceChangeListener(this);
+        mBlurRadiusPreference.setOnPreferenceChangeListener(this);
     }
 
+
+    private void loadData() {
+        mCustomEnabledPreference.setChecked(isCustomEnabled());
+        mColorPickerPreference.saveValue(getColor(-1));
+        mColorAlphaPreference.setValue(getSeekBarValue(mColorAlphaKey, 60));
+        mCornerRadiusPreference.setValue(getSeekBarValue(mCornerRadiusKey, 18));
+        mBlurEnabledPreference.setChecked(isBackgroundBlurEnabled());
+        mBlurRadiusPreference.setValue(getSeekBarValue(mBlurRadiusKey, 60));
+    }
+
+    private boolean isCustomEnabled() {
+        return hasKey(mCustomBackgroundEnabledKey) && PrefsUtils.getSharedBoolPrefs(getContext(), mCustomBackgroundEnabledKey, false);
+    }
+
+    private void setColor() {
+        mColorPickerPreference.saveValue(getColor(2113929215));
+    }
+
+    private int getColor(int defValue) {
+        return hasKey(mColorKey) ? PrefsUtils.getSharedIntPrefs(getContext(), mColorKey, defValue) : defValue;
+    }
 
     private boolean isBackgroundBlurEnabled() {
         return hasKey(mBlurEnabledKey) && PrefsUtils.getSharedBoolPrefs(getContext(), mBlurEnabledKey, false);
     }
 
-    private int getBackgroundColor() {
-        if (hasKey(mColorKey)) {
-            return PrefsUtils.getSharedIntPrefs(getContext(), mColorKey, 2113929215);
-        } else {
-            return 2113929215;
-        }
+    private int getSeekBarValue(String key, int defValue) {
+        return hasKey(key) ? PrefsUtils.getSharedIntPrefs(getContext(), key, defValue) : defValue;
     }
-
-    private void setSeekBarPreferenceValue(SeekBarPreferenceEx preference, String key, int defValue) {
-        if (hasKey(key)) {
-            preference.setValue(PrefsUtils.getSharedIntPrefs(getContext(), key, defValue));
-        }
-    }
-
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object o) {
-        if (preference == mBlurEnabled) {
-            setBlurEnabled(!mBlurEnabled.isChecked());
-        } else if (preference == mBlurRadius) {
-            setBackgroundBlurRadius((int) o);
-        } else if (preference == mColor) {
+        if (preference == mCustomEnabledPreference) {
+            setCustomEnable((Boolean) o);
+        } else if (preference == mColorPickerPreference) {
             setBackgroundColor((int) o);
-        } else if (preference == mColorAlpha) {
+        } else if (preference == mColorAlphaPreference) {
             setBackgroundColorAlpha((int) o);
-        } else if (preference == mCornerRadius) {
+        } else if (preference == mCornerRadiusPreference) {
             setBackgroundCornerRadius((int) o);
+        } else if (preference == mBlurEnabledPreference) {
+            setBlurEnabled((Boolean) o);
+        } else if (preference == mBlurRadiusPreference) {
+            setBackgroundBlurRadius((int) o);
         }
         return false;
     }
 
-    private void setBlurEnabled(boolean isBlurEnabled) {
-        mBlurEnabled.setChecked(isBlurEnabled);
-        PrefsUtils.mSharedPreferences.edit().putBoolean(mBlurEnabledKey, isBlurEnabled).apply();
-        if (isBlurEnabled) {
-            setSeekBarPreferenceValue(mBlurRadius, mBlurRadiusKey, 60);
-            /*setSeekBarPreferenceValue(mBgAlpha, mBgAlphaKey, 120);*/
-        }
-    }
-
-    private void setBackgroundBlurRadius(int value) {
-        mBlurRadius.setValue(value);
-        PrefsUtils.mSharedPreferences.edit().putInt(mBlurRadiusKey, value).apply();
+    private void setCustomEnable(boolean isCustomEnabled) {
+        mCustomEnabledPreference.setChecked(isCustomEnabled);
+        PrefsUtils.mSharedPreferences.edit().putBoolean(mCustomBackgroundEnabledKey, isCustomEnabled).apply();
     }
 
     private void setBackgroundColor(int value) {
@@ -122,12 +134,22 @@ public class CustomBackgroundSettings extends SettingsPreferenceFragment impleme
     }
 
     private void setBackgroundColorAlpha(int value) {
-        mColorAlpha.setValue(value);
+        mColorAlphaPreference.setValue(value);
         PrefsUtils.mSharedPreferences.edit().putInt(mColorAlphaKey, value).apply();
     }
 
     private void setBackgroundCornerRadius(int value) {
-        mCornerRadius.setValue(value);
+        mCornerRadiusPreference.setValue(value);
         PrefsUtils.mSharedPreferences.edit().putInt(mCornerRadiusKey, value).apply();
+    }
+
+    private void setBlurEnabled(boolean isBlurEnabled) {
+        mBlurEnabledPreference.setChecked(isBlurEnabled);
+        PrefsUtils.mSharedPreferences.edit().putBoolean(mBlurEnabledKey, isBlurEnabled).apply();
+    }
+
+    private void setBackgroundBlurRadius(int value) {
+        mBlurRadiusPreference.setValue(value);
+        PrefsUtils.mSharedPreferences.edit().putInt(mBlurRadiusKey, value).apply();
     }
 }
