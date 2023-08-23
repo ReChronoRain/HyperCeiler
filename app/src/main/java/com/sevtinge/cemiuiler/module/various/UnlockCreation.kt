6590763f3e847781da6647e2f08348afbb45589a
Neko Qiqi <linqiqi233@qq.com>
2023-08-23 23:53:31 +0800

    test2

--- app/src/main/java/com/sevtinge/cemiuiler/module/various/UnlockCreation.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/various/UnlockCreation.kt
@@ -1,11 +1,11 @@
 package com.sevtinge.cemiuiler.module.various
 
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
-import​ ​com.github.kyuubiran.ezxhelper.ClassUtils.setStaticObject
+import com.github.kyuubiran.ezxhelper.ClassUtils.setStaticObject
 import com.sevtinge.cemiuiler.module.base.BaseHook
 
 object UnlockCreation : BaseHook() {
     override fun init() {
-        setStaticObject(loadClass(​"​miui.os.Build​"​)​, ​"​IS_TABLET​"​, ​true​)
+        setStaticObject(loadClass("miui.os.Build"), "IS_TABLET", true)
     }
 }
