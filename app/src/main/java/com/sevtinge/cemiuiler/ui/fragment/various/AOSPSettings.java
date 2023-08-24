0c23934a80fc0beac89df3fa4638bcf016b3c968
Neko Qiqi <linqiqi233@qq.com>
2023-08-25 00:48:53 +0800

    add various-aosp ui settings

---
+++ app/src/main/java/com/sevtinge/cemiuiler/ui/fragment/various/AOSPSettings.java
@@ -0,0 +1,11 @@
+package com.sevtinge.cemiuiler.ui.fragment.various;
+
+import android.view.View;
+
+import com.sevtinge.cemiuiler.R;
+import com.sevtinge.cemiuiler.ui.fragment.base.SettingsPreferenceFragment;
+
+public class AOSPSettings extends SettingsPreferenceFragment {
+    @Override
+    public int getContentResId() { return R.xml.various_aosp; }
+}
