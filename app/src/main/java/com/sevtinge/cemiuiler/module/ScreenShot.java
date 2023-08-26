48171d7a6216551cc8f3f29efe7e0f75d106eb38
Neko Qiqi <linqiqi233@qq.com>
2023-08-26 10:26:44 +0800

    feat: New Screenshot-Custom Screenshot Model with Shell

--- app/src/main/java/com/sevtinge/cemiuiler/module/ScreenShot.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/ScreenShot.java
@@ -1,6 +1,7 @@
 package com.sevtinge.cemiuiler.module;
 
 import com.sevtinge.cemiuiler.module.base.BaseModule;
+import com.sevtinge.cemiuiler.module.screenshot.DeviceShellCustomize;
 import com.sevtinge.cemiuiler.module.screenshot.SaveToPictures;
 import com.sevtinge.cemiuiler.module.screenshot.UnlockMinimumCropLimit;
 import com.sevtinge.cemiuiler.module.various.UnlockSuperClipboard;
@@ -11,7 +12,12 @@ public class ScreenShot extends BaseModule {
     public void handleLoadPackage() {
         initHook(new UnlockMinimumCropLimit(), mPrefsMap.getBoolean("screenshot_unlock_minimum_crop_limit"));
         initHook(SaveToPictures.INSTANCE, mPrefsMap.getBoolean("screenshot_save_to_pictures"));
+        initHook(DeviceShellCustomize.INSTANCE, !TextUtils.isEmpty(mPrefsMap.getString("screenshot_device_customize", "")));
+        
+        // 超级剪切板
         initHook(UnlockSuperClipboard.INSTANCE, mPrefsMap.getBoolean("various_super_clipboard_enable"));
+        
+        
     }
 }
 
