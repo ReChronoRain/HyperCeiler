5a6328a6eb70db008a76bf30954d7e4bfa8cdbf5
mu7220 <80700814+mu7220@users.noreply.github.com>
2023-09-07 23:31:30 +0800

    智能助理-负一屏背景设置 添加官方模糊选项

--- app/src/main/java/com/sevtinge/cemiuiler/module/app/PersonalAssistant.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/app/PersonalAssistant.java
@@ -3,6 +3,7 @@ package com.sevtinge.cemiuiler.module.app;
 import com.sevtinge.cemiuiler.module.base.BaseModule;
 import com.sevtinge.cemiuiler.module.hook.personalassistant.BlurOverlay;
 import com.sevtinge.cemiuiler.module.hook.personalassistant.BlurPersonalAssistant;
+import com.sevtinge.cemiuiler.module.hook.personalassistant.BlurPersonalAssistantBackGround;
 import com.sevtinge.cemiuiler.module.hook.personalassistant.EnableFoldWidget;
 import com.sevtinge.cemiuiler.module.hook.personalassistant.PersonalAssistantDexKit;
 import com.sevtinge.cemiuiler.module.hook.personalassistant.WidgetCrack;
@@ -14,7 +15,12 @@ public class PersonalAssistant extends BaseModule {
         initHook(new PersonalAssistantDexKit());
         initHook(new BlurOverlay(), false);
         initHook(new EnableFoldWidget(), mPrefsMap.getBoolean("personal_assistant_fold_widget_enable"));
-        initHook(BlurPersonalAssistant.INSTANCE, mPrefsMap.getBoolean("pa_enable"));
+
+        if (mPrefsMap.getStringAsInt("personal_assistant_value", 1) != 1) {
+            initHook(BlurPersonalAssistant.INSTANCE, mPrefsMap.getBoolean("pa_enable"));
+        } else {
+            initHook(BlurPersonalAssistantBackGround.INSTANCE, mPrefsMap.getBoolean("pa_enable"));
+        }
 
         initHook(new WidgetCrack(), mPrefsMap.getBoolean("hidden_function") && mPrefsMap.getBoolean("personal_assistant_widget_crack"));
     }
--- app/src/main/java/com/sevtinge/cemiuiler/ui/fragment/PersonalAssistantFragment.java
+++ app/src/main/java/com/sevtinge/cemiuiler/ui/fragment/PersonalAssistantFragment.java
@@ -5,12 +5,21 @@ import android.view.View;
 import com.sevtinge.cemiuiler.R;
 import com.sevtinge.cemiuiler.ui.base.BaseSettingsActivity;
 import com.sevtinge.cemiuiler.ui.fragment.base.SettingsPreferenceFragment;
+import com.sevtinge.cemiuiler.utils.PrefsUtils;
 
+import moralnorm.preference.ColorPickerPreference;
+import moralnorm.preference.DropDownPreference;
+import moralnorm.preference.Preference;
+import moralnorm.preference.SeekBarPreferenceEx;
 import moralnorm.preference.SwitchPreference;
 
-public class PersonalAssistantFragment extends SettingsPreferenceFragment {
+public class PersonalAssistantFragment extends SettingsPreferenceFragment
+    implements Preference.OnPreferenceChangeListener {
 
     SwitchPreference mWidgetCrack;
+    SeekBarPreferenceEx mBlurRadius;
+    ColorPickerPreference mBlurColor;
+    DropDownPreference mBlurBackGround;
 
     @Override
     public int getContentResId() {
@@ -27,10 +36,30 @@ public class PersonalAssistantFragment extends SettingsPreferenceFragment {
 
     @Override
     public void initPrefs() {
+        int mBlurMode = Integer.parseInt(PrefsUtils.getSharedStringPrefs(getContext(), "prefs_key_personal_assistant_value", "1"));
         mWidgetCrack = findPreference("prefs_key_personal_assistant_widget_crack");
+        mBlurBackGround = findPreference("prefs_key_personal_assistant_value");
+        mBlurRadius = findPreference("prefs_key_personal_assistant_blurradius");
+        mBlurColor = findPreference("prefs_key_personal_assistant_color");
 
         if (!getSharedPreferences().getBoolean("prefs_key_hidden_function", false)) {
             mWidgetCrack.setVisible(false);
         }
+
+        setBlurMode(mBlurMode);
+        mBlurBackGround.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object o) {
+        if (preference == mBlurBackGround) {
+            setBlurMode(Integer.parseInt((String) o));
+        }
+        return true;
+    }
+
+    private void setBlurMode(int mode) {
+        mBlurRadius.setVisible(mode == 2);
+        mBlurColor.setVisible(mode == 2);
     }
 }
