a1b69ebf54874cec710632098ac3f0f310e96c42
mu7220 <80700814+mu7220@users.noreply.github.com>
2023-09-25 20:52:58 +0800

    新增 截屏-解锁智能隐私打码（需要截屏版本为 1.4.60-08211449）

--- app/src/main/java/com/sevtinge/cemiuiler/module/app/ScreenShot.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/app/ScreenShot.java
@@ -6,6 +6,7 @@ import com.sevtinge.cemiuiler.module.base.BaseModule;
 import com.sevtinge.cemiuiler.module.hook.screenshot.DeviceShellCustomize;
 import com.sevtinge.cemiuiler.module.hook.screenshot.SaveToPictures;
 import com.sevtinge.cemiuiler.module.hook.screenshot.UnlockMinimumCropLimit;
+import com.sevtinge.cemiuiler.module.hook.screenshot.UnlockPrivacyMarking;
 import com.sevtinge.cemiuiler.module.hook.various.UnlockSuperClipboard;
 
 public class ScreenShot extends BaseModule {
@@ -15,11 +16,10 @@ public class ScreenShot extends BaseModule {
         initHook(new UnlockMinimumCropLimit(), mPrefsMap.getBoolean("screenshot_unlock_minimum_crop_limit"));
         initHook(SaveToPictures.INSTANCE, mPrefsMap.getBoolean("screenshot_save_to_pictures"));
         initHook(DeviceShellCustomize.INSTANCE, !TextUtils.isEmpty(mPrefsMap.getString("screenshot_device_customize", "")));
+        initHook(UnlockPrivacyMarking.INSTANCE, mPrefsMap.getBoolean("screenshot_unlock_privacy_marking"));
 
         // 超级剪切板
         initHook(UnlockSuperClipboard.INSTANCE, mPrefsMap.getBoolean("various_super_clipboard_enable"));
-
-
     }
 }
 
