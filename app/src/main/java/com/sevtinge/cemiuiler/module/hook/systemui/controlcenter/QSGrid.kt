852c73f859931873b9c3f7c48636c2cf1363dc0d
ZYM <zym013579@users.noreply.github.com>
2023-09-26 00:11:46 +0800

    调整 系统界面-旧版经典控制中心自定义行列数功能；新增 电量与性能-禁止恢复电池优化白名单(#382)
    * 修改旧版经典控制中心自定义行列数功能；新增禁止恢复电池优化白名单功能

---
+++ app/src/main/java/com/sevtinge/cemiuiler/module/hook/systemui/controlcenter/QSGrid.kt
@@ -0,0 +1,67 @@
+package com.sevtinge.cemiuiler.module.hook.systemui.controlcenter
+
+import android.content.res.Configuration
+import android.view.View
+import android.view.ViewGroup
+
+import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
+import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
+import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
+
+import com.sevtinge.cemiuiler.module.base.BaseHook
+
+import de.robv.android.xposed.XposedHelpers
+
+class QSGrid : BaseHook() {
+    override fun init() {
+        val cols = mPrefsMap.getInt("system_control_center_old_qs_columns", 4)
+        val colsHorizontal = mPrefsMap.getInt("system_control_center_old_qs_columns_horizontal", 5)
+        val rows = mPrefsMap.getInt("system_control_center_old_qs_rows", 3)
+        val rowsHorizontal = mPrefsMap.getInt("system_control_center_old_qs_rows_horizontal", 2)
+
+        loadClass("com.android.systemui.qs.MiuiTileLayout").methodFinder().first {
+                name == "updateColumns"
+            }.createHook {
+                after {
+                    val viewGroup = it.thisObject as ViewGroup
+                    val mConfiguration: Configuration = viewGroup.context.resources.configuration
+                    if (mConfiguration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+                        XposedHelpers.setObjectField (
+                            it.thisObject,
+                            "mColumns",
+                            cols
+                        )
+                    } else {
+                        XposedHelpers.setObjectField (
+                            it.thisObject,
+                            "mColumns",
+                            colsHorizontal
+                        )
+                    }
+                }
+            }
+
+        loadClass("com.android.systemui.qs.MiuiTileLayout").methodFinder().first {
+                name == "updateResources"
+            }.createHook {
+                after {
+                    val viewGroup = it.thisObject as ViewGroup
+                    val mConfiguration: Configuration = viewGroup.context.resources.configuration
+                    if (mConfiguration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+                        XposedHelpers.setObjectField (
+                            it.thisObject,
+                            "mMaxAllowedRows",
+                            rows
+                        )
+                    } else {
+                        XposedHelpers.setObjectField (
+                            it.thisObject,
+                            "mMaxAllowedRows",
+                            rowsHorizontal
+                        )
+                    }
+                    viewGroup.requestLayout()
+                }
+            }
+    }
+}
