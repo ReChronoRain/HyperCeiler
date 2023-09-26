bbeaa38bfeecc8d640d476be4b04936469c1473b
mu7220 <80700814+mu7220@users.noreply.github.com>
2023-09-26 10:59:24 +0800

    调整hook

--- app/src/main/java/com/sevtinge/cemiuiler/module/hook/creation/UnlockCreation.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/hook/creation/UnlockCreation.kt
@@ -1,11 +1,11 @@
 package com.sevtinge.cemiuiler.module.hook.creation
 
-import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
 import com.github.kyuubiran.ezxhelper.ClassUtils.setStaticObject
 import com.sevtinge.cemiuiler.module.base.BaseHook
+import com.sevtinge.cemiuiler.utils.api.LazyClass.clazzMiuiBuild
 
 object UnlockCreation : BaseHook() {
     override fun init() {
-        setStaticObject(loadClass("miui.os.Build"), "IS_TABLET", true)
+        setStaticObject(clazzMiuiBuild, "IS_TABLET", true)
     }
 }
