24162386a1b68739b937d16240810b857cd5623c
mu7220 <80700814+mu7220@users.noreply.github.com>
2023-10-20 21:29:58 +0800

    [修复 & dexKit 适配] 截屏-解锁最小图片限制失效的问题

--- app/src/main/java/com/sevtinge/cemiuiler/module/app/ScreenShot.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/app/ScreenShot.java
@@ -3,6 +3,8 @@ package com.sevtinge.cemiuiler.module.app;
 import android.text.TextUtils;
 
 import com.sevtinge.cemiuiler.module.base.BaseModule;
+import com.sevtinge.cemiuiler.module.base.CloseHostDir;
+import com.sevtinge.cemiuiler.module.base.LoadHostDir;
 import com.sevtinge.cemiuiler.module.hook.screenshot.DeviceShellCustomize;
 import com.sevtinge.cemiuiler.module.hook.screenshot.SaveToPictures;
 import com.sevtinge.cemiuiler.module.hook.screenshot.UnlockMinimumCropLimit;
@@ -13,10 +15,15 @@ public class ScreenShot extends BaseModule {
 
     @Override
     public void handleLoadPackage() {
-        initHook(new UnlockMinimumCropLimit(), mPrefsMap.getBoolean("screenshot_unlock_minimum_crop_limit"));
+        // dexKit load
+        initHook(LoadHostDir.INSTANCE);
+        initHook(UnlockMinimumCropLimit.INSTANCE, mPrefsMap.getBoolean("screenshot_unlock_minimum_crop_limit"));
         initHook(SaveToPictures.INSTANCE, mPrefsMap.getBoolean("screenshot_save_to_pictures"));
         initHook(DeviceShellCustomize.INSTANCE, !TextUtils.isEmpty(mPrefsMap.getString("screenshot_device_customize", "")));
         initHook(UnlockPrivacyMarking.INSTANCE, mPrefsMap.getBoolean("screenshot_unlock_privacy_marking"));
+        // dexKit finish
+        initHook(CloseHostDir.INSTANCE);
+
         // 超级剪切板
         initHook(UnlockSuperClipboard.INSTANCE, mPrefsMap.getBoolean("various_super_clipboard_enable"));
     }
--- app/src/main/java/com/sevtinge/cemiuiler/module/hook/screenshot/UnlockMinimumCropLimit.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/hook/screenshot/UnlockMinimumCropLimit.kt
@@ -1,22 +1,37 @@
-package com.sevtinge.cemiuiler.module.hook.screenshot;
+package com.sevtinge.cemiuiler.module.hook.screenshot
 
-import com.sevtinge.cemiuiler.module.base.BaseHook;
+import com.github.kyuubiran.ezxhelper.EzXHelper
+import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
+import com.sevtinge.cemiuiler.module.base.BaseHook
+import com.sevtinge.cemiuiler.utils.DexKit.addUsingStringsEquals
+import com.sevtinge.cemiuiler.utils.DexKit.dexKitBridge
+import java.lang.reflect.Modifier
 
-import de.robv.android.xposed.XC_MethodReplacement;
-
-public class UnlockMinimumCropLimit extends BaseHook {
-
-    Class<?> mScreenCropView;
-
-    @Override
-    public void init() {
-        mScreenCropView = findClassIfExists("com.miui.gallery.editor.photo.screen.crop.ScreenCropView$h");
-        returnIntConstant(mScreenCropView, "a");
+object UnlockMinimumCropLimit : BaseHook() {
+    private val mScreenCropViewMethod by lazy {
+        dexKitBridge.findMethod {
+            matcher {
+                declaredClass {
+                    addUsingStringsEquals("fixImageBounds %f,%f")
+                }
+                usingNumbers(0.5f, 200)
+                returnType = "int"
+                modifiers = Modifier.PRIVATE
+            }
+        }.map { it.getMethodInstance(EzXHelper.classLoader) }.first()
     }
 
-    private void returnIntConstant(Class<?> cls, String methodName) {
-        findAndHookMethod(cls, methodName, XC_MethodReplacement.returnConstant(0));
+    override fun init() {
+        mScreenCropViewMethod.createHook {
+            returnConstant(0)
+        }
+
+        /*val mScreenCropView =
+            findClassIfExists("com.miui.gallery.editor.photo.screen.crop.ScreenCropView\$h")
+        returnIntConstant(mScreenCropView, "a")*/
     }
+
+    /*private fun returnIntConstant(cls: Class<*>?, methodName: String) {
+        findAndHookMethod(cls, methodName, XC_MethodReplacement.returnConstant(0))
+    }*/
 }
-
-
