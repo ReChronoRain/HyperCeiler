206bfb51b39cb0d093531b61c59efe928cf73eb5
mu7220 <80700814+mu7220@users.noreply.github.com>
2023-10-24 00:58:35 +0800

    [修复] 小米互传 2.21.0 禁用自动关闭小米互传失效的问题

--- app/src/main/java/com/sevtinge/cemiuiler/module/hook/mishare/NoAutoTurnOff.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/hook/mishare/NoAutoTurnOff.kt
@@ -5,12 +5,37 @@ import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHooks
 import com.sevtinge.cemiuiler.module.base.BaseHook
 import com.sevtinge.cemiuiler.utils.DexKit.addUsingStringsEquals
 import com.sevtinge.cemiuiler.utils.DexKit.dexKitBridge
+import com.sevtinge.cemiuiler.utils.api.BlurDraw.getValueByFields
+import de.robv.android.xposed.XposedHelpers
+import java.lang.reflect.Modifier
 
 object NoAutoTurnOff : BaseHook() {
     private val nullMethod by lazy {
         dexKitBridge.findMethod {
             matcher {
                 addUsingStringsEquals("EnabledState", "mishare_enabled")
+            }
+        }.map { it.getMethodInstance(classLoader) }.toList()
+    }
+
+    private val null2Method by lazy {
+        dexKitBridge.findMethod {
+            matcher {
+                declaredClass {
+                    addUsingStringsEquals("mishare:advertise_lock", "power")
+                }
+                paramCount = 2
+                modifiers = Modifier.STATIC
+            }
+        }.map { it.getMethodInstance(classLoader) }.toList()
+    }
+
+    private val null3Method by lazy {
+        dexKitBridge.findMethod {
+            matcher {
+                addUsingStringsEquals("com.miui.mishare.action.GRANT_NFC_TOUCH_PERMISSION")
+                usingNumbers(600000L)
+                modifiers = Modifier.PRIVATE
             }
         }.map { it.getMethodInstance(classLoader) }.toList()
     }
@@ -29,12 +54,57 @@ object NoAutoTurnOff : BaseHook() {
     }
 
     override fun init() {
+        val nullClass = dexKitBridge.findClass {
+            matcher {
+                addUsingStringsEquals("NfcShareTaskManager", "task out of limit type ")
+            }
+        }.map { it.getInstance(classLoader) }.first()
+
+        val nullField = dexKitBridge.findField {
+            matcher {
+                declaredClass(nullClass)
+                modifiers = Modifier.STATIC
+            }
+        }.map { it.getFieldInstance(classLoader) }.first()
+
         // 禁用小米互传功能自动关闭部分
         nullMethod.createHooks {
             before {
                 it.result = null
             }
         }
+
+        try {
+            null2Method.createHooks {
+                before {
+                    it.result = null
+                }
+            }
+        } catch (_: Throwable) {
+        }
+
+        try {
+            null3Method.createHooks {
+                before {
+                    val fieldNames = ('a'..'z').map { name -> name.toString() }
+                    val getField = getValueByFields(it.thisObject, fieldNames) ?: return@before
+                    XposedHelpers.callMethod(getField, "removeCallbacks", it.thisObject)
+                    logI("null3Method hook success, $getField")
+                }
+            }
+        } catch (t: Throwable) {
+            logE("null3Method Hook failed", t)
+        }
+
+        try {
+            logI("$nullClass")
+            logI("$nullField")
+            XposedHelpers.setStaticIntField(nullClass, nullField.name, 999999999)
+            logI("nullField hook success")
+        } catch (t: Throwable) {
+            logE("nullField hook failed")
+        }
+
 
         // 干掉小米互传十分钟倒计时 Toast
         toastMethod.createHooks {
