7877e919f1a85df2482fc996953cc3e68b5d42c8
mu7220 <80700814+mu7220@users.noreply.github.com>
2023-11-06 13:00:13 +0800

    [fix] 系统界面-状态栏-图标-电池图标/百分比/百分比符号 by HyperOS

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemui/statusbar/icon/all/HideBatteryIcon.kt
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemui/statusbar/icon/all/HideBatteryIcon.kt
@@ -8,7 +8,6 @@ import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 import com.sevtinge.hyperceiler.module.base.BaseHook
-import com.sevtinge.hyperceiler.utils.devicesdk.isAndroidR
 import com.sevtinge.hyperceiler.utils.devicesdk.isMoreAndroidVersion
 import com.sevtinge.hyperceiler.utils.getObjectField
 import com.sevtinge.hyperceiler.utils.getObjectFieldAs
@@ -16,37 +15,41 @@ import com.sevtinge.hyperceiler.utils.getObjectFieldAs
 object HideBatteryIcon : BaseHook() {
     override fun init() {
         val mBatteryMeterViewClass = when {
-            isAndroidR() -> loadClass("com.android.systemui.MiuiBatteryMeterView")
             isMoreAndroidVersion(31) -> loadClass("com.android.systemui.statusbar.views.MiuiBatteryMeterView")
-            else -> null
+            else -> loadClass("com.android.systemui.MiuiBatteryMeterView")
         }
-
-        mBatteryMeterViewClass!!.methodFinder().first {
-            name == "updateResources"
+        try {
+            mBatteryMeterViewClass.methodFinder().first {
+                name == "updateResources"
+            }
+        } catch(t: Throwable) {
+            mBatteryMeterViewClass.methodFinder().first {
+                name == "updateAll"
+            }
         }.createHook {
             after { param ->
-                // 隐藏电池图标
-                if (mPrefsMap.getBoolean("system_ui_status_bar_battery_icon")) {
-                    (param.thisObject?.getObjectFieldAs<ImageView>("mBatteryIconView"))?.visibility =
-                        View.GONE
-                    if (param.thisObject?.getObjectField("mBatteryStyle") == 1) {
-                        (param.thisObject?.getObjectFieldAs<FrameLayout>("mBatteryDigitalView"))?.visibility =
+                if (param.thisObject != null) {
+                    // 隐藏电池图标
+                    if (mPrefsMap.getBoolean("system_ui_status_bar_battery_icon")) {
+                        (param.thisObject.getObjectFieldAs<ImageView>("mBatteryIconView")).visibility =
                             View.GONE
+
+                        if (param.thisObject.getObjectField("mBatteryStyle") == 1) {
+                            (param.thisObject.getObjectFieldAs<FrameLayout>("mBatteryDigitalView")).visibility =
+                                View.GONE
+                        }
                     }
-                }
-                // 隐藏电池百分号
-                if (mPrefsMap.getBoolean("system_ui_status_bar_battery_percent") ||
-                    mPrefsMap.getBoolean("system_ui_status_bar_battery_percent_mark")
-                ) {
-                    (param.thisObject?.getObjectFieldAs<TextView>("mBatteryPercentMarkView"))?.textSize =
-                        0F
-                }
-                // 隐藏电池内的百分比
-                if (mPrefsMap.getBoolean("system_ui_status_bar_battery_percent")) {
-                    (param.thisObject?.getObjectFieldAs<TextView>("mBatteryPercentView"))?.textSize =
-                        0F
-                    (param.thisObject?.getObjectFieldAs<TextView>("mBatteryTextDigitView"))?.textSize =
-                        0F
+                    // 隐藏电池百分号
+                    if (mPrefsMap.getBoolean("system_ui_status_bar_battery_percent") ||
+                        mPrefsMap.getBoolean("system_ui_status_bar_battery_percent_mark")
+                    ) {
+                        (param.thisObject?.getObjectFieldAs<TextView>("mBatteryPercentMarkView"))?.textSize = 0F
+                    }
+                    // 隐藏电池内的百分比
+                    if (mPrefsMap.getBoolean("system_ui_status_bar_battery_percent")) {
+                        (param.thisObject?.getObjectFieldAs<TextView>("mBatteryPercentView"))?.textSize = 0F
+                        (param.thisObject?.getObjectFieldAs<TextView>("mBatteryTextDigitView"))?.textSize = 0F
+                    }
                 }
             }
         }
@@ -56,11 +59,13 @@ object HideBatteryIcon : BaseHook() {
         }.createHook {
             after { param ->
                 // 隐藏电池充电图标
-                if (mPrefsMap.getBoolean("system_ui_status_bar_battery_charging")) {
-                    (param.thisObject?.getObjectFieldAs<ImageView>("mBatteryChargingInView"))?.visibility =
-                        View.GONE
-                    (param.thisObject?.getObjectFieldAs<ImageView>("mBatteryChargingView"))?.visibility =
-                        View.GONE
+                if (param.thisObject != null) {
+                    if (mPrefsMap.getBoolean("system_ui_status_bar_battery_charging")) {
+                        (param.thisObject.getObjectFieldAs<ImageView>("mBatteryChargingInView")).visibility =
+                            View.GONE
+                        (param.thisObject.getObjectFieldAs<ImageView>("mBatteryChargingView")).visibility =
+                            View.GONE
+                    }
                 }
             }
         }
