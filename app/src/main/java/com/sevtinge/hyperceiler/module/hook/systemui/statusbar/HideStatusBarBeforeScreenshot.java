ea4df60dbf8bb2468055cf15da4ee10a3af63582
HChenX <xhc-xh@qq.com>
2023-11-11 12:56:50 +0800

    [修复] hyper设备-截屏时隐藏状态栏失效的问题
    Signed-off-by: HChenX <xhc-xh@qq.com>

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemui/statusbar/HideStatusBarBeforeScreenshot.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemui/statusbar/HideStatusBarBeforeScreenshot.java
@@ -5,30 +5,61 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Bundle;
 import android.view.View;
 
 import com.sevtinge.hyperceiler.module.base.BaseHook;
 
 public class HideStatusBarBeforeScreenshot extends BaseHook {
+    public static final String collapsedStatusBar = "com.android.systemui.statusbar.phone.MiuiCollapsedStatusBarFragment";
 
     @Override
     public void init() {
-        findAndHookMethod("com.android.systemui.statusbar.phone.MiuiCollapsedStatusBarFragment", lpparam.classLoader, "initMiuiViewsOnViewCreated", View.class, new MethodHook() {
-            @SuppressLint("UnspecifiedRegisterReceiverFlag")
-            @Override
-            protected void after(MethodHookParam param) throws Throwable {
-                View view = (View) param.args[0];
-                BroadcastReceiver br = new BroadcastReceiver() {
+        try {
+            findClass(collapsedStatusBar).getDeclaredMethod("initMiuiViewsOnViewCreated", View.class);
+            findAndHookMethod(collapsedStatusBar,
+                "initMiuiViewsOnViewCreated", View.class, new MethodHook() {
+
                     @Override
-                    public void onReceive(Context context, Intent intent) {
-                        if ("miui.intent.TAKE_SCREENSHOT".equals(intent.getAction())) {
-                            boolean finished = intent.getBooleanExtra("IsFinished", true);
-                            view.setVisibility(finished ? View.VISIBLE : View.INVISIBLE);
+                    protected void after(MethodHookParam param) {
+                        View view = (View) param.args[0];
+                        // logE(TAG, "1: " + param.args[0]);
+                        setCollapsedStatusBar(view);
+                    }
+                }
+            );
+        } catch (NoSuchMethodException e) {
+            try {
+                findClass(collapsedStatusBar).getDeclaredMethod("onViewCreated", View.class, Bundle.class);
+                findAndHookMethod(collapsedStatusBar,
+                    "onViewCreated", View.class, Bundle.class,
+                    new MethodHook() {
+                        @Override
+                        protected void after(MethodHookParam param) {
+                            View view = (View) param.args[0];
+                            // logE(TAG, "2: " + param.args[0]);
+                            setCollapsedStatusBar(view);
                         }
                     }
-                };
-                view.getContext().registerReceiver(br, new IntentFilter("miui.intent.TAKE_SCREENSHOT"));
+                );
+            } catch (NoSuchMethodException f) {
+                logE(TAG, "No such: " + collapsedStatusBar + " method initMiuiViewsOnViewCreated and onViewCreated");
             }
-        });
+        }
+    }
+
+    @SuppressLint("UnspecifiedRegisterReceiverFlag")
+    public void setCollapsedStatusBar(View view) {
+        BroadcastReceiver br = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if ("miui.intent.TAKE_SCREENSHOT".equals(intent.getAction())) {
+                    boolean finished = intent.getBooleanExtra("IsFinished", true);
+                    // logE(TAG, "1: " + finished);
+                    view.setVisibility(finished ? View.VISIBLE : View.INVISIBLE);
+                }
+            }
+        };
+        view.getContext().registerReceiver(br, new IntentFilter("miui.intent.TAKE_SCREENSHOT"));
     }
 }
