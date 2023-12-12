4fc5dbe523230fb983ff9be62eb2bb9645e79914
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2023-12-12 19:21:19 +0800

    尝试修复 系统框架-其他-允许不受信任的触摸 在Android14失效

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemframework/AllowUntrustedTouchForU.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemframework/AllowUntrustedTouchForU.java
@@ -6,11 +6,17 @@ import de.robv.android.xposed.XposedHelpers;
 
 public class AllowUntrustedTouchForU  extends BaseHook {
 
-    Class<?> mInputManager;
+    //Class<?> mInputManager;
 
     @Override
     public void init() {
-        mInputManager = findClassIfExists("android.hardware.input.InputManager");
-        XposedHelpers.setStaticLongField(mInputManager, "BLOCK_UNTRUSTED_TOUCHES", 0x96aec7eL);
+        //mInputManager = findClassIfExists("android.hardware.input.InputManager");
+        //XposedHelpers.setStaticLongField(mInputManager, "BLOCK_UNTRUSTED_TOUCHES", 0x96aec7eL);
+        findAndHookMethod("com.android.server.wm.WindowState", "getTouchOcclusionMode", new MethodHook(){
+            @Override
+            protected void after(MethodHookParam param) throws Throwable {
+                param.setResult(2);
+            }
+        });
     }
 }
