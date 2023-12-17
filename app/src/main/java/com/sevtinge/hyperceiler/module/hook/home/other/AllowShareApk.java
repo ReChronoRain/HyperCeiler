e130996766d20340d4d7f3e80ea1ea453fe33df3
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2023-12-18 02:57:25 +0800

    优化 系统桌面-其他-允许分享应用 支持系统应用 （部分未覆盖安装的系统应用分享会闪退 在修）

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/home/other/AllowShareApk.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/home/other/AllowShareApk.java
@@ -1,8 +1,13 @@
 package com.sevtinge.hyperceiler.module.hook.home.other;
 
+import android.content.Context;
+
+import com.sevtinge.hyperceiler.R;
 import com.sevtinge.hyperceiler.module.base.BaseHook;
 
-public class AllowShareApk extends BaseHook {
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
+
+public class AllowShareApk extends BaseHook{
     @Override
     public void init() throws NoSuchMethodException {
         findAndHookMethod("com.miui.home.launcher.common.Utilities", "isSecurityCenterSupportShareAPK", new MethodHook() {
@@ -12,5 +17,29 @@ public class AllowShareApk extends BaseHook {
                 }
             }
         );
+        findAndHookMethod("com.miui.home.launcher.shortcuts.SystemShortcutMenuItem$ShareAppShortcutMenuItem", "isValid", "com.miui.home.launcher.ItemInfo", new MethodHook() {
+                @Override
+                protected void before(MethodHookParam param) {
+                    findAndHookMethod("com.miui.home.launcher.common.Utilities", "isSystemPackage", Context.class, String.class, new MethodHook() {
+                            @Override
+                            protected void before(MethodHookParam param) {
+                                param.setResult(false);
+                            }
+                        }
+                    );
+                }
+            }
+        );
+        //mResHook.setResReplacement("com.miui.home", "XML", "file_paths", R.xml.hook_home_file_paths);
+
+    }
+
+    public void initResource(XC_InitPackageResources.InitPackageResourcesParam resparam) throws Throwable {
+        try {
+            resparam.res.setReplacement("com.miui.home", "xml", "file_paths", R.xml.hook_home_file_paths);
+            //resparam.res.setReplacement("com.miui.home", "xml", "launcher_settings", R.xml.hook_home_launcher_settings);
+        } catch (Exception e) {
+            logE(String.valueOf(e));
+        }
     }
 }
