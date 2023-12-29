ba374ce5129e3dec9b390dd7922a761cb9698387
mu7220 <80700814+mu7220@users.noreply.github.com>
2023-12-29 16:44:50 +0800

    fix HyperOS 系统界面-通知与控制中心-重定向通知设置（by yife）

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemui/controlcenter/CompactNotificationsHook.kt
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemui/controlcenter/CompactNotificationsHook.kt
@@ -15,32 +15,10 @@ object CompactNotificationsHook : BaseHook() {
     override fun init() {
         val abHeight = 39.0f
 
-        if (mPrefsMap.getBoolean("system_ui_control_center_compact_notice")) {
-            mResHook.setDensityReplacement(
-                "android",
-                "dimen",
-                "notification_action_height",
-                abHeight
-            )
-            mResHook.setDensityReplacement(
-                "android",
-                "dimen",
-                "android_notification_action_height",
-                abHeight
-            )
-            mResHook.setDensityReplacement(
-                "android",
-                "dimen",
-                "notification_action_list_height",
-                abHeight
-            )
-            mResHook.setDensityReplacement(
-                "com.android.systemui",
-                "dimen",
-                "notification_row_extra_padding",
-                0F
-            )
-        }
+        mResHook.setDensityReplacement("android", "dimen", "notification_action_height", abHeight)
+        mResHook.setDensityReplacement("android", "dimen", "android_notification_action_height", abHeight)
+        mResHook.setDensityReplacement("android", "dimen", "notification_action_list_height", abHeight)
+        mResHook.setDensityReplacement("com.android.systemui", "dimen", "notification_row_extra_padding", 0F)
 
         hookAllMethods(
             "com.android.systemui.statusbar.notification.row.wrapper.NotificationViewWrapper",
