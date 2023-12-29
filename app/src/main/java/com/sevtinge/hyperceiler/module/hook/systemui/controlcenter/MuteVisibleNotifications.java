7ed3fcbdb34609f22498826a345440193ce14571
mu7220 <80700814+mu7220@users.noreply.github.com>
2023-12-29 16:08:53 +0800

    fix Android 14 系统界面-通知与控制中心-亮屏时静音

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemui/controlcenter/MuteVisibleNotifications.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemui/controlcenter/MuteVisibleNotifications.java
@@ -1,5 +1,7 @@
 package com.sevtinge.hyperceiler.module.hook.systemui.controlcenter;
 
+import static com.sevtinge.hyperceiler.utils.devicesdk.SystemSDKKt.isMoreAndroidVersion;
+
 import android.content.Context;
 import android.os.PowerManager;
 
@@ -8,12 +10,18 @@ import com.sevtinge.hyperceiler.module.base.BaseHook;
 import de.robv.android.xposed.XposedHelpers;
 
 public class MuteVisibleNotifications extends BaseHook {
+    String NotificationLoadClass;
 
     @Override
     public void init() {
-        hookAllMethods("com.android.systemui.statusbar.notification.policy.NotificationAlertController", lpparam.classLoader, "buzzBeepBlink", new MethodHook() {
+        if (isMoreAndroidVersion(34)) {
+            NotificationLoadClass = "com.android.systemui.statusbar.notification.policy.MiuiAlertManager";
+        } else {
+            NotificationLoadClass = "com.android.systemui.statusbar.notification.policy.NotificationAlertController";
+        }
+        hookAllMethods(NotificationLoadClass, lpparam.classLoader, "buzzBeepBlink", new MethodHook() {
                 @Override
-                protected void before(MethodHookParam param) throws Throwable {
+                protected void before(MethodHookParam param) {
                     Context mContext = (Context) XposedHelpers.getObjectField(param.thisObject, "mContext");
                     PowerManager powerMgr = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
                     if (powerMgr.isInteractive()) {
