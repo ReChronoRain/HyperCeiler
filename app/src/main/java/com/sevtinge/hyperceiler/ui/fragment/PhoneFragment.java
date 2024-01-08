5043e7b29fb3903e7a3993376e0536f55d652ae1
焕晨HChen <HChen_LXi@outlook.com>
2024-01-08 10:41:39 +0800

    调整写法

--- app/src/main/java/com/sevtinge/hyperceiler/ui/fragment/PhoneFragment.java
+++ app/src/main/java/com/sevtinge/hyperceiler/ui/fragment/PhoneFragment.java
@@ -1,12 +1,18 @@
 package com.sevtinge.hyperceiler.ui.fragment;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.view.View;
 
 import com.sevtinge.hyperceiler.R;
 import com.sevtinge.hyperceiler.ui.base.BaseSettingsActivity;
 import com.sevtinge.hyperceiler.ui.fragment.base.SettingsPreferenceFragment;
 
+import moralnorm.preference.Preference;
+
 public class PhoneFragment extends SettingsPreferenceFragment {
+    Preference mPhone;
+
     @Override
     public int getContentResId() {
         return R.xml.phone;
@@ -14,9 +20,21 @@ public class PhoneFragment extends SettingsPreferenceFragment {
 
     @Override
     public View.OnClickListener addRestartListener() {
-        return view -> ((BaseSettingsActivity)getActivity()).showRestartDialog(
+        return view -> ((BaseSettingsActivity) getActivity()).showRestartDialog(
             getResources().getString(R.string.phone),
             "com.android.phone"
         );
+    }
+
+    @Override
+    public void initPrefs() {
+        mPhone = findPreference("prefs_key_phone_additional_network_settings");
+        mPhone.setOnPreferenceClickListener(preference -> {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+            intent.setComponent(new ComponentName("com.android.phone", "com.android.phone.SwitchDebugActivity"));
+            startActivity(intent);
+            return true;
+        });
     }
 }
