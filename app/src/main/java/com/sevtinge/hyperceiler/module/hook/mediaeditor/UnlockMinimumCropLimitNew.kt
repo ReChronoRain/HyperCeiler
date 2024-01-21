ba86d4694b65a24917601d5db10ef53317109efc
太卜司 · 柒柒 <80700814+mu7220@users.noreply.github.com>
2024-01-21 14:03:08 +0800

    更新依赖 & 修改部分 dexkit 写法
    Signed-off-by: mu7220 <80700814+mu7220@users.noreply.github.com>

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/mediaeditor/UnlockMinimumCropLimitNew.kt
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/mediaeditor/UnlockMinimumCropLimitNew.kt
@@ -1,7 +1,6 @@
 package com.sevtinge.hyperceiler.module.hook.mediaeditor
 
 import com.github.kyuubiran.ezxhelper.EzXHelper
-import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHooks
 import com.sevtinge.hyperceiler.module.base.BaseHook
 import com.sevtinge.hyperceiler.utils.DexKit.addUsingStringsEquals
@@ -9,7 +8,7 @@ import com.sevtinge.hyperceiler.utils.DexKit.dexKitBridge
 import java.lang.reflect.Modifier
 
 object UnlockMinimumCropLimitNew : BaseHook() {
-    private val mScreenCropViewMethodToNew by lazy {
+    private val mScreenCropViewMethod by lazy {
         dexKitBridge.findMethod {
             matcher {
                 declaredClass {
@@ -19,11 +18,7 @@ object UnlockMinimumCropLimitNew : BaseHook() {
                 returnType = "int"
                 modifiers = Modifier.FINAL
             }
-        }.map { it.getMethodInstance(EzXHelper.classLoader) }.toList()
-    }
-
-    private val mScreenCropViewMethodToOld by lazy {
-        dexKitBridge.findMethod {
+            // 老版本匹配
             matcher {
                 declaredClass {
                     addUsingStringsEquals("fixImageBounds %f,%f")
@@ -32,14 +27,11 @@ object UnlockMinimumCropLimitNew : BaseHook() {
                 returnType = "int"
                 modifiers = Modifier.FINAL
             }
-        }.single().getMethodInstance(EzXHelper.safeClassLoader)
+        }.map { it.getMethodInstance(EzXHelper.classLoader) }.toList()
     }
 
     override fun init() {
-        mScreenCropViewMethodToNew.createHooks {
-            returnConstant(0)
-        }
-        mScreenCropViewMethodToOld.createHook {
+        mScreenCropViewMethod.createHooks {
             returnConstant(0)
         }
     }
