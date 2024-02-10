0c5481235ee63122841da4ae7de8fe259cbd836c
HChen <HChen_LXi@outlook.com>
2024-02-10 17:06:47 +0800

    更新 ShellUtils.java

--- app/src/main/java/com/sevtinge/hyperceiler/utils/ShellUtils.java
+++ app/src/main/java/com/sevtinge/hyperceiler/utils/ShellUtils.java
@@ -26,7 +26,10 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ShellUtils {
     /**
@@ -262,7 +265,7 @@ public class ShellUtils {
      * 本工具使用简单的方法延续 Su/Sh 命令执行窗口，使得调用者无须频繁的执行 Su。
      * 调用示例:
      * <pre> {@code
-     * int result = new ShellUtils.OpenShellExecWindow("ls", true, true) {
+     * int result = new ShellUtils.OpenShellExecWindow("ls /data/", false, true) {
      *             @Override
      *             public void readOutput(String out) {
      *                 AndroidLogUtils.LogI("[Shell test]", "normal output: " + out);
@@ -272,24 +275,39 @@ public class ShellUtils {
      *             public void readError(String out) {
      *                 AndroidLogUtils.LogI("[Shell test]", "error output: " + out);
      *             }
+     *
+     *             @Override
+     *             public void result(String command, int result) {
+     *                 AndroidLogUtils.LogI("[Shell test]", "command: " + command + " result: " + result);
+     *             }
      *         }
-     *             .append("ls /data")
-     *             .append("mkdir -p /data/adb")
+     *             .append("mkdir /data/adb/")
      *             .append("touch /data/adb/test")
      *             .close();
      * }
      * 请在适当的时机调用 {@link OpenShellExecWindow#close} 用来释放资源。
      * int result 值为执行全部命令后 exit 的返回值，可以用来判断命令是否执行成功。
+     * 重写 {@link OpenShellExecWindow#result(String, int)} 方法可以实时获取每条命令的执行结果。
      * @author 焕晨HChen
      */
     public static class OpenShellExecWindow implements IOutput {
         private Process process;
         private DataOutputStream os;
+        private static IPassCommands passCommands;
+
+        private boolean needResult;
+
+        private int count = 1;
+
+        protected static void setICommand(IPassCommands pass) {
+            passCommands = pass;
+        }
 
         public OpenShellExecWindow(String command, boolean needRoot, boolean needResultMsg) {
             try {
                 OutPut.setOutputListen(this);
                 Error.setOutputListen(this);
+                needResult = needResultMsg;
                 process = Runtime.getRuntime().exec(needRoot ? "su" : "sh");
                 os = new DataOutputStream(process.getOutputStream());
                 os.write(command.getBytes());
@@ -300,23 +318,53 @@ public class ShellUtils {
                     OutPut output = new OutPut(process.getInputStream());
                     error.start();
                     output.start();
+                    if (passCommands != null)
+                        passCommands.passCommands(command);
+                    done(0);
                 }
             } catch (IOException e) {
                 AndroidLogUtils.LogE(ITAG.TAG, "OpenShellExecWindow E", e);
             }
         }
 
+        /**
+         * 追加需要执行的命令。
+         *
+         * @param command 命令
+         * @return this
+         */
         public OpenShellExecWindow append(String command) {
             try {
                 os.write(command.getBytes());
                 os.writeBytes("\n");
                 os.flush();
+                if (needResult) {
+                    if (passCommands != null)
+                        passCommands.passCommands(command);
+                    done(count);
+                    count = count + 1;
+                }
             } catch (IOException e) {
                 AndroidLogUtils.LogE(ITAG.TAG, "OpenShellExecWindow append E", e);
             }
             return this;
         }
 
+        private void done(int count) {
+            try {
+                os.writeBytes("echo \"The execution of command <" + count + "> is complete. Return value: <$?>\" 1>&2");
+                os.writeBytes("\n");
+                os.flush();
+            } catch (IOException e) {
+                AndroidLogUtils.LogE(ITAG.TAG, "OpenShellExecWindow done E", e);
+            }
+        }
+
+        /**
+         * 关闭 ShellWindow。
+         *
+         * @return 本 process 的最终执行结果
+         */
         public int close() {
             int result = -1;
             try {
@@ -338,12 +386,36 @@ public class ShellUtils {
             return result;
         }
 
+        /**
+         * 重写本方法可以实时获取常规流数据。
+         *
+         * @param out 常规流数据
+         */
         @Override
         public void readOutput(String out) {
         }
 
+        /**
+         * 重写本方法可以实时获取错误流数据。
+         *
+         * @param out 错误流数据
+         */
         @Override
         public void readError(String out) {
+        }
+
+        /**
+         * 重写本方法可以实时获取每条命令的执行结果。
+         *
+         * @param command 命令
+         * @param result  结果
+         */
+        @Override
+        public void result(String command, int result) {
+        }
+
+        protected interface IPassCommands {
+            void passCommands(String command);
         }
     }
 
@@ -363,7 +435,7 @@ public class ShellUtils {
         public void run() {
             try (BufferedReader br = new BufferedReader(new InputStreamReader(mInput))) {
                 String line;
-                while (Thread.currentThread().isInterrupted() && (line = br.readLine()) != null) {
+                while ((line = br.readLine()) != null) {
                     mIOutput.readOutput(line);
                 }
             } catch (IOException e) {
@@ -375,9 +447,13 @@ public class ShellUtils {
     private static class Error extends Thread {
         private final InputStream mInput;
         private static IOutput mIOutput;
+        private final Pattern pattern;
+        private final Command command;
 
         public Error(InputStream inputStream) {
             mInput = inputStream;
+            pattern = Pattern.compile(".*<(\\d+)>.*<(\\d+)>.*");
+            command = new Command();
         }
 
         public static void setOutputListen(IOutput iOutput) {
@@ -388,11 +464,35 @@ public class ShellUtils {
         public void run() {
             try (BufferedReader br = new BufferedReader(new InputStreamReader(mInput))) {
                 String line;
-                while (Thread.currentThread().isInterrupted() && (line = br.readLine()) != null) {
-                    mIOutput.readError(line);
+                while ((line = br.readLine()) != null) {
+                    Matcher matcher = pattern.matcher(line);
+                    if (matcher.find()) {
+                        String count = matcher.group(1);
+                        String result = matcher.group(2);
+                        if (result != null && count != null) {
+                            mIOutput.result(command.passCommands.get(Integer.parseInt(count)), Integer.parseInt(result));
+                        }
+                    } else {
+                        mIOutput.readError(line);
+                    }
                 }
             } catch (IOException e) {
                 AndroidLogUtils.LogE(ITAG.TAG, "Shell Error run E", e);
+            } catch (NumberFormatException f) {
+                AndroidLogUtils.LogE(ITAG.TAG, "Shell get result E", f);
+            }
+        }
+
+        private static class Command implements OpenShellExecWindow.IPassCommands {
+            public ArrayList<String> passCommands = new ArrayList<>();
+
+            public Command() {
+                OpenShellExecWindow.setICommand(this);
+            }
+
+            @Override
+            public void passCommands(String command) {
+                passCommands.add(command);
             }
         }
     }
@@ -401,5 +501,7 @@ public class ShellUtils {
         void readOutput(String out);
 
         void readError(String out);
+
+        void result(String command, int result);
     }
 }
