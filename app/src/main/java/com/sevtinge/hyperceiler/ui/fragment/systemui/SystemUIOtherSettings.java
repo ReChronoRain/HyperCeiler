e852c448fcff5e19cde8f2cfc6e721b54d31db1b
星夜萌 <157605784+xing0meng@users.noreply.github.com>
2024-02-16 15:57:12 +0800

    添加 系统界面-其他-原生音量条 (#364)
    * Update SystemUIOtherSettings.java
    
    * Update system_ui_other.xml
    
    * Update strings.xml
    
    * Update strings.xml
    
    * Update prefs_about_contributor.xml

--- app/src/main/java/com/sevtinge/hyperceiler/ui/fragment/systemui/SystemUIOtherSettings.java
+++ app/src/main/java/com/sevtinge/hyperceiler/ui/fragment/systemui/SystemUIOtherSettings.java
@@ -1,21 +1,21 @@
 /*
-  * This file is part of HyperCeiler.
+ * This file is part of HyperCeiler.
 
-  * HyperCeiler is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU Affero General Public License as
-  * published by the Free Software Foundation, either version 3 of the
-  * License.
+ * HyperCeiler is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License.
 
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU Affero General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
 
-  * You should have received a copy of the GNU Affero General Public License
-  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-  * Copyright (C) 2023-2024 HyperCeiler Contributions
-*/
+ * Copyright (C) 2023-2024 HyperCeiler Contributions
+ */
 package com.sevtinge.hyperceiler.ui.fragment.systemui;
 
 import static com.sevtinge.hyperceiler.utils.devicesdk.SystemSDKKt.isAndroidVersion;
@@ -23,13 +23,18 @@ import static com.sevtinge.hyperceiler.utils.devicesdk.SystemSDKKt.isMiuiVersion
 import static com.sevtinge.hyperceiler.utils.devicesdk.SystemSDKKt.isMoreAndroidVersion;
 import static com.sevtinge.hyperceiler.utils.devicesdk.SystemSDKKt.isMoreHyperOSVersion;
 
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
 import android.view.View;
+
+import androidx.annotation.NonNull;
 
 import com.sevtinge.hyperceiler.R;
 import com.sevtinge.hyperceiler.ui.base.BaseSettingsActivity;
 import com.sevtinge.hyperceiler.ui.fragment.base.SettingsPreferenceFragment;
 
 import moralnorm.preference.DropDownPreference;
+import moralnorm.preference.Preference;
 import moralnorm.preference.PreferenceCategory;
 import moralnorm.preference.SwitchPreference;
 
@@ -41,6 +46,7 @@ public class SystemUIOtherSettings extends SettingsPreferenceFragment {
     SwitchPreference mOriginCharge;
     SwitchPreference mMiuiMultiWinSwitch;
     SwitchPreference mBottomBar;
+    SwitchPreference mVolume;
     SwitchPreference mDisableBluetoothRestrict; // 禁用蓝牙临时关闭
 
     @Override
@@ -65,6 +71,7 @@ public class SystemUIOtherSettings extends SettingsPreferenceFragment {
         mDisableBluetoothRestrict = findPreference("prefs_key_system_ui_disable_bluetooth_restrict");
         mMiuiMultiWinSwitch = findPreference("prefs_key_system_ui_disable_miui_multi_win_switch");
         mBottomBar = findPreference("prefs_key_system_ui_disable_bottombar");
+        mVolume = findPreference("prefs_key_system_ui_disable_volume");
 
         mChargeAnimationTitle.setVisible(!isMoreHyperOSVersion(1f));
         mMonetOverlay.setVisible(!isAndroidVersion(30));
@@ -72,5 +79,26 @@ public class SystemUIOtherSettings extends SettingsPreferenceFragment {
         mDisableBluetoothRestrict.setVisible(isMiuiVersion(14f) && isMoreAndroidVersion(31));
         mMiuiMultiWinSwitch.setVisible(isMoreHyperOSVersion(1f) && isMoreAndroidVersion(34));
         mBottomBar.setVisible(isMoreHyperOSVersion(1f) && isMoreAndroidVersion(34));
+
+        mVolume.setOnPreferenceChangeListener(
+            new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(@NonNull Preference preference, Object o) {
+                    ComponentName componentName = new ComponentName("miui.systemui.plugin",
+                        "miui.systemui.volume.VolumeDialogPlugin");
+                    PackageManager packageManager = getContext().getPackageManager();
+                    if ((boolean) o) {
+                        packageManager.setComponentEnabledSetting(componentName,
+                            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                            PackageManager.DONT_KILL_APP);
+                    } else {
+                        packageManager.setComponentEnabledSetting(componentName,
+                            PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                            PackageManager.DONT_KILL_APP);
+                    }
+                    return true;
+                }
+            }
+        );
     }
 }
