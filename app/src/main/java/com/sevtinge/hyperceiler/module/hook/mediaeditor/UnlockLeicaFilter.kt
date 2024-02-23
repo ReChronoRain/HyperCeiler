c3dc91a2dda9d4910b21ffc0787062da9e0205b9
太卜司 · 柒柒 <80700814+mu7220@users.noreply.github.com>
2024-02-23 13:20:51 +0800

    fix: 小米相册_编辑 适配 1.6.5.10.2 (除了徕卡滤镜.png)
    Signed-off-by: mu7220 <80700814+mu7220@users.noreply.github.com>

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/mediaeditor/UnlockLeicaFilter.kt
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/mediaeditor/UnlockLeicaFilter.kt
@@ -19,41 +19,16 @@
 package com.sevtinge.hyperceiler.module.hook.mediaeditor
 
 import com.github.kyuubiran.ezxhelper.EzXHelper
-import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHooks
+import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
 import com.sevtinge.hyperceiler.module.base.BaseHook
 import com.sevtinge.hyperceiler.module.base.dexkit.DexKit.addUsingStringsEquals
 import com.sevtinge.hyperceiler.module.base.dexkit.DexKit.dexKitBridge
+import java.lang.reflect.Method
 import java.lang.reflect.Modifier
 
 object UnlockLeicaFilter : BaseHook() {
-    override fun init() {
-        val leica1 = dexKitBridge.findMethod {
-            matcher {
-                // 1.5 找不到，遂放弃
-                /*addCall {
-                    addCall {
-                        addUsingStringsEquals("context.getString(R.string.filter_category_leica)")
-                        returnType = "java.util.List"
-                        modifiers = Modifier.FINAL
-                        // paramCount = 0
-                    }
-                    returnType = "boolean"
-                }*/
-                addCall {
-                    addCall {
-                        addUsingStringsEquals("wayne")
-                        returnType = "java.util.List"
-                        modifiers = Modifier.STATIC
-                    }
-                    returnType = "boolean"
-                }
-                returnType = "boolean"
-                modifiers = Modifier.STATIC
-                paramCount = 0
-            }
-        }.map { it.getMethodInstance(EzXHelper.classLoader) }.toList()
-
-        val leica2 = dexKitBridge.findMethod {
+    private val leica by lazy {
+        dexKitBridge.findMethod {
             matcher {
                 // 仅适配 1.6.0.0.5
                 declaredClass {
@@ -63,26 +38,20 @@ object UnlockLeicaFilter : BaseHook() {
                 returnType = "boolean"
                 paramCount = 0
             }
-        }.map { it.getMethodInstance(EzXHelper.classLoader) }.toList()
+        }.map { it.getMethodInstance(EzXHelper.classLoader) }.toSet()
+    }
 
-        // debug 用
-        for (l in leica1) {
-            logI(TAG, "LeicaA name is $l")
-        }
-        for (l in leica2) {
-            logI(TAG,"LeicaB name is $l")
-        }
+    override fun init() {
+        leica.forEach { method ->
+            logI(TAG, "Leica name is $method") // debug 用
 
-        try {
-            leica1.createHooks {
-                returnConstant(true)
-            }
-            leica2.createHooks {
-                returnConstant(true)
-            }
-        } catch (t: Throwable) {
-            logE(TAG, "no find leica method")
-            return
+            returnTrue(method)
+        }
+    }
+
+    private fun returnTrue(method: Method) {
+        method.createHook {
+            returnConstant(true)
         }
     }
 }
