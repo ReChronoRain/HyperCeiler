ee5448f5550e8eac025d8d4159dc30908e4c16e0
Neko Qiqi <lingqiqi233@gmail.com>
2024-03-13 23:40:56 +0800

    optimization: UnlockClipboard.kt (Use a safer hook method to avoid a series of problems caused by turning off MIUI optimization for a short period of time) by wommo
    Signed-off-by: Neko Qiqi <lingqiqi233@gmail.com>

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemui/UnlockClipboard.kt
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemui/UnlockClipboard.kt
@@ -18,55 +18,48 @@
  */
 package com.sevtinge.hyperceiler.module.hook.systemui
 
+import android.content.*
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
+import com.github.kyuubiran.ezxhelper.ObjectUtils.getObjectOrNullAs
+import com.github.kyuubiran.ezxhelper.ObjectUtils.invokeMethodBestMatch
+import com.github.kyuubiran.ezxhelper.ObjectUtils.setObject
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 import com.sevtinge.hyperceiler.module.base.*
-import com.sevtinge.hyperceiler.utils.devicesdk.*
-import de.robv.android.xposed.*
 
 object UnlockClipboard : BaseHook() {
     override fun init() {
         // hook 点来自 淡い夏
-        // 解锁原生剪切板编辑框 和 截屏编辑框
-        var hook: XC_MethodHook.Unhook? = null
-
-        loadClass("com.android.systemui.clipboardoverlay.ClipboardListener").methodFinder()
-            .filterByName("start")
-            .first().createHook {
+        // 解锁原生剪切板编辑框
+        // 新方法来自 WOMMO
+        val clazzClipboardListener =
+            loadClass("com.android.systemui.clipboardoverlay.ClipboardListener")
+        if (clazzClipboardListener.declaredFields.any {
+                it.name == "sCtsTestPkgList"
+            }) clazzClipboardListener.methodFinder().filterByName("onPrimaryClipChanged")
+            .filterNonAbstract().single().createHook {
                 before {
-                    hook = if (isMoreAndroidVersion(34)) {
-                        // 给正常 Android 14 HyperOS 用户用的
-                        loadClass("com.miui.systemui.modulesettings.DeveloperSettings\$Companion").methodFinder()
-                            .filterByName("isMiuiOptimizationEnabled")
-                            .first().createHook {
-                                returnConstant(false)
-                            }
-                    } else {
-                        // 给正常 Android 13 用户用的
-                        loadClass("com.miui.systemui.SettingsManager").methodFinder()
-                            .filterByName("getMiuiOptimizationEnabled")
-                            .first().createHook {
-                                returnConstant(false)
-                            }
+                    val mClipboardManager =
+                        getObjectOrNullAs<ClipboardManager>(it.thisObject, "mClipboardManager")!!
+                    val primaryClipSource =
+                        invokeMethodBestMatch(mClipboardManager, "getPrimaryClipSource") as String
+                    val oldList =
+                        getObjectOrNullAs<List<String>>(it.thisObject, "sCtsTestPkgList")!!
+                    val newList = mutableListOf<String>().apply {
+                        addAll(oldList)
+                        if (!contains(primaryClipSource)) add(primaryClipSource)
                     }
-                }
-                after {
-                    // 必须 unhook()
-                    // 否则会出现各种奇怪的问题
-                    hook?.unhook()
+                    setObject(it.thisObject, "sCtsTestPkgList", newList)
                 }
             }
-
-        // 14u 泄露包的 hook 点，需要改写 getPrimaryClip，目前没啥法子，等后续有人来写
-        /*loadClass("com.android.systemui.clipboardoverlay.ClipboardListener").methodFinder()
-            .filterByName("onPrimaryClipChanged")
-            .first().createHook {
+        else clazzClipboardListener.methodFinder().filterByName("start").filterNonAbstract()
+            .single().createHook {
                 before {
-                    val getClipboardManager = ObjectUtils.getObjectOrNull(it.thisObject, "mClipboardManager") ?: return@before
-                    val getPrimaryClip = getClipboardManager.callMethod("getPrimaryClip")
-                    logE(TAG, "getPrimaryClip is $getPrimaryClip")
+                    val mClipboardManager =
+                        getObjectOrNullAs<ClipboardManager>(it.thisObject, "mClipboardManager")!!
+                    mClipboardManager.addPrimaryClipChangedListener(it.thisObject as ClipboardManager.OnPrimaryClipChangedListener?)
+                    it.result = null
                 }
-            }*/
+            }
     }
 }
