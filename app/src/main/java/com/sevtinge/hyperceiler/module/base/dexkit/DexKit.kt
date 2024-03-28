1a815fe6263d987b0be5ec44f976099fea3a8969
HChen <HChen_LXi@outlook.com>
2024-03-28 17:58:48 +0800

    opt: InitDexKit

--- app/src/main/java/com/sevtinge/hyperceiler/module/base/dexkit/DexKit.kt
+++ app/src/main/java/com/sevtinge/hyperceiler/module/base/dexkit/DexKit.kt
@@ -18,34 +18,18 @@
 */
 package com.sevtinge.hyperceiler.module.base.dexkit
 
-import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
-import org.luckypray.dexkit.DexKitBridge
-import org.luckypray.dexkit.query.enums.StringMatchType
-import org.luckypray.dexkit.query.matchers.ClassMatcher
-import org.luckypray.dexkit.query.matchers.MethodMatcher
+import org.luckypray.dexkit.*
+import org.luckypray.dexkit.query.enums.*
+import org.luckypray.dexkit.query.matchers.*
 
 /**
  * DexKit 工具
  */
 object DexKit {
-    var hostDir: String? = null
-    var isInitialized = false
+    var initDexKit: InitDexKit? = null
+
     val dexKitBridge: DexKitBridge by lazy {
-        InitDexKit.init()
-    }
-
-    /**
-     * 初始化 DexKit 的 apk 完整路径
-     */
-    fun initDexKit(loadPackageParam: LoadPackageParam) {
-        hostDir = loadPackageParam.appInfo.sourceDir
-    }
-
-    /**
-     * 关闭 DexKit bridge
-     */
-    fun closeDexKit() {
-        if (isInitialized) dexKitBridge.close()
+        initDexKit!!.init()
     }
 
     /**
