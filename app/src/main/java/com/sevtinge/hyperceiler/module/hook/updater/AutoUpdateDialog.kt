1829f4029c6184b3f747756d33d826135ca875a4
Horange <horange321@163.com>
2024-03-30 10:18:04 +0800

    <Add>(AutoUpdateDialog)禁用流量自动更新

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/updater/AutoUpdateDialog.kt
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/updater/AutoUpdateDialog.kt
@@ -1,20 +1,42 @@
 package com.sevtinge.hyperceiler.module.hook.updater
 
+import com.github.kyuubiran.ezxhelper.*
 import com.sevtinge.hyperceiler.module.base.BaseHook
+import de.robv.android.xposed.XposedBridge.*
 
 class AutoUpdateDialog : BaseHook() {
     override fun init() {
-        //TODO 不写死程序
-        findAndHookMethod(
-            "com.android.updater.i1",
-            "Z2",
-            Boolean::class.java,
-            Boolean::class.java,
-            object : replaceHookedMethod() {
-                override fun replace(param: MethodHookParam?): Any {
-                    return 0
+        // TODO 不写死程序
+        try {
+            findAndHookMethod(
+                "com.android.updater.i1",
+                "Z2",
+                Boolean::class.java,
+                Boolean::class.java,
+                object : replaceHookedMethod() {
+                    override fun replace(param: MethodHookParam?): Any {
+                        return 0
+                    }
                 }
-            }
-        )
+            )
+        } catch (e: Exception) {
+            log(e)
+        }
+
+        try {
+            findAndHookMethod(
+                "com.android.updater.i1",
+                "q3",
+                Long::class.java,
+                Int::class.java,
+                object : replaceHookedMethod() {
+                    override fun replace(param: MethodHookParam?): Any {
+                        return 0
+                    }
+                }
+            )
+        } catch (e: Exception) {
+            log(e)
+        }
     }
 }
