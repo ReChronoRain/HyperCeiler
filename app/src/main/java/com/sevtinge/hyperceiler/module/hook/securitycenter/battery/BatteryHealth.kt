5b4ef955a6ed416ee91812c86343038057e993af
Horange <horange321@163.com>
2024-04-28 23:31:23 +0800

    <Refactor>dexKit 电池健康度

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/securitycenter/battery/BatteryHealth.kt
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/securitycenter/battery/BatteryHealth.kt
@@ -1,34 +1,78 @@
 package com.sevtinge.hyperceiler.module.hook.securitycenter.battery
 
-import android.os.*
 import com.github.kyuubiran.ezxhelper.*
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
 import com.sevtinge.hyperceiler.module.base.*
-import com.sevtinge.hyperceiler.module.base.dexkit.*
 import com.sevtinge.hyperceiler.utils.*
+import com.sevtinge.hyperceiler.module.base.dexkit.DexKit
 import org.luckypray.dexkit.query.enums.*
 
 
 object BatteryHealth : BaseHook() {
+    private val dexKitBridge = DexKit.getDexKitBridge()
+
     private val getSecurityBatteryHealth by lazy {
-        DexKit.getDexKitBridge().findMethod {
+        dexKitBridge.findMethod {
             matcher {
                 addUsingString("battery_health_soh", StringMatchType.Equals)
             }
         }.single().getMethodInstance(EzXHelper.classLoader)
     }
 
+    private val c = dexKitBridge.findClass {
+        searchPackages("com.miui.powercenter.nightcharge")
+        findFirst = true
+        matcher {
+            className = "com.miui.powercenter.nightcharge.SmartChargeFragment"
+        }
+    }
+
+    private val onc = dexKitBridge.findMethod {
+        findFirst = true
+        searchInClass(c)
+        matcher {
+            name = "onCreatePreferences"
+        }
+    }.single().getMethodInstance(EzXHelper.classLoader)
+
+    private val cc = dexKitBridge.findClass {
+        searchPackages("com.miui.powercenter.nightcharge")
+        findFirst = true
+        matcher {
+            methods {
+                add {
+                    name = "handleMessage"
+                }
+            }
+        }
+    }
+
+    val m = dexKitBridge.findMethod {
+        searchInClass(cc)
+        findFirst = true
+        matcher {
+            name = "handleMessage"
+        }
+    }.single().getMethodInstance(EzXHelper.classLoader)
+
+    private lateinit var gff: Any
+
+
     override fun init() {
+        onc.createHook {
+            after {
+                gff = it.thisObject
+                    .callMethod("findPreference", "reference_battery_health")!!
+            }
+        }
+
         getSecurityBatteryHealth.createHook {
             after { params ->
                 val health = params.args[0] as Int // 获取手机管家内部的健康度
 
-                findClassIfExists("com.miui.powercenter.nightcharge.SmartChargeFragment\$c").let { c ->
-                    c.hookAfterMethod("handleMessage", Message::class.java) {
-                        // TODO hardcode。想办法改进
-                        it.thisObject.getObjectField("a")!!.callMethod("get")!!.let { cc ->
-                            cc.getObjectField("c")!!.callMethod("setText", "$health %")
-                        }
+                m.createHook {
+                    after {
+                        gff.callMethod("setText", "$health %")
                     }
                 }
             }
