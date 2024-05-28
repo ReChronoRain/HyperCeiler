52080bfc2a91150f7e461dd8383e118545100d91
HChenX <HChen_LXi@outlook.com>
2024-05-28 23:01:11 +0800

    优化: 抽屉高级材质性能

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/home/drawer/AllAppsContainerViewSuperBlur.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/home/drawer/AllAppsContainerViewSuperBlur.java
@@ -2,7 +2,6 @@ package com.sevtinge.hyperceiler.module.hook.home.drawer;
 
 import static com.sevtinge.hyperceiler.utils.devicesdk.DisplayUtils.dp2px;
 
-import android.graphics.Color;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
@@ -15,6 +14,8 @@ import com.sevtinge.hyperceiler.utils.blur.MiBlurUtilsKt;
 import de.robv.android.xposed.XposedHelpers;
 
 public class AllAppsContainerViewSuperBlur extends BaseHook {
+    private boolean isBlur;
+
     @Override
     public void init() throws NoSuchMethodException {
         findAndHookMethod("com.miui.home.launcher.allapps.BaseAllAppsContainerView",
@@ -30,10 +31,8 @@ public class AllAppsContainerViewSuperBlur extends BaseHook {
                         view.getLayoutParams().height = FrameLayout.LayoutParams.MATCH_PARENT;
                         view.getLayoutParams().width = FrameLayout.LayoutParams.MATCH_PARENT;
                         view.setBackgroundColor(mPrefsMap.getInt("home_drawer_blur_super_bg_color", -1));
-                        // MiBlurUtils.setContainerPassBlur(view, 200);
-                        MiBlurUtils.setMiViewBlurMode(view, 2);
-                        // MiBlurUtils.clearMiBackgroundBlendColor(view);
-                        MiBlurUtils.addMiBackgroundBlendColor(view, Color.argb(255, 0, 0, 0), 103);
+                        if (!isBlur) MiBlurUtils.setMiViewBlurMode(view, 2);
+                        isBlur = true;
                         MiBlurUtilsKt.INSTANCE.setBlurRoundRect(frameLayout, dp2px(
                                 mPrefsMap.getInt("home_drawer_blur_super_radius", 30)));
                         appsView.addView(frameLayout, 0);
