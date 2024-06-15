fee1092dca39f601d301391214b6868e837a0979
HChenX <HChen_LXi@outlook.com>
2024-06-15 16:59:33 +0800

    优化: 抛错增加堆栈信息

--- app/src/main/java/com/sevtinge/hyperceiler/module/base/BaseHook.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/base/BaseHook.java
@@ -1,27 +1,30 @@
 /*
-  * This file is part of HyperCeiler.
+ * This file is part of HyperCeiler.
 
-  * HyperCeiler is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU Affero General Public License as
-  * published by the Free Software Foundation, either version 3 of the
-  * License.
+ * HyperCeiler is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License.
 
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU Affero General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
 
-  * You should have received a copy of the GNU Affero General Public License
-  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-  * Copyright (C) 2023-2024 HyperCeiler Contributions
-*/
+ * Copyright (C) 2023-2024 HyperCeiler Contributions
+ */
 package com.sevtinge.hyperceiler.module.base;
 
 import static com.sevtinge.hyperceiler.utils.log.LogManager.logLevel;
 
 import com.sevtinge.hyperceiler.module.base.tool.HookTool;
 import com.sevtinge.hyperceiler.module.base.tool.ResourcesTool;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
@@ -45,7 +48,10 @@ public abstract class BaseHook extends HookTool {
                 logI(TAG, lpparam.packageName, "Hook Success.");
             }
         } catch (Throwable t) {
-            if (logLevel >= 1) logE(TAG, lpparam.packageName, "Hook Failed", t);
+            StringWriter stringWriter = new StringWriter();
+            PrintWriter printWriter = new PrintWriter(stringWriter);
+            t.printStackTrace(printWriter);
+            if (logLevel >= 1) logE(TAG, lpparam.packageName, "Hook Failed: " + stringWriter);
         }
     }
 
