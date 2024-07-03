f7e23ae7c74a253a321322e0aadb1d5cb7555d20
Your Name <you@example.com>
2024-07-03 11:57:40 +0800

    修复“为每个应用管理语言”失效的问题

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemsettings/LanguageMenuShowAllApps.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemsettings/LanguageMenuShowAllApps.java
@@ -24,16 +24,21 @@ import com.sevtinge.hyperceiler.module.base.BaseHook;
 
 import java.util.Objects;
 
-import de.robv.android.xposed.XC_MethodHook;
-
 public class LanguageMenuShowAllApps extends BaseHook {
     @Override
     public void init() throws NoSuchMethodException {
         findAndHookMethod("android.util.FeatureFlagUtils", "isEnabled", Context.class, String.class, new MethodHook() {
             @Override
-            protected void before(MethodHookParam param) throws Throwable {
+            protected void before(MethodHookParam param) {
                 String param1 = (String) param.args[1];
                 if (Objects.equals(param1, "settings_app_locale_opt_in_enabled")) param.setResult(false);
+            }
+        });
+
+        findAndHookMethod("com.android.settings.applications.AppLocaleUtil", "isAppLocaleSupported", Context.class, String.class, new MethodHook() {
+            @Override
+            protected void before(MethodHookParam param) {
+                param.setResult(true);
             }
         });
     }
