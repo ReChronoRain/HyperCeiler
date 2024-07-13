c724ab9372ac019ca9e5330b99fd33d5bd1d53ee
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2024-07-13 22:50:13 +0800

    opt #747

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/securitycenter/other/SkipCountDownLimit.kt
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/securitycenter/other/SkipCountDownLimit.kt
@@ -2,6 +2,7 @@ package com.sevtinge.hyperceiler.module.hook.securitycenter.other
 
 import android.app.*
 import android.os.*
+import com.github.kyuubiran.ezxhelper.*
 import com.github.kyuubiran.ezxhelper.ClassLoaderProvider.safeClassLoader
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
@@ -9,62 +10,54 @@ import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinde
 import com.sevtinge.hyperceiler.module.base.*
 import com.sevtinge.hyperceiler.module.base.dexkit.*
 import com.sevtinge.hyperceiler.module.base.dexkit.DexKitTool.toMethod
+import com.sevtinge.hyperceiler.utils.*
+import de.robv.android.xposed.*
 
 /**
- * @author Xzakota
+ * @author Xzakota & Sevtinge
  */
 object SkipCountDownLimit : BaseHook() {
+
+    private val skipCountDownLimitMethod by lazy {
+        DexKit.getDexKitBridge("SkipCountDownLimit") {
+            it.findMethod {
+                matcher {
+                    declaredClass("com.miui.permcenter.privacymanager.InterceptBaseFragment")
+                    usingNumbers(-1, 0)
+                }
+            }.single().getMethodInstance(safeClassLoader)
+        }.toMethod()
+    }
+
     override fun init() {
-        when (mPrefsMap.getStringAsInt("security_center_skip_count_down_limit_type", 0)) {
-            0 -> {
-                loadClass("com.miui.permcenter.privacymanager.model.InterceptBaseActivity").methodFinder()
-                    .filterByName("onCreate")
-                    .filterByParamTypes(Bundle::class.java)
-                    .first().createHook {
-                        before { param ->
-                            val feat: Bundle.() -> Unit = {
-                                // putInt("KEY_STEP_COUNT", 0)
-                                putInt("KET_STEP_COUNT", 0)
-                                putBoolean("KEY_ALLOW_ENABLE", true)
-                            }
-                            param.args.first().let {
-                                val bundle = it as? Bundle?
-                                if (bundle == null) {
-                                    param.args[0] = Bundle().apply(feat)
-                                } else {
-                                    bundle.feat()
-                                }
+        if (!mPrefsMap.getBoolean("security_center_skip_count_down_limit_direct")) {
+            loadClass("com.miui.permcenter.privacymanager.model.InterceptBaseActivity").methodFinder()
+                .filterByName("onCreate")
+                .filterByParamTypes(Bundle::class.java)
+                .first().createHook {
+                    before { param ->
+                        val feat: Bundle.() -> Unit = {
+                            // putInt("KEY_STEP_COUNT", 0)
+                            putInt("KET_STEP_COUNT", 0)
+                            putBoolean("KEY_ALLOW_ENABLE", true)
+                        }
+                        param.args.first().let {
+                            val bundle = it as? Bundle?
+                            if (bundle == null) {
+                                param.args[0] = Bundle().apply(feat)
+                            } else {
+                                bundle.feat()
                             }
                         }
                     }
-            }
-
-            1 -> {
-                val hookClassName = "com.miui.permcenter.privacymanager.InterceptBaseFragment"
-                loadClass(hookClassName).methodFinder()
-                    .filterByName("onCreate")
-                    .filterByParamTypes(Bundle::class.java)
-                    .first().createHook {
-                        after { param ->
-                            val getAppCompatActivity = DexKit.getDexKitBridge("SkipCountDownLimit") {
-                                it.findMethod {
-                                    matcher {
-                                        declaredClass {
-                                            className = hookClassName
-                                        }
-                                        name = "getAppCompatActivity"
-                                    }
-                                }.single().getMethodInstance(safeClassLoader)
-                            }.toMethod()
-
-                            getAppCompatActivity.invoke(param.thisObject).run {
-                                this as Activity
-                                setResult(-1)
-                                finish()
-                            }
-                        }
-                    }
-            }
+                }
+        } else {
+            val hookClassName = "com.miui.permcenter.privacymanager.InterceptBaseFragment"
+            findAndHookMethod(hookClassName, "onCreate", Bundle::class.java, object : MethodHook() {
+                override fun before(param: MethodHookParam?) {
+                    XposedHelpers.callMethod(param?.thisObject, skipCountDownLimitMethod.name, true)
+                }
+            })
         }
     }
 }
