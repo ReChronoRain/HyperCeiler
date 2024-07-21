2e2db1f44a054e865f30404a9d5193b2c59a32f8
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2024-07-21 14:29:21 +0800

    feat: getapps - disable ads

--- app/src/main/java/com/sevtinge/hyperceiler/module/app/GetApps.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/app/GetApps.java
@@ -21,6 +21,7 @@ package com.sevtinge.hyperceiler.module.app;
 import com.sevtinge.hyperceiler.module.base.BaseModule;
 import com.sevtinge.hyperceiler.module.base.HookExpand;
 import com.sevtinge.hyperceiler.module.hook.getapps.DeviceModify;
+import com.sevtinge.hyperceiler.module.hook.getapps.DisableAds;
 import com.sevtinge.hyperceiler.module.hook.getapps.DisablePackageMonitor;
 
 @HookExpand(pkg = "com.xiaomi.market", isPad = false, tarAndroid = 33)
@@ -28,6 +29,7 @@ public class GetApps extends BaseModule {
 
     @Override
     public void handleLoadPackage() {
+        initHook(new DisableAds(), mPrefsMap.getBoolean("market_disable_ads"));
         initHook(new DeviceModify(), mPrefsMap.getStringAsInt("market_device_modify_new", 0) != 0);
 
         initHook(DisablePackageMonitor.INSTANCE, mPrefsMap.getBoolean("market_package_monitor"));
