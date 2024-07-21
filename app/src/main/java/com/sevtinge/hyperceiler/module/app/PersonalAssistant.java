306a6bcdb74e4f3fbfc30a07cb9f86ffeef51eab
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2024-07-21 17:23:14 +0800

    feat: personal asststant - set travel notification spotlight in status bar max width
    操你妈小米默认只给到60这谁显示的完

--- app/src/main/java/com/sevtinge/hyperceiler/module/app/PersonalAssistant.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/app/PersonalAssistant.java
@@ -23,6 +23,7 @@ import com.sevtinge.hyperceiler.module.base.HookExpand;
 import com.sevtinge.hyperceiler.module.hook.personalassistant.BlurPersonalAssistant;
 import com.sevtinge.hyperceiler.module.hook.personalassistant.BlurPersonalAssistantBackGround;
 import com.sevtinge.hyperceiler.module.hook.personalassistant.EnableFoldWidget;
+import com.sevtinge.hyperceiler.module.hook.personalassistant.SetTravelNotificationStatusBarInfoMaxWidth;
 import com.sevtinge.hyperceiler.module.hook.personalassistant.UnlockWidgetCountLimit;
 
 @HookExpand(pkg = "com.miui.personalassistant", isPad = false, tarAndroid = 33)
@@ -30,7 +31,6 @@ public class PersonalAssistant extends BaseModule {
 
     @Override
     public void handleLoadPackage() {
-
         // initHook(new BlurOverlay(), false);
         initHook(new EnableFoldWidget(), mPrefsMap.getBoolean("personal_assistant_fold_widget_enable"));
         initHook(new UnlockWidgetCountLimit(), mPrefsMap.getBoolean("personal_assistant_unlock_widget_count_limit"));
@@ -40,6 +40,8 @@ public class PersonalAssistant extends BaseModule {
         } else if (mPrefsMap.getStringAsInt("personal_assistant_value", 0) == 1) {
             initHook(BlurPersonalAssistantBackGround.INSTANCE, true);
         }
+
+        initHook(new SetTravelNotificationStatusBarInfoMaxWidth(), mPrefsMap.getInt("personal_assistant_set_tv_notif_info_max_width", 60) != 60);
     }
 
 }
