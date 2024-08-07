ba31fc13fccb7888db73d80acf6064d587ba5bc1
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2024-08-07 22:56:51 +0800

    feat: file explorer - unlock file parse

--- app/src/main/java/com/sevtinge/hyperceiler/module/app/FileExplorer.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/app/FileExplorer.java
@@ -21,6 +21,7 @@ package com.sevtinge.hyperceiler.module.app;
 import com.sevtinge.hyperceiler.module.base.BaseModule;
 import com.sevtinge.hyperceiler.module.base.HookExpand;
 import com.sevtinge.hyperceiler.module.hook.fileexplorer.SelectName;
+import com.sevtinge.hyperceiler.module.hook.fileexplorer.UnlockFileParse;
 import com.sevtinge.hyperceiler.module.hook.various.UnlockSuperClipboard;
 
 @HookExpand(pkg = "com.android.fileexplorer", isPad = false, tarAndroid = 33)
@@ -28,6 +29,7 @@ public class FileExplorer extends BaseModule {
 
     @Override
     public void handleLoadPackage() {
+        initHook(new UnlockFileParse(), mPrefsMap.getBoolean("file_explorer_unlock_file_parse"));
         initHook(SelectName.INSTANCE, mPrefsMap.getBoolean("file_explorer_can_selectable") || mPrefsMap.getBoolean("file_explorer_is_single_line"));
         initHook(UnlockSuperClipboard.INSTANCE, mPrefsMap.getStringAsInt("various_super_clipboard_e", 0) != 0);
     }
