566d2fb81138b3fe86c9ad9568b076e6837a51d7
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2024-08-09 00:11:27 +0800

    fix: package installer - display more pkg info

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/packageinstaller/DisableInstallerFullSafeVersion.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/packageinstaller/DisableInstallerFullSafeVersion.java
@@ -1,16 +1,24 @@
 package com.sevtinge.hyperceiler.module.hook.packageinstaller;
 
+import static de.robv.android.xposed.XposedHelpers.setStaticBooleanField;
+
 import com.sevtinge.hyperceiler.module.base.BaseHook;
 import com.sevtinge.hyperceiler.module.base.dexkit.DexKit;
 import com.sevtinge.hyperceiler.module.base.dexkit.IDexKit;
 
 import org.luckypray.dexkit.DexKitBridge;
+import org.luckypray.dexkit.query.FindField;
 import org.luckypray.dexkit.query.FindMethod;
+import org.luckypray.dexkit.query.matchers.ClassMatcher;
+import org.luckypray.dexkit.query.matchers.FieldMatcher;
 import org.luckypray.dexkit.query.matchers.MethodMatcher;
+import org.luckypray.dexkit.result.FieldData;
 import org.luckypray.dexkit.result.MethodData;
 
 import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
 public class DisableInstallerFullSafeVersion extends BaseHook {
     @Override
@@ -32,5 +40,20 @@ public class DisableInstallerFullSafeVersion extends BaseHook {
                 param.setResult(false);
             }
         });
+        Field field = (Field) DexKit.getDexKitBridge("FullSecurityProtectVersion", new IDexKit() {
+            @Override
+            public AnnotatedElement dexkit(DexKitBridge bridge) throws ReflectiveOperationException {
+                FieldData fieldData = bridge.findField(FindField.create()
+                        .matcher(FieldMatcher.create()
+                                .declaredClass(ClassMatcher.create()
+                                        .usingStrings("FullSafeHelper")
+                                )
+                                .type(boolean.class)
+                                .modifiers(Modifier.FINAL)
+                        )).singleOrNull();
+                return fieldData.getFieldInstance(lpparam.classLoader);
+            }
+        });
+        setStaticBooleanField(field.getDeclaringClass(), field.getName(), false);
     }
 }
