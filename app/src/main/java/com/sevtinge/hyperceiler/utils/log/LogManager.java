edee6f4a236ba2e293301c318139fd3201dcdca2
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2024-08-27 14:40:47 +0800

    opt: logger alive check

--- app/src/main/java/com/sevtinge/hyperceiler/utils/log/LogManager.java
+++ app/src/main/java/com/sevtinge/hyperceiler/utils/log/LogManager.java
@@ -75,18 +75,18 @@ public class LogManager {
             if (logFiles.size() == 1) {
                 String fileName = logFiles.get(0);
                 String filePath = "/data/adb/lspd/log/" + fileName;
-                String grepOutput = safeExecCommandWithRoot("grep -q 'LSPosed-Bridge' " + filePath + " && echo 'FOUND' || echo 'EMPTY'");
-
-                if (!grepOutput.trim().equals("FOUND")) {
+                String firstLine = safeExecCommandWithRoot("head -n 1 " + filePath).trim();
+                if (firstLine.equals("----part 1 start----")) {
+                    String grepOutput = safeExecCommandWithRoot("grep -q 'LSPosed-Bridge' " + filePath + " && echo 'FOUND' || echo 'EMPTY'");
+                    if (!grepOutput.trim().equals("FOUND")) {
+                        LOGGER_CHECKER_ERR_CODE = "EMPTY_XPOSED_LOG_FILE";
+                        return false;
+                    }
+                } else if (firstLine.equals("")) {
                     LOGGER_CHECKER_ERR_CODE = "EMPTY_XPOSED_LOG_FILE";
                     return false;
                 }
             } else if (logFiles.isEmpty()) {
-                LOGGER_CHECKER_ERR_CODE = "NO_XPOSED_LOG_FILE";
-                return false;
-            }
-
-            if (Objects.equals(safeExecCommandWithRoot("ls /data/adb/lspd/log/ | grep \"^modules_.*\\.log\""), "")) {
                 LOGGER_CHECKER_ERR_CODE = "NO_XPOSED_LOG_FILE";
                 return false;
             }
