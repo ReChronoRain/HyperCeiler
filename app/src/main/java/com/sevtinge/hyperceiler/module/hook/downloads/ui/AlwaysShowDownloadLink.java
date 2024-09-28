d99a68d6cf50fd5c6265b056d56d4281c5528eb2
frank <frank_782@qq.com>
2024-09-28 18:28:49 +0800

    opt - AlwaysShowDownloadLink: using dexkit

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/downloads/ui/AlwaysShowDownloadLink.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/downloads/ui/AlwaysShowDownloadLink.java
@@ -22,23 +22,64 @@ import static de.robv.android.xposed.XposedHelpers.getObjectField;
 import static de.robv.android.xposed.XposedHelpers.setObjectField;
 
 import com.sevtinge.hyperceiler.module.base.BaseHook;
+import com.sevtinge.hyperceiler.module.base.dexkit.DexKit;
+import com.sevtinge.hyperceiler.module.base.dexkit.IDexKit;
+
+import org.luckypray.dexkit.DexKitBridge;
+import org.luckypray.dexkit.query.FindMethod;
+import org.luckypray.dexkit.query.*;
+import org.luckypray.dexkit.query.matchers.*;
+import org.luckypray.dexkit.result.ClassData;
+import org.luckypray.dexkit.result.MethodData;
+
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Method;
 
 public class AlwaysShowDownloadLink extends BaseHook {
+
     @Override
     public void init() {
-        findAndHookMethod("h1.h", "F", String.class,new MethodHook() {
+
+        Class<?> class1 = (Class<?>) DexKit.getDexKitBridge("DownloadInfo", new IDexKit() {
             @Override
-            public void before(MethodHookParam param) throws Throwable {
-                param.setResult(false);
+            public AnnotatedElement dexkit(DexKitBridge bridge) throws ReflectiveOperationException {
+                ClassData classData = bridge.findClass(FindClass.create()
+                        .excludePackages("androidx")
+                        .matcher(
+                                ClassMatcher.create()
+                                        .usingEqStrings("/s", " | ", "/")
+                        )).singleOrNull();;
+                return classData.getInstance(lpparam.classLoader);
             }
         });
-        findAndHookMethod("h1.h", "R", "i1.a",new MethodHook() {
+
+        Method method1 = (Method) DexKit.getDexKitBridge("ShowTaskDetailMatcher", new IDexKit() {
             @Override
+            public AnnotatedElement dexkit(DexKitBridge bridge) throws ReflectiveOperationException {
+                MethodData methodData = bridge.findMethod(FindMethod.create()
+                        .matcher(MethodMatcher.create()
+                                .declaredClass(ClassMatcher.create().usingStrings("onEventMainThread noScrollListView="))
+                                .usingNumbers(4, 8, 0)
+                                .paramTypes(class1)
+                                .returnType(void.class)
+                        )).singleOrNull();;
+                return methodData.getMethodInstance(lpparam.classLoader);
+            }
+        });
+
+        hookMethod(method1, new MethodHook() {
             public void before(MethodHookParam param) throws Throwable {
-                // @TODO 显示来源应用和任务来源
+                // @TODO 显示来源应用和路径
                 logD(TAG, lpparam.packageName, "source: " + getObjectField(param.args[0], "r") + "  path: " + getObjectField(param.args[0], "i"));
                 setObjectField(param.args[0], "y", "");
             }
         });
+        // findAndHookMethod("h1.h", "R", "i1.a",new MethodHook() {
+        //     @Override
+        //     public void before(MethodHookParam param) throws Throwable {
+        //         logD(TAG, lpparam.packageName, "source: " + getObjectField(param.args[0], "r") + "  path: " + getObjectField(param.args[0], "i"));
+        //         setObjectField(param.args[0], "y", "");
+        //     }
+        // });
     }
 }
