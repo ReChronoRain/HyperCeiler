ccb079e23173157c7b572193c51654248b02877c
frank <frank_782@qq.com>
2024-09-29 09:10:51 +0800

    opt - downloadlink : add field dexkit

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/downloads/ui/AlwaysShowDownloadLink.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/downloads/ui/AlwaysShowDownloadLink.java
@@ -26,14 +26,14 @@ import com.sevtinge.hyperceiler.module.base.dexkit.DexKit;
 import com.sevtinge.hyperceiler.module.base.dexkit.IDexKit;
 
 import org.luckypray.dexkit.DexKitBridge;
-import org.luckypray.dexkit.query.FindMethod;
 import org.luckypray.dexkit.query.*;
 import org.luckypray.dexkit.query.matchers.*;
-import org.luckypray.dexkit.result.ClassData;
-import org.luckypray.dexkit.result.MethodData;
+import org.luckypray.dexkit.result.*;
 
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Method;
+import java.lang.reflect.Field;
+import java.util.List;
 
 public class AlwaysShowDownloadLink extends BaseHook {
 
@@ -48,10 +48,11 @@ public class AlwaysShowDownloadLink extends BaseHook {
                         .matcher(
                                 ClassMatcher.create()
                                         .usingEqStrings("/s", " | ", "/")
-                        )).singleOrNull();;
+                        )).singleOrNull();
                 return classData.getInstance(lpparam.classLoader);
             }
         });
+
 
         Method method1 = (Method) DexKit.getDexKitBridge("ShowTaskDetailMatcher", new IDexKit() {
             @Override
@@ -62,18 +63,56 @@ public class AlwaysShowDownloadLink extends BaseHook {
                                 .usingNumbers(4, 8, 0)
                                 .paramTypes(class1)
                                 .returnType(void.class)
-                        )).singleOrNull();;
+                        )).singleOrNull();
                 return methodData.getMethodInstance(lpparam.classLoader);
+            }
+        });
+
+        Field field1 = (Field) DexKit.getDexKitBridge("DownloadUrl", new IDexKit() {
+            @Override
+            public AnnotatedElement dexkit(DexKitBridge bridge) throws ReflectiveOperationException {
+                FieldData fieldData = bridge.findField(FindField.create()
+                        .matcher(FieldMatcher.create()
+                                .addReadMethod(MethodMatcher.create()
+                                        .usingStrings("android.intent.action.PICK", "com.android.fileexplorer"))
+                                .declaredClass(class1)
+                                .type(String.class)
+                        )).singleOrNull();
+                return fieldData.getFieldInstance(lpparam.classLoader);
+            }
+        });
+
+        Field field2 = (Field) DexKit.getDexKitBridge("DownloadDesc", new IDexKit() {
+            @Override
+            public AnnotatedElement dexkit(DexKitBridge bridge) throws ReflectiveOperationException {
+                FieldDataList fieldDataList = bridge.findField(FindField.create()
+                        .matcher(FieldMatcher.create()
+                                .addReadMethod(MethodMatcher.create()
+                                        .declaredClass(ClassMatcher.create().usingStrings("onEventMainThread noScrollListView="))
+                                        .usingNumbers(4, 8, 0)
+                                        .paramTypes(class1)
+                                        .returnType(void.class))
+                                .type(String.class)
+                        ));
+                // 感觉不太优雅 先这样吧.jpg 暂时没有更好的匹配方式了 反正method里就这两string
+                for (FieldData field : fieldDataList) {
+                    if (field.getName().equals(field1.getName())) continue;
+                    return field.getFieldInstance(lpparam.classLoader);
+                }
+                return null;
+
             }
         });
 
         hookMethod(method1, new MethodHook() {
             public void before(MethodHookParam param) throws Throwable {
+                String url = (String) getObjectField(param.args[0], field1.getName());
                 // @TODO 显示来源应用和路径
-                logD(TAG, lpparam.packageName, "source: " + getObjectField(param.args[0], "r") + "  path: " + getObjectField(param.args[0], "i"));
-                setObjectField(param.args[0], "y", "");
+                logD(TAG, lpparam.packageName, "url:" + url);
+                setObjectField(param.args[0], field2.getName(), "");
             }
         });
+
         // findAndHookMethod("h1.h", "R", "i1.a",new MethodHook() {
         //     @Override
         //     public void before(MethodHookParam param) throws Throwable {
