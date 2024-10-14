58e220219fc4e0a4212cbf55884e6f5b24aea727
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2024-10-14 19:57:05 +0800

    fix: cant match canary and beta build in miuistub

--- miuistub/build.gradle.kts
+++ miuistub/build.gradle.kts
@@ -5,6 +5,24 @@ plugins {
 android {
     namespace = "miui.os"
     compileSdk = 35
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            setProguardFiles(listOf(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"))
+        }
+        create("beta") {
+            isMinifyEnabled = false
+            setProguardFiles(listOf(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"))
+        }
+        create("canary") {
+            isMinifyEnabled = false
+            setProguardFiles(listOf(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"))
+        }
+        debug {
+            isMinifyEnabled = false
+            setProguardFiles(listOf(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"))
+        }
+    }
 }
 
 dependencies {
