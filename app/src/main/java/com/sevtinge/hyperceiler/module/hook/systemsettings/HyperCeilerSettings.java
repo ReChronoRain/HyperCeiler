37f631ec488523292655e5ade84c21120cad4e7d
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2024-11-02 02:55:13 +0800

    fix: hyperceiler icon in settings

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemsettings/HyperCeilerSettings.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemsettings/HyperCeilerSettings.java
@@ -19,6 +19,7 @@
 package com.sevtinge.hyperceiler.module.hook.systemsettings;
 
 import static com.sevtinge.hyperceiler.module.base.tool.OtherTool.getModuleRes;
+import static com.sevtinge.hyperceiler.utils.devicesdk.SystemSDKKt.isMoreHyperOSVersion;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -83,6 +84,7 @@ public class HyperCeilerSettings extends BaseHook {
                 bundle.putParcelableArrayList("header_user", users);
                 XposedHelpers.setObjectField(header, "extras", bundle);
 
+                int device = mContext.getResources().getIdentifier("my_device", "id", mContext.getPackageName());
                 int themes = mContext.getResources().getIdentifier("launcher_settings", "id", mContext.getPackageName());
                 int special = mContext.getResources().getIdentifier("other_special_feature_settings", "id", mContext.getPackageName());
                 int timer = mContext.getResources().getIdentifier("app_timer", "id", mContext.getPackageName());
@@ -92,8 +94,12 @@ public class HyperCeilerSettings extends BaseHook {
                 for (Object head : headers) {
                     position++;
                     long id = XposedHelpers.getLongField(head, "id");
-                    if (opt == 1 && id == -1) {
-                        headers.add(position - 1, header);
+                    if (opt == 1 && id == device) {
+                        if (isMoreHyperOSVersion(2f)) {
+                            if (id == device) headers.add(position, header);
+                        } else {
+                            if (id == -1) headers.add(position - 1, header);
+                        }
                     } else if (opt == 2 && id == themes) {
                         headers.add(position, header);
                     } else if (opt == 3 && id == (RomUtils.getMiuiVersion() < 14 ? special : timer)) {
