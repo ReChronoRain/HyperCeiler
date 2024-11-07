45978091001ac0924b435e1e1272a0ae4cf80556
铃柒柒 <lingqiqi233@gmail.com>
2024-11-07 21:23:27 +0800

    fix: UnlockHyperMind hook failed (15.x.x.x ~ 16.2.x.x +)

--- app/src/main/java/com/sevtinge/hyperceiler/module/app/MiLink.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/app/MiLink.java
@@ -22,7 +22,7 @@ import com.hchen.database.HookBase;
 import com.sevtinge.hyperceiler.module.base.BaseModule;
 import com.sevtinge.hyperceiler.module.hook.milink.AllowCameraDevices;
 import com.sevtinge.hyperceiler.module.hook.milink.FuckHpplay;
-import com.sevtinge.hyperceiler.module.hook.milink.UnlockHMind;
+import com.sevtinge.hyperceiler.module.hook.milink.NewUnlockHMind;
 import com.sevtinge.hyperceiler.module.hook.milink.UnlockMiShare;
 
 @HookBase(pkg = "com.milink.service", skip = true, isPad = false, tarAndroid = 0)
@@ -31,7 +31,7 @@ public class MiLink extends BaseModule {
     @Override
     public void handleLoadPackage() {
         initHook(new UnlockMiShare(), mPrefsMap.getBoolean("milink_unlock_mishare"));
-        initHook(new UnlockHMind(), mPrefsMap.getBoolean("milink_unlock_hmind"));
+        initHook(NewUnlockHMind.INSTANCE, mPrefsMap.getBoolean("milink_unlock_hmind"));
         initHook(new AllowCameraDevices(), mPrefsMap.getBoolean("milink_allow_camera_devices"));
         initHook(new FuckHpplay(), mPrefsMap.getBoolean("milink_fuck_hpplay"));
     }
---
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/milink/NewUnlockHMind.kt
@@ -0,0 +1,54 @@
+/*
+  * This file is part of HyperCeiler.
+
+  * HyperCeiler is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU Affero General Public License as
+  * published by the Free Software Foundation, either version 3 of the
+  * License.
+
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU Affero General Public License for more details.
+
+  * You should have received a copy of the GNU Affero General Public License
+  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+  * Copyright (C) 2023-2024 HyperCeiler Contributions
+*/
+package com.sevtinge.hyperceiler.module.hook.milink
+
+import com.github.kyuubiran.ezxhelper.EzXHelper.safeClassLoader
+import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
+import com.sevtinge.hyperceiler.module.base.*
+import com.sevtinge.hyperceiler.module.base.dexkit.*
+import com.sevtinge.hyperceiler.module.base.dexkit.DexKitTool.toMethod
+import java.lang.reflect.*
+
+object NewUnlockHMind : BaseHook() {
+    private val unlockHMind by lazy {
+        // 解锁 Xiaomi HyperMind
+        // 适配 15.x.x.x ~ 16.x.x.x
+        // 这要是坏了，除非动了 cetus 字符串，否则不可能会炸
+        // 哦对了，我说怎么平板不配，原来 TMD 把横屏适配删了，米米你啥时候加回来 QAQ！
+        DexKit.getDexKitBridge("NewHMindManager") { dexkit ->
+            dexkit.findMethod {
+                matcher {
+                    addCaller {
+                        declaredClass = "com.miui.circulate.world.CirculateWorldActivity"
+                        usingStrings("cetus")
+                    }
+                    declaredClass = "com.milink.hmindlib.HMindManager"
+                    returnType = "boolean"
+                    modifiers = Modifier.FINAL
+                }
+            }.single().getMethodInstance(safeClassLoader)
+        }.toMethod()
+    }
+
+    override fun init() {
+        unlockHMind.createHook {
+            returnConstant(true)
+        }
+    }
+}
