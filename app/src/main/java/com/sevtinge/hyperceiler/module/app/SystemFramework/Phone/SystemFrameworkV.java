429cce79607620f1f09b6c14dbc7c891d645dcb0
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2024-11-20 18:08:25 +0800

    feat: framework - Allow update system apps
    感谢笑死鸡倾情赞助
    
    等下 包管理服务怎么都12项了

--- app/src/main/java/com/sevtinge/hyperceiler/module/app/SystemFramework/Phone/SystemFrameworkV.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/app/SystemFramework/Phone/SystemFrameworkV.java
@@ -67,6 +67,7 @@ import com.sevtinge.hyperceiler.module.hook.systemframework.VolumeFirstPress;
 import com.sevtinge.hyperceiler.module.hook.systemframework.VolumeMediaSteps;
 import com.sevtinge.hyperceiler.module.hook.systemframework.VolumeSeparateControl;
 import com.sevtinge.hyperceiler.module.hook.systemframework.VolumeSteps;
+import com.sevtinge.hyperceiler.module.hook.systemframework.corepatch.AllowUpdateSystemApp;
 import com.sevtinge.hyperceiler.module.hook.systemframework.corepatch.BypassIsolationViolation;
 import com.sevtinge.hyperceiler.module.hook.systemframework.corepatch.BypassSignCheckForT;
 import com.sevtinge.hyperceiler.module.hook.systemframework.display.AllDarkMode;
@@ -149,6 +150,7 @@ public class SystemFrameworkV extends BaseModule {
         // 核心破解
         initHook(BypassSignCheckForT.INSTANCE, mPrefsMap.getBoolean("system_framework_core_patch_auth_creak") || mPrefsMap.getBoolean("system_framework_core_patch_disable_integrity"));
         initHook(new BypassIsolationViolation(), mPrefsMap.getBoolean("system_framework_core_patch_bypass_isolation_violation"));
+        initHook(new AllowUpdateSystemApp(), mPrefsMap.getBoolean("system_framework_core_patch_allow_update_system_app"));
 
         // 网络
         initHook(DualNRSupport.INSTANCE, mPrefsMap.getBoolean("phone_double_5g_nr"));
