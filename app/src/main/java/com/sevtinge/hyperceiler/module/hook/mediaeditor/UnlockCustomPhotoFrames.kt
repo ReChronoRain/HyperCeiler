d71e7fb43e8d68ad7757ae2cde62281d0c22a0c5
铃柒柒 <lingqiqi233@gmail.com>
2024-11-21 01:19:12 +0800

    fix：UnlockCustomPhotoFrames hook failed for 1.7.5.0.4 ~ 1.9.x.x.x (#993)

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/mediaeditor/UnlockCustomPhotoFrames.kt
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/mediaeditor/UnlockCustomPhotoFrames.kt
@@ -23,6 +23,7 @@ import com.sevtinge.hyperceiler.module.base.*
 import com.sevtinge.hyperceiler.module.base.dexkit.*
 import com.sevtinge.hyperceiler.module.base.dexkit.DexKitTool.toElementList
 import com.sevtinge.hyperceiler.module.base.dexkit.DexKitTool.toMethodDataList
+import com.sevtinge.hyperceiler.module.base.tool.OtherTool.*
 import java.lang.reflect.*
 
 object UnlockCustomPhotoFrames : BaseHook() {
@@ -31,6 +32,10 @@ object UnlockCustomPhotoFrames : BaseHook() {
     }
     private val isOpenSpring by lazy {
         mPrefsMap.getBoolean("mediaeditor_unlock_spring")
+    }
+    private val isNewMediaeditor by lazy {
+        // 以 1.7.5.0.4 为新旧版本分界线
+        getPackageVersionCode(lpparam) >= 4658180
     }
     private val isLeica by lazy { frames == 1 }
     private val isRedmi by lazy { frames == 2 }
@@ -41,9 +46,14 @@ object UnlockCustomPhotoFrames : BaseHook() {
             bridge.findMethod {
                 matcher {
                     // 真是妹想到啊，1.5 和 1.6 版本还以为不会套回去了
-                    // 现在这个查找方式直接兼容 1.4 - 1.6
+                    // 现在这个查找方式直接兼容 1.4 - 1.9
                     // 1.6.5.10.2 迪斯尼定制画框解锁的地方和现在的不一样
-                    declaredClass("com.miui.mediaeditor.photo.config.galleryframe.GalleryFrameAccessUtils")
+                    // 1.7.5.0.4 之后，类名迁移，内部文件改动较大，此为临时解决方案
+                    if (isNewMediaeditor) {
+                        declaredClass("com.miui.mediaeditor.config.galleryframe.GalleryFrameAccessUtils")
+                    } else {
+                        declaredClass("com.miui.mediaeditor.photo.config.galleryframe.GalleryFrameAccessUtils")
+                    }
                     modifiers = Modifier.FINAL or Modifier.STATIC
                     returnType = "boolean"
                     paramCount = 0
@@ -58,7 +68,11 @@ object UnlockCustomPhotoFrames : BaseHook() {
         DexKit.getDexKitBridgeList("PB") { dexkit ->
             dexkit.findMethod {
                 matcher {
-                    declaredClass("com.miui.mediaeditor.photo.config.galleryframe.GalleryFrameAccessUtils")
+                    if (isNewMediaeditor) {
+                        declaredClass("com.miui.mediaeditor.config.galleryframe.GalleryFrameAccessUtils")
+                    } else {
+                        declaredClass("com.miui.mediaeditor.photo.config.galleryframe.GalleryFrameAccessUtils")
+                    }
                     // modifiers = Modifier.STATIC // 1.6.5.10.2 改成 STATIC，原来是 FINAL
                     returnType = "boolean"
                     paramCount = 1
@@ -84,7 +98,11 @@ object UnlockCustomPhotoFrames : BaseHook() {
                 }
             }?.toElementList()
         }.toMethodList()
-        val actions = listOf<(Method) -> Unit>(::xiaomi, ::poco, ::redmi, ::other)
+        val actions = if (isNewMediaeditor) {
+            listOf<(Method) -> Unit>(::xiaomi, ::redmi, ::poco, ::other)
+        } else {
+            listOf<(Method) -> Unit>(::xiaomi, ::poco, ::redmi, ::other)
+        }
         val orderedPublicA = DexKit.getDexKitBridgeList("PA") { _ ->
             publicA?.toElementList()
         }.toMethodList()
@@ -103,7 +121,9 @@ object UnlockCustomPhotoFrames : BaseHook() {
 
         if (isOpenSpring && publicC.isNotEmpty()) {
             publicC.forEach { method ->
-                other(method)  // 1.6.0.5.2 新增限时新春定制画框
+                other(method)  
+                // 1.6.0.5.2 新增限时新春定制画框
+                // 后续版本已移除，其实可以删掉的，但还是留着吧，兴许后面可能还有用
             }
         }
     }
