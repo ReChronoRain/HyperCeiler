e737c41fcf2caa37d4b4a805bedd2eafe9cd6e37
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2024-12-05 23:13:40 +0800

    opt: log

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemui/controlcenter/CCGridForHyperOS.kt
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemui/controlcenter/CCGridForHyperOS.kt
@@ -33,18 +33,6 @@ object CCGridForHyperOS {
 
     @JvmStatic
     fun initCCGridForHyperOS(classLoader: ClassLoader?) {
-        /*miui.systemui.controlcenter.qs.tileview.QSTileItemIconView.updateIcon$default(Unknown Source:11)
-        miui.systemui.controlcenter.panel.main.brightness.BrightnessPanelTilesController.updateSize(Unknown Source:97)
-        miui.systemui.controlcenter.panel.main.brightness.BrightnessPanelController.updateSize(Unknown Source:21)
-        miui.systemui.controlcenter.panel.main.brightness.BrightnessPanelController.onConfigurationChanged(Unknown Source:44)
-        miui.systemui.controlcenter.utils.ControlCenterViewController.dispatchConfigurationChanged(Unknown Source:7)
-        miui.systemui.controlcenter.windowview.ControlCenterWindowViewController.onConfigurationChanged(Unknown Source:93)
-        miui.systemui.controlcenter.windowview.ControlCenterWindowViewController.onConfigChanged(Unknown Source:13)
-        miui.systemui.autodensity.AutoDensityControllerImpl$DummyApplication.onConfigurationChanged(Unknown Source:65)*/
-        HookTool.hookAllMethods("miui.systemui.controlcenter.utils.ControlCenterViewController", classLoader, "dispatchConfigurationChanged", object : MethodHook() {
-            override fun before(param: MethodHookParam) {
-                XposedLogUtils.logD("PluginHelper", param.thisObject.toString())
-            }})
         var warningD: Drawable? = null
         var enabledD: Drawable? = null
         var restrictedD: Drawable? = null
@@ -57,11 +45,6 @@ object CCGridForHyperOS {
                 override fun before(param: MethodHookParam) {
                     orientation = (param.thisObject as View).context.resources.configuration.orientation
                     if (configuration == orientation) return
-
-                    /*val stackTrace = Thread.currentThread().stackTrace
-                    val stackTraceBuilder = StringBuilder()
-                    for (element in stackTrace) stackTraceBuilder.append(element.toString()).append("\n")
-                    logD("PluginHelper", stackTraceBuilder.toString())*/
 
                     val pluginContext: Context = XposedHelpers.getObjectField(param.thisObject, "pluginContext") as Context
 
