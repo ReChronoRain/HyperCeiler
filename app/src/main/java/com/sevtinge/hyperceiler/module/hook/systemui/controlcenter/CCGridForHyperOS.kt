13400a0ff65ddbfcb98832e158d50a0fb1548ad4
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2024-12-05 23:13:07 +0800

    opt: CCGrid for HyperOS

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemui/controlcenter/CCGridForHyperOS.kt
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemui/controlcenter/CCGridForHyperOS.kt
@@ -20,6 +20,8 @@ package com.sevtinge.hyperceiler.module.hook.systemui.controlcenter
 
 import android.content.*
 import android.graphics.drawable.*
+import android.view.*
+import com.sevtinge.hyperceiler.module.base.tool.*
 import com.sevtinge.hyperceiler.module.base.tool.HookTool.*
 import com.sevtinge.hyperceiler.utils.log.*
 import com.sevtinge.hyperceiler.utils.prefs.*
@@ -31,46 +33,48 @@ object CCGridForHyperOS {
 
     @JvmStatic
     fun initCCGridForHyperOS(classLoader: ClassLoader?) {
+        /*miui.systemui.controlcenter.qs.tileview.QSTileItemIconView.updateIcon$default(Unknown Source:11)
+        miui.systemui.controlcenter.panel.main.brightness.BrightnessPanelTilesController.updateSize(Unknown Source:97)
+        miui.systemui.controlcenter.panel.main.brightness.BrightnessPanelController.updateSize(Unknown Source:21)
+        miui.systemui.controlcenter.panel.main.brightness.BrightnessPanelController.onConfigurationChanged(Unknown Source:44)
+        miui.systemui.controlcenter.utils.ControlCenterViewController.dispatchConfigurationChanged(Unknown Source:7)
+        miui.systemui.controlcenter.windowview.ControlCenterWindowViewController.onConfigurationChanged(Unknown Source:93)
+        miui.systemui.controlcenter.windowview.ControlCenterWindowViewController.onConfigChanged(Unknown Source:13)
+        miui.systemui.autodensity.AutoDensityControllerImpl$DummyApplication.onConfigurationChanged(Unknown Source:65)*/
+        HookTool.hookAllMethods("miui.systemui.controlcenter.utils.ControlCenterViewController", classLoader, "dispatchConfigurationChanged", object : MethodHook() {
+            override fun before(param: MethodHookParam) {
+                XposedLogUtils.logD("PluginHelper", param.thisObject.toString())
+            }})
         var warningD: Drawable? = null
         var enabledD: Drawable? = null
         var restrictedD: Drawable? = null
         var disabledD: Drawable? = null
         var unavailableD: Drawable? = null
-        var updateTemp1 = false
-        var updateTemp2 = false
+        var configuration = 0
+        var orientation: Int
 
         XposedHelpers.findAndHookMethod("miui.systemui.controlcenter.qs.tileview.QSTileItemIconView", classLoader, "updateIcon", "com.android.systemui.plugins.qs.QSTile\$State", Boolean::class.javaPrimitiveType, Boolean::class.javaPrimitiveType, object : MethodHook() {
                 override fun before(param: MethodHookParam) {
-                    val param1 = param.args[1] as Boolean
-                    val param2 = param.args[2] as Boolean
-                    val update = (param1 && param2 && (!updateTemp1 || !updateTemp2))
-                    if (update) {
-                        updateTemp1 = param1
-                        updateTemp2 = param2
-                    }
+                    orientation = (param.thisObject as View).context.resources.configuration.orientation
+                    if (configuration == orientation) return
+
+                    /*val stackTrace = Thread.currentThread().stackTrace
+                    val stackTraceBuilder = StringBuilder()
+                    for (element in stackTrace) stackTraceBuilder.append(element.toString()).append("\n")
+                    logD("PluginHelper", stackTraceBuilder.toString())*/
 
                     val pluginContext: Context = XposedHelpers.getObjectField(param.thisObject, "pluginContext") as Context
 
-                    if (warningD == null || update) {
-                        val warning: Int = pluginContext.resources.getIdentifier("qs_background_warning", "drawable", "miui.systemui.plugin")
-                        warningD = pluginContext.theme.getDrawable(warning);
-                    }
-                    if (enabledD == null || update) {
-                        val enabled: Int = pluginContext.resources.getIdentifier("qs_background_enabled", "drawable", "miui.systemui.plugin")
-                        enabledD = pluginContext.theme.getDrawable(enabled);
-                    }
-                    if (restrictedD == null || update) {
-                        val restricted: Int = pluginContext.resources.getIdentifier("qs_background_restricted", "drawable", "miui.systemui.plugin")
-                        restrictedD = pluginContext.theme.getDrawable(restricted);
-                    }
-                    if (disabledD == null || update) {
-                        val disabled: Int = pluginContext.resources.getIdentifier("qs_background_disabled", "drawable", "miui.systemui.plugin")
-                        disabledD = pluginContext.theme.getDrawable(disabled);
-                    }
-                    if (unavailableD == null || update) {
-                        val unavailable: Int = pluginContext.resources.getIdentifier("qs_background_unavailable", "drawable", "miui.systemui.plugin")
-                        unavailableD = pluginContext.theme.getDrawable(unavailable);
-                    }
+                    val warning: Int = pluginContext.resources.getIdentifier("qs_background_warning", "drawable", "miui.systemui.plugin")
+                    val enabled: Int = pluginContext.resources.getIdentifier("qs_background_enabled", "drawable", "miui.systemui.plugin")
+                    val restricted: Int = pluginContext.resources.getIdentifier("qs_background_restricted", "drawable", "miui.systemui.plugin")
+                    val disabled: Int = pluginContext.resources.getIdentifier("qs_background_disabled", "drawable", "miui.systemui.plugin")
+                    val unavailable: Int = pluginContext.resources.getIdentifier("qs_background_unavailable", "drawable", "miui.systemui.plugin")
+                    warningD = pluginContext.theme.getDrawable(warning)
+                    enabledD = pluginContext.theme.getDrawable(enabled)
+                    restrictedD = pluginContext.theme.getDrawable(restricted)
+                    disabledD = pluginContext.theme.getDrawable(disabled)
+                    unavailableD = pluginContext.theme.getDrawable(unavailable)
 
                     if (warningD is GradientDrawable) {
                         (warningD as GradientDrawable).cornerRadius = radius
@@ -87,6 +91,8 @@ object CCGridForHyperOS {
                     if (unavailableD is GradientDrawable) {
                         (unavailableD as GradientDrawable).cornerRadius = radius
                     }
+
+                    configuration = orientation
                 }
             }
         )
