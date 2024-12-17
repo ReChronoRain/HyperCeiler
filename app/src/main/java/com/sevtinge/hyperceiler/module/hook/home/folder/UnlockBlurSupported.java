6f3ff51b5a8eb0f514b81d8b409717ee06b79de5
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2024-12-17 20:36:31 +0800

    fix: UnlockBlurSupported

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/home/folder/UnlockBlurSupported.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/home/folder/UnlockBlurSupported.java
@@ -29,32 +29,48 @@ public class UnlockBlurSupported extends BaseHook {
     @Override
     public void init() throws NoSuchMethodException {
         findAndHookMethod("com.miui.home.launcher.common.BlurUtilities",
-            "isBlurSupported",
-            new MethodHook() {
-                @Override
-                protected void before(MethodHookParam param) {
-                    boolean isDefaultIcon = (boolean) XposedHelpers.callStaticMethod(
-                        findClassIfExists("com.miui.home.launcher.DeviceConfig"),
-                        "isDefaultIcon");
-                    if (!isDefaultIcon)
-                        param.setResult(true);
+                "isBlurSupported",
+                new MethodHook() {
+                    @Override
+                    protected void before(MethodHookParam param) {
+                        boolean isDefaultIcon = (boolean) XposedHelpers.callStaticMethod(
+                                findClassIfExists("com.miui.home.launcher.DeviceConfig"),
+                                "isDefaultIcon");
+                        if (!isDefaultIcon)
+                            param.setResult(true);
+                    }
                 }
-            }
         );
 
-        findAndHookMethod("com.miui.home.launcher.folder.LauncherFolder2x2IconContainer",
-            "resolveTopPadding", Rect.class, new MethodHook() {
+        try {
+            findAndHookMethod("com.miui.home.launcher.folder.LauncherFolder2x2IconContainer",
+                    "resolveTopPadding", Rect.class, new MethodHook() {
+                        @Override
+                        protected void before(MethodHookParam param) {
+                            // Rect rect = (Rect) param.args[0];
+                            View view = (View) param.thisObject;
+                            XposedHelpers.callMethod(view,
+                                    "setPadding", 0,
+                                    XposedHelpers.callMethod(param.thisObject,
+                                            "getMContainerPaddingTop"), 0, 0);
+                            param.setResult(null);
+                        }
+                    }
+            );
+        } catch (Error | Exception ignore) {
+            findAndHookMethod("com.miui.home.launcher.DeviceConfig", "isUseDefaultIconFolder1x1", new MethodHook() {
                 @Override
-                protected void before(MethodHookParam param) {
-                    // Rect rect = (Rect) param.args[0];
-                    View view = (View) param.thisObject;
-                    XposedHelpers.callMethod(view,
-                        "setPadding", 0,
-                        XposedHelpers.callMethod(param.thisObject,
-                            "getMContainerPaddingTop"), 0, 0);
-                    param.setResult(null);
+                protected void before(MethodHookParam param) throws Throwable {
+                    param.setResult(true);
                 }
-            }
-        );
+            });
+
+            findAndHookMethod("com.miui.home.launcher.DeviceConfig", "isUseDefaultIconFolderLarge", new MethodHook() {
+                @Override
+                protected void before(MethodHookParam param) throws Throwable {
+                    param.setResult(true);
+                }
+            });
+        }
     }
 }
