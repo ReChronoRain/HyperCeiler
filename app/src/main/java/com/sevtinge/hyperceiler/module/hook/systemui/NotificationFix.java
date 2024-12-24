c87a120bf7f61e881c6ca9f695c2e0cc2050831d
hamjin <jinham@qq.com>
2024-12-24 13:57:43 +0800

    fix: NotificationFix on OS2 (#1072)
    * fix: NotificationFix on OS2
    
    Signed-off-by: hamjin <jinham@qq.com>
    Co-authored-by: 铃柒柒 <lingqiqi233@gmail.com>

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemui/NotificationFix.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemui/NotificationFix.java
@@ -1,22 +1,24 @@
 /*
-  * This file is part of HyperCeiler.
+ * This file is part of HyperCeiler.
 
-  * HyperCeiler is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU Affero General Public License as
-  * published by the Free Software Foundation, either version 3 of the
-  * License.
+ * HyperCeiler is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License.
 
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU Affero General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
 
-  * You should have received a copy of the GNU Affero General Public License
-  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-  * Copyright (C) 2023-2024 HyperCeiler Contributions
-*/
+ * Copyright (C) 2023-2024 HyperCeiler Contributions
+ */
 package com.sevtinge.hyperceiler.module.hook.systemui;
+
+import static com.sevtinge.hyperceiler.utils.devicesdk.SystemSDKKt.isMoreAndroidVersion;
 
 import com.sevtinge.hyperceiler.module.base.BaseHook;
 
@@ -25,6 +27,14 @@ import de.robv.android.xposed.XposedHelpers;
 public class NotificationFix extends BaseHook {
     @Override
     public void init() {
-        XposedHelpers.setStaticBooleanField(XposedHelpers.findClass("com.android.systemui.statusbar.notification.NotificationSettingsManager", lpparam.classLoader), "USE_WHITE_LISTS", false);
+        String className;
+        String fieldName = "USE_WHITE_LISTS";
+
+        if (isMoreAndroidVersion(35))
+            className = "com.miui.systemui.notification.NotificationSettingsManager";
+        else
+            className = "com.android.systemui.statusbar.notification.NotificationSettingsManager";
+
+        XposedHelpers.setStaticBooleanField(XposedHelpers.findClass(className, lpparam.classLoader), fieldName, false);
     }
 }
