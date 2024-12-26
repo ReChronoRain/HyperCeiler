c5fe1dd5032cde33878e3dbd89785905be09eec5
Voemp <Hoshinakiyoru@163.com>
2024-12-26 20:47:40 +0800

    fix: 应用商店 - 强制应用安全检测为优秀 (#1075)

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/getapps/BypassRiskCheck.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/getapps/BypassRiskCheck.java
@@ -19,6 +19,7 @@
 
 package com.sevtinge.hyperceiler.module.hook.getapps;
 
+import static com.sevtinge.hyperceiler.utils.Helpers.getPackageVersionCode;
 import static de.robv.android.xposed.XposedHelpers.getStaticObjectField;
 import static de.robv.android.xposed.XposedHelpers.setObjectField;
 
@@ -29,18 +30,52 @@ import java.util.ArrayList;
 public class BypassRiskCheck extends BaseHook {
     @Override
     public void init() throws NoSuchMethodException {
-        findAndHookMethod("com.xiaomi.market.common.component.componentbeans.UnknownAppResult", "isExcellent", new MethodHook() {
-            @Override
-            protected void before(MethodHookParam param) throws Throwable {
-                param.setResult(true);
-            }
-        });
-        findAndHookMethod("com.xiaomi.market.common.component.componentbeans.RiskAppResult", "isExcellent", new MethodHook() {
-            @Override
-            protected void before(MethodHookParam param) throws Throwable {
-                param.setResult(true);
-            }
-        });
+        boolean isNew = getPackageVersionCode(lpparam) >= 40005740;
+        if (isNew) {
+            findAndHookMethod("com.xiaomi.market.business_ui.main.mine.app_security.check_page.risk_app.AppSecurityRiskAppView", "updateResultStatus", int.class, new MethodHook() {
+                @Override
+                protected void before(MethodHookParam param) throws Throwable {
+                    param.args[0] = 0;
+                }
+            });
+            findAndHookMethod("com.xiaomi.market.common.component.componentbeans.AppSecurityCheckRules$Companion", "getTotalResult", "com.xiaomi.market.common.component.componentbeans.AppSecurityCheckRules", int.class, int.class, new MethodHook() {
+                @Override
+                protected void before(MethodHookParam param) throws Throwable {
+                    param.setResult(getStaticObjectField(findClassIfExists("com.xiaomi.market.common.component.componentbeans.TotalResult"), "TOTAL_EXCELLENT"));
+                }
+            });
+            findAndHookMethod("com.xiaomi.market.business_ui.main.mine.app_security.AppSecurityCheckManager", "getRiskResultTxtByMine", int.class, int.class, new MethodHook() {
+                @Override
+                protected void before(MethodHookParam param) throws Throwable {
+                    param.args[1] = 0;
+                }
+            });
+        } else {
+            findAndHookMethod("com.xiaomi.market.common.component.componentbeans.UnknownAppResult", "isExcellent", new MethodHook() {
+                @Override
+                protected void before(MethodHookParam param) throws Throwable {
+                    param.setResult(true);
+                }
+            });
+            findAndHookMethod("com.xiaomi.market.common.component.componentbeans.RiskAppResult", "isExcellent", new MethodHook() {
+                @Override
+                protected void before(MethodHookParam param) throws Throwable {
+                    param.setResult(true);
+                }
+            });
+            findAndHookMethod("com.xiaomi.market.business_ui.main.mine.app_security.check_page.unknown_app.AppSecurityUnknownAppView", "setListOpenImageStatus", new MethodHook() {
+                @Override
+                protected void before(MethodHookParam param) throws Throwable {
+                    setObjectField(param.thisObject, "unknownModelList", new ArrayList<>());
+                }
+            });
+            findAndHookMethod("com.xiaomi.market.common.component.componentbeans.AppSecurityCheckRules$Companion", "getTotalResult", "com.xiaomi.market.common.component.componentbeans.AppSecurityCheckRules", "com.xiaomi.market.common.component.componentbeans.GuardResult", "com.xiaomi.market.common.component.componentbeans.RiskAppResult", "com.xiaomi.market.common.component.componentbeans.UnknownAppResult", new MethodHook() {
+                @Override
+                protected void before(MethodHookParam param) throws Throwable {
+                    param.setResult(getStaticObjectField(findClassIfExists("com.xiaomi.market.common.component.componentbeans.TotalResult"), "TOTAL_EXCELLENT"));
+                }
+            });
+        }
         /*findAndHookMethod("com.xiaomi.market.common.component.componentbeans.GuardResult", "isExcellent", new MethodHook() {
             @Override
             protected void before(MethodHookParam param) throws Throwable {
@@ -51,18 +86,6 @@ public class BypassRiskCheck extends BaseHook {
             @Override
             protected void before(MethodHookParam param) throws Throwable {
                 setObjectField(param.thisObject, "riskModelList", new ArrayList<>());
-            }
-        });
-        findAndHookMethod("com.xiaomi.market.business_ui.main.mine.app_security.check_page.unknown_app.AppSecurityUnknownAppView", "setListOpenImageStatus", new MethodHook() {
-            @Override
-            protected void before(MethodHookParam param) throws Throwable {
-                setObjectField(param.thisObject, "unknownModelList", new ArrayList<>());
-            }
-        });
-        findAndHookMethod("com.xiaomi.market.common.component.componentbeans.AppSecurityCheckRules$Companion", "getTotalResult", "com.xiaomi.market.common.component.componentbeans.AppSecurityCheckRules", "com.xiaomi.market.common.component.componentbeans.GuardResult", "com.xiaomi.market.common.component.componentbeans.RiskAppResult", "com.xiaomi.market.common.component.componentbeans.UnknownAppResult", new MethodHook() {
-            @Override
-            protected void before(MethodHookParam param) throws Throwable {
-                param.setResult(getStaticObjectField(findClassIfExists("com.xiaomi.market.common.component.componentbeans.TotalResult"), "TOTAL_EXCELLENT"));
             }
         });
         findAndHookMethod("com.xiaomi.market.business_ui.main.mine.app_security.AppSecurityCheckManager", "getSecurityResultTxtByPage", "com.xiaomi.market.common.component.componentbeans.TotalResult", int.class, int.class, new MethodHook() {
