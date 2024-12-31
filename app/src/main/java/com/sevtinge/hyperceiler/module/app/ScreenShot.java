0efd6cf69506197957a650569ed7c94af57f6f55
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2025-01-01 01:31:38 +0800

    fix: Flag Secure

--- app/src/main/java/com/sevtinge/hyperceiler/module/app/ScreenShot.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/app/ScreenShot.java
@@ -18,7 +18,7 @@
 */
 package com.sevtinge.hyperceiler.module.app;
 
-import static com.sevtinge.hyperceiler.utils.devicesdk.SystemSDKKt.isAndroidVersion;
+import static com.sevtinge.hyperceiler.utils.devicesdk.SystemSDKKt.isMoreAndroidVersion;
 
 import android.text.TextUtils;
 
@@ -42,6 +42,6 @@ public class ScreenShot extends BaseModule {
         initHook(UnlockPrivacyMarking.INSTANCE, mPrefsMap.getBoolean("screenshot_unlock_privacy_marking"));
         // 超级剪切板
         initHook(UnlockSuperClipboard.INSTANCE, mPrefsMap.getStringAsInt("various_super_clipboard_e", 0) != 0);
-        initHook(new FlagSecure(), mPrefsMap.getBoolean("system_other_flag_secure") && isAndroidVersion(33));
+        initHook(new FlagSecure(), mPrefsMap.getBoolean("system_other_flag_secure") && isMoreAndroidVersion(33));
     }
 }
