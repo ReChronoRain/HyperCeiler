c9da8d7633f62dd33e75d176a3b3e5c2dd16cfa6
铃柒柒 <lingqiqi233@gmail.com>
2025-01-01 21:59:24 +0800

    adj: remove mediaeditor some old func

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/mediaeditor/UnlockCustomPhotoFrames.kt
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/mediaeditor/UnlockCustomPhotoFrames.kt
@@ -19,132 +19,106 @@
 package com.sevtinge.hyperceiler.module.hook.mediaeditor
 
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
-import com.github.kyuubiran.ezxhelper.MemberExtensions.paramCount
-import com.github.kyuubiran.ezxhelper.ObjectHelper.Companion.objectHelper
 import com.sevtinge.hyperceiler.module.base.*
 import com.sevtinge.hyperceiler.module.base.dexkit.*
-import com.sevtinge.hyperceiler.utils.*
-import com.sevtinge.hyperceiler.utils.api.*
-import com.sevtinge.hyperceiler.utils.log.*
+import com.sevtinge.hyperceiler.module.base.tool.OtherTool.*
 import java.lang.reflect.*
 
 object UnlockCustomPhotoFrames : BaseHook() {
     private val frames by lazy {
         mPrefsMap.getStringAsInt("mediaeditor_unlock_custom_photo_frames", 0)
     }
-    private val isHookType by lazy {
-        mPrefsMap.getStringAsInt("mediaeditor_hook_type", 0) == 2
+    private val isNewMediaeditor by lazy {
+        // 以 1.7.5.0.4 为新旧版本分界线
+        getPackageVersionCode(lpparam) >= 4658180
     }
-    private val isOpenSpring by lazy {
-        mPrefsMap.getBoolean("mediaeditor_unlock_spring")
-    }
-    private val isLeica by lazy {
-        if (!isHookType) {
-            frames == 1
-        } else {
-            mPrefsMap.getBoolean("mediaeditor_unlock_custom_photo_frames_leica")
-        }
-    }
-    private val isRedmi by lazy {
-        if (!isHookType) {
-            frames == 2
-        } else {
-            mPrefsMap.getBoolean("mediaeditor_unlock_custom_photo_frames_redmi")
-        }
-    }
-    private val isPOCO by lazy {
-        if (!isHookType) {
-            frames == 3
-        } else {
-            mPrefsMap.getBoolean("mediaeditor_unlock_custom_photo_frames_poco")
-        }
-    }
+    private val isLeica by lazy { frames == 1 }
+    private val isRedmi by lazy { frames == 2 }
+    private val isPOCO by lazy { frames == 3 }
 
-    private val methodA by lazy<List<Method>> {
-        DexKit.findMemberList("MA") { bridge ->
+    private val publicA by lazy<List<Method>> {
+        DexKit.findMemberList("PA") { bridge ->
             bridge.findMethod {
                 matcher {
-                    // 改动日志:
-                    // 现在这个查找方式直接兼容 1.5 - 1.9
-                    // 1.6.5.10.2 之后迪斯尼定制画框解锁的地方和现在的不一样
-                    // 1.7.5.0.4 之后，类名迁移，内部文件改动较大
-                    // 合并缓存，现在只需查询一次即可获取全部 6 个需要 Hook 的方法，且不会出现多余的方法 (2025.1.8)
-                    // 这里的 declaredClass 不清楚米米还会不会混淆，混淆了非常麻烦，比如从 1.3 - 1.4 版本的混淆
-                    if (isHookType) {
+                    // 真是妹想到啊，1.5 和 1.6 版本还以为不会套回去了
+                    // 现在这个查找方式直接兼容 1.4 - 1.9
+                    // 1.6.5.10.2 迪斯尼定制画框解锁的地方和现在的不一样
+                    // 1.7.5.0.4 之后，类名迁移，内部文件改动较大，此为临时解决方案
+                    if (isNewMediaeditor) {
                         declaredClass("com.miui.mediaeditor.config.galleryframe.GalleryFrameAccessUtils")
                     } else {
                         declaredClass("com.miui.mediaeditor.photo.config.galleryframe.GalleryFrameAccessUtils")
                     }
-                    addUsingField {
-                        // 如果后面有改的话请释放这个注释
-                        // modifiers = Modifier.STATIC or Modifier.FINAL
-                        type = "java.util.List"
-                    }
+                    modifiers = Modifier.FINAL or Modifier.STATIC
                     returnType = "boolean"
+                    paramCount = 0
+                    // 1.6.5.10.2 以上方法查找完剩下 a() c() e()
                 }
             }
         }
     }
 
-    private val springA by lazy<Field> {
-        DexKit.findMember("SA") { bridge ->
-            bridge.findField {
+    // 公共解锁特定机型定制画框使用限制
+    private val publicB by lazy<List<Method>> {
+        DexKit.findMemberList("PB") { dexkit ->
+            dexkit.findMethod {
                 matcher {
-                    // 改动日志:
-                    // 这里是新春定制画框的解锁，仅在部分版本中存在
-                    // 这里的 declaredClass 不清楚米米还会不会混淆，混淆了非常麻烦，比如从 1.3 - 1.4 版本的混淆
-                    if (isHookType) {
+                    if (isNewMediaeditor) {
                         declaredClass("com.miui.mediaeditor.config.galleryframe.GalleryFrameAccessUtils")
                     } else {
                         declaredClass("com.miui.mediaeditor.photo.config.galleryframe.GalleryFrameAccessUtils")
                     }
-                    modifiers = Modifier.STATIC
-                    type = "boolean"
-                }
-            }.single()
-        }
-    }
-
-    private val redmiB by lazy<Field> {
-        DexKit.findMember("RB") { bridge ->
-            bridge.findField {
-                matcher {
-                    // 改动日志:
-                    // 这里是红米定制联名画框的解锁
-                    // 从 1.6.5.10.2 版本开始，解锁此画框的难度较高
-                    // 这里的 declaredClass 不清楚米米还会不会混淆，混淆了非常麻烦，比如从 1.3 - 1.4 版本的混淆
-                    if (isHookType) {
-                        declaredClass("com.miui.mediaeditor.config.galleryframe.GalleryFrameAccessUtils")
-                    } else {
-                        declaredClass("com.miui.mediaeditor.photo.config.galleryframe.GalleryFrameAccessUtils")
+                    // modifiers = Modifier.STATIC // 1.6.5.10.2 改成 STATIC，原来是 FINAL
+                    returnType = "boolean"
+                    paramCount = 1
+                    addUsingField {
+                        modifiers = Modifier.STATIC or Modifier.FINAL
                     }
-                    modifiers = Modifier.STATIC or Modifier.FINAL
                 }
-            }.last()
+            }
         }
     }
 
     override fun init() {
-        var index = 0
-        val actions = listOf<(Method) -> Unit>(::xiaomi, ::poco, ::redmi)
-
-        methodA.forEach { method ->
-            if (method.paramCount == 1) {
-                // 公共解锁特定机型定制画框使用限制
-                other(method)
-            } else {
-                val action = actions[index]
-                action(method)
-                index = (index + 1) % actions.size
+        val publicC = DexKit.findMemberList<Method>("PC") {
+            it.findMethod {
+                matcher {
+                    publicA.forEach { a ->
+                        name = a.name
+                    }
+                    usingFields {
+                        add {
+                            type = "boolean"
+                        }
+                    }
+                }
             }
         }
 
-        if (isOpenSpring && springA != null) {
-            springA.setBoolean(null, true)
+        val actions = if (isNewMediaeditor) {
+            listOf<(Method) -> Unit>(::xiaomi, ::redmi, ::poco, ::other)
+        } else {
+            listOf<(Method) -> Unit>(::xiaomi, ::poco, ::redmi, ::other)
+        }
+        val differentItems = publicA.subtract(publicC.toSet())
+        var index = 0
+
+        differentItems.forEach { method ->
+            val action = actions.getOrElse(index) { ::other }
+            action(method)
+            index = (index + 1) % actions.size
         }
 
-        /*if (isRedmi && redmiB != null) {
+        publicB.forEach { method ->
+            other(method)
+        }
 
+        /*if (isOpenSpring && publicC.isNotEmpty()) {
+            publicC.forEach { method ->
+                other(method)
+                // 1.6.0.5.2 新增限时新春定制画框
+                // 后续版本已移除，其实可以删掉的，但还是留着吧，兴许后面可能还有用
+            }
         }*/
     }
 
