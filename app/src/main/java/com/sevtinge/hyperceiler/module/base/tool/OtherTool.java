aa407fc1ee49ce406075605eb3eb180e1fe79d13
铃柒柒 <lingqiqi233@gmail.com>
2025-01-10 14:14:48 +0800

    fix: 修复 手机管家-全局侧边栏-隐藏侧边栏 功能失效的问题

--- app/src/main/java/com/sevtinge/hyperceiler/module/base/tool/OtherTool.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/base/tool/OtherTool.java
@@ -128,11 +128,12 @@ public class OtherTool {
     public static String getPackageVersionName(XC_LoadPackage.LoadPackageParam lpparam) {
         try {
             Class<?> parserCls = XposedHelpers.findClass("android.content.pm.PackageParser", lpparam.classLoader);
-            Object parser = parserCls.newInstance();
+            Object parser = parserCls.getDeclaredConstructor().newInstance();
             File apkPath = new File(lpparam.appInfo.sourceDir);
             Object pkg = XposedHelpers.callMethod(parser, "parsePackage", apkPath, 0);
             return (String) XposedHelpers.getObjectField(pkg, "mVersionName");
         } catch (Throwable e) {
+            logE("getPackageVersionCode", e);
             return "";
         }
     }
@@ -140,11 +141,12 @@ public class OtherTool {
     public static int getPackageVersionCode(XC_LoadPackage.LoadPackageParam lpparam) {
         try {
             Class<?> parserCls = XposedHelpers.findClass("android.content.pm.PackageParser", lpparam.classLoader);
-            Object parser = parserCls.newInstance();
+            Object parser = parserCls.getDeclaredConstructor().newInstance();
             File apkPath = new File(lpparam.appInfo.sourceDir);
             Object pkg = XposedHelpers.callMethod(parser, "parsePackage", apkPath, 0);
             return XposedHelpers.getIntField(pkg, "mVersionCode");
         } catch (Throwable e) {
+            logE("getPackageVersionCode", e);
             return -1;
         }
     }
