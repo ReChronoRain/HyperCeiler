1b348b7ca755965b32ff1432d7dea5c520eccc54
xzakota <xzakota@gmail.com>
2025-01-30 23:28:20 +0800

    opt: when enabling hide smart screen, do not hide the bottom left button

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemui/lockscreen/RemoveSmartScreen.kt
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/systemui/lockscreen/RemoveSmartScreen.kt
@@ -18,28 +18,14 @@
 */
 package com.sevtinge.hyperceiler.module.hook.systemui.lockscreen
 
-import android.view.View
-import android.widget.LinearLayout
-import com.github.kyuubiran.ezxhelper.ClassUtils.loadClassOrNull
-import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
-import com.github.kyuubiran.ezxhelper.ObjectUtils
-import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 import com.sevtinge.hyperceiler.module.base.BaseHook
-import com.sevtinge.hyperceiler.utils.devicesdk.isMoreAndroidVersion
-import com.sevtinge.hyperceiler.utils.devicesdk.isMoreHyperOSVersion
+import com.sevtinge.hyperceiler.module.base.tool.HookTool.MethodHook.returnConstant
 
 object RemoveSmartScreen : BaseHook() {
     override fun init() {
-        loadClassOrNull("com.android.keyguard.injector.KeyguardBottomAreaInjector")!!.methodFinder()
-            .filterByName("updateIcons")
-            .single().createHook {
-                after {
-                    val left =
-                        ObjectUtils.getObjectOrNullAs<LinearLayout>(it.thisObject, "mLeftAffordanceViewLayout") ?: return@after
-                    left.visibility = View.GONE
-                }
-            }
-
+        findAndHookMethod(
+            "com.android.keyguard.negative.KeyguardMoveLeftController", "isLeftViewLaunchActivity",
+            returnConstant(false)
+        )
     }
-
 }
