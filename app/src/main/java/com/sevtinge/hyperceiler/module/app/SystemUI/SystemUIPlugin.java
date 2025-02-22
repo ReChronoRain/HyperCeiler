61465bf4e10171e41964e5686adb2c47a1e6c953
铃柒柒 <lingqiqi233@gmail.com>
2025-02-22 23:26:49 +0800

    opt: Focus notification background color mixing Support flashlight notification (#1139)

---
+++ app/src/main/java/com/sevtinge/hyperceiler/module/app/SystemUI/SystemUIPlugin.java
@@ -0,0 +1,13 @@
+package com.sevtinge.hyperceiler.module.app.SystemUI;
+
+import com.hchen.database.HookBase;
+import com.sevtinge.hyperceiler.module.base.BaseModule;
+import com.sevtinge.hyperceiler.module.hook.systemui.plugin.FlashLightNotificationColor;
+
+@HookBase(targetPackage = "miui.systemui.plugin")
+public class SystemUIPlugin extends BaseModule {
+    @Override
+    public void handleLoadPackage() {
+        initHook(FlashLightNotificationColor.INSTANCE, mPrefsMap.getBoolean("system_ui_control_center_opt_notification_element_background_color"));
+    }
+}
