4f03b0aa78e061925170104047f14f9088d93d27
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2025-03-08 03:12:48 +0800

    Update CustomWatermark.java

--- app/src/main/java/com/sevtinge/hyperceiler/module/hook/camera/CustomWatermark.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/hook/camera/CustomWatermark.java
@@ -59,14 +59,18 @@ public class CustomWatermark extends BaseHook {
         for (Method method : methods) {
             // Method method = methodData.getMethodInstance(lpparam.classLoader);
             logD(TAG, lpparam.packageName, "Current hooking method is " + method);
-            hookMethod(method, new MethodHook() {
-                @Override
-                protected void before(MethodHookParam param) throws Throwable {
-                    SparseArray<String[]> sparseArray = new SparseArray<>(1);
-                    sparseArray.put(0, new String[]{mPrefsMap.getString("camera_custom_watermark_manufacturer", "XIAOMI"), mPrefsMap.getString("camera_custom_watermark_device", "MI PHONE")});
-                    param.setResult(sparseArray);
-                }
-            });
+            try {
+                hookMethod(method, new MethodHook() {
+                    @Override
+                    protected void before(MethodHookParam param) throws Throwable {
+                        SparseArray<String[]> sparseArray = new SparseArray<>(1);
+                        sparseArray.put(0, new String[]{mPrefsMap.getString("camera_custom_watermark_manufacturer", "XIAOMI"), mPrefsMap.getString("camera_custom_watermark_device", "MI PHONE")});
+                        param.setResult(sparseArray);
+                    }
+                });
+            } catch (Exception e) {
+                logE(TAG, lpparam.packageName, e);
+            }
         }
     }
 }
