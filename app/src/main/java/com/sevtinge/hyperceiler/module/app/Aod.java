eb1463eff2b5a7145e32f2000c052c2ffd85fd47
铃柒柒 <lingqiqi233@gmail.com>
2025-04-04 12:00:54 +0800

    fix: HideDelimiter hook failed by HyperOS2.0.200+ (type=1 still failed)
    feat: add hyperceiler started on miui_keyguard_shortcuts

--- app/src/main/java/com/sevtinge/hyperceiler/module/app/Aod.java
+++ app/src/main/java/com/sevtinge/hyperceiler/module/app/Aod.java
@@ -22,11 +22,13 @@ import com.hchen.database.HookBase;
 import com.sevtinge.hyperceiler.module.base.BaseModule;
 import com.sevtinge.hyperceiler.module.hook.aod.UnlockAlwaysOnDisplay;
 import com.sevtinge.hyperceiler.module.hook.aod.UnlockAodAon;
+import com.sevtinge.hyperceiler.module.hook.aod.UnlockShortCuts;
 
 @HookBase(targetPackage = "com.miui.aod")
 public class Aod extends BaseModule {
     @Override
     public void handleLoadPackage() {
+        initHook(UnlockShortCuts.INSTANCE, true);
         initHook(UnlockAlwaysOnDisplay.INSTANCE, mPrefsMap.getBoolean("aod_unlock_always_on_display"));
         initHook(new UnlockAodAon(), mPrefsMap.getBoolean("aod_unlock_aon"));
     }
