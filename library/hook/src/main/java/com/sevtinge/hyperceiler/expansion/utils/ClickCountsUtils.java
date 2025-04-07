335649578c123ab6a941deff79d1a233030eb8dd
Fan095 <732388462@qq.com>
2025-04-07 21:15:58 +0800

    更新build.gradle.kts

---
+++ library/hook/src/main/java/com/sevtinge/hyperceiler/expansion/utils/ClickCountsUtils.java
@@ -0,0 +1,9 @@
+package com.sevtinge.hyperceiler.expansion.utils;
+
+public class ClickCountsUtils {
+
+    public static Integer getClickCounts() {
+        return 0;
+    }
+
+}
---
+++ library/hook/src/main/java/com/sevtinge/hyperceiler/expansion/utils/ShellSafeUtils.java
@@ -0,0 +1,9 @@
+package com.sevtinge.hyperceiler.expansion.utils;
+
+public class ShellSafeUtils {
+
+    public static boolean isSafeCommand(String str) {
+        return false;
+    }
+
+}
