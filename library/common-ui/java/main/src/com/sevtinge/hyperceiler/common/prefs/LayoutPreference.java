4b4061bc555c6311f43c9f32c1b7fe61512f876a
Fan095 <732388462@qq.com>
2025-04-18 18:09:30 +0800

    update LayoutPreference

--- library/common-ui/java/main/src/com/sevtinge/hyperceiler/common/prefs/LayoutPreference.java
+++ library/common-ui/java/main/src/com/sevtinge/hyperceiler/common/prefs/LayoutPreference.java
@@ -2,11 +2,14 @@ package com.sevtinge.hyperceiler.common.prefs;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,10 +18,14 @@ import androidx.preference.PreferenceViewHolder;
 
 import com.sevtinge.hyperceiler.ui.R;
 
-import fan.preference.FolmeAnimationController;
 import fan.preference.PreferenceStyle;
 
 public class LayoutPreference extends Preference implements PreferenceStyle {
+
+    private int mLayoutResId;
+    private CharSequence mTtitle;
+
+    private Drawable mBackground;
 
     private View mRootView;
     private final View.OnClickListener mClickListener = view -> performClick(view);
@@ -43,18 +50,37 @@ public class LayoutPreference extends Preference implements PreferenceStyle {
     }
 
     private void init(Context context, AttributeSet attrs, int defStyleAttr) {
-        TypedArray a = context.obtainStyledAttributes(attrs, fan.preference.R.styleable.Preference, defStyleAttr, 0);
-        int layoutResId = a.getResourceId(fan.preference.R.styleable.Preference_android_layout, 0);
-        if (layoutResId == 0) throw new IllegalArgumentException("LayoutPreference requires a layout to be defined");
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.LayoutPreference, defStyleAttr, 0);
+        mLayoutResId = a.getResourceId(R.styleable.LayoutPreference_android_layout, 0);
+        if (mLayoutResId == 0) throw new IllegalArgumentException("LayoutPreference requires a layout to be defined");
+        mTtitle = a.getText(R.styleable.LayoutPreference_android_title);
+        mBackground = a.getDrawable(R.styleable.LayoutPreference_android_background);
         a.recycle();
-        setView(LayoutInflater.from(getContext()).inflate(layoutResId, null, false));
+        setView(LayoutInflater.from(getContext()).inflate(mLayoutResId, null, false));
     }
 
 
     private void setView(View view) {
         setLayoutResource(R.layout.preference_layout_frame);
         mRootView = view;
+        setLayoutTitle(mTtitle);
+        setLayoutBackground(mBackground);
         setShouldDisableView(false);
+    }
+
+    private void setLayoutTitle(CharSequence title) {
+        TextView titleView = findViewById(android.R.id.title);
+        if (titleView != null) {
+            titleView.setText(title);
+            titleView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void setLayoutBackground(Drawable background) {
+        FrameLayout containerView = findViewById(R.id.container);
+        if (containerView != null && background != null) {
+            containerView.setBackground(background);
+        }
     }
 
     public <T extends View> T findViewById(int id) {
