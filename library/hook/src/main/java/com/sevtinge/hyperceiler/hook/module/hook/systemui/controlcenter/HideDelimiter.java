2461a959f22fbda0a9a9b0cf4f4c44ad69844bf5
绀漓丨Sevtinge <89193494+Sevtinge@users.noreply.github.com>
2025-05-23 16:39:15 +0800

    fix: hide delimiter pref3 on os2.2/2.3

--- library/hook/src/main/java/com/sevtinge/hyperceiler/hook/module/hook/systemui/controlcenter/HideDelimiter.java
+++ library/hook/src/main/java/com/sevtinge/hyperceiler/hook/module/hook/systemui/controlcenter/HideDelimiter.java
@@ -19,6 +19,7 @@
 package com.sevtinge.hyperceiler.hook.module.hook.systemui.controlcenter;
 
 import static com.sevtinge.hyperceiler.hook.utils.PropUtils.getProp;
+import static com.sevtinge.hyperceiler.hook.utils.devicesdk.SystemSDKKt.getSystemVersionIncremental;
 import static com.sevtinge.hyperceiler.hook.utils.devicesdk.SystemSDKKt.isMoreAndroidVersion;
 
 import android.telephony.SubscriptionInfo;
@@ -112,6 +113,7 @@ public class HideDelimiter extends BaseHook {
             if (prefs == 3) {
                 if (isMoreAndroidVersion(35)) {
 
+                    if (!getSystemVersionIncremental().startsWith("OS2.0.20") && !getSystemVersionIncremental().startsWith("OS2.0.30")) {
                     findAndHookMethod("miui.stub.statusbar.StatusBarStub$registerMiuiCarrierTextController$1$addCallback$callback$1", "onCarrierTextChanged", String.class, new MethodHook() {
                         @Override
                         protected void before(MethodHookParam param) throws Throwable {
@@ -134,6 +136,7 @@ public class HideDelimiter extends BaseHook {
                         }
                     });
 
+                    }
                     try {
                         findAndHookMethod("com.android.systemui.statusbar.policy.MiuiCarrierTextControllerImpl", "updateCarrierText", new MethodHook() {
                             @Override
