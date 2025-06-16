764e43682ef71ca9b5061316e3d1fac805926d98
ciimina <gemingjia0201@163.com>
2025-06-16 23:28:04 +0800

    feat: add custom home recent card view height

---
+++ library/hook/src/main/java/com/sevtinge/hyperceiler/hook/module/hook/home/recent/TaskViewHeight.kt
@@ -0,0 +1,22 @@
+package com.sevtinge.hyperceiler.hook.module.hook.home.recent
+
+import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
+import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
+import com.sevtinge.hyperceiler.hook.module.base.BaseHook
+import com.sevtinge.hyperceiler.hook.utils.hookBeforeMethod
+
+object TaskViewHeight : BaseHook() {
+    override fun init() {
+        val taskViewHeightValue by lazy {
+            mPrefsMap.getInt("home_recent_task_view_height", 52).toFloat() / 100
+        }
+
+        loadClass("com.miui.home.recents.layoutconfig.TaskHorizonalLayoutConfig")
+            .methodFinder()
+            .filterByName("getTaskViewCenterYInWindowFraction")
+            .first()
+            .hookBeforeMethod {
+                it.result = taskViewHeightValue
+            }
+    }
+}
