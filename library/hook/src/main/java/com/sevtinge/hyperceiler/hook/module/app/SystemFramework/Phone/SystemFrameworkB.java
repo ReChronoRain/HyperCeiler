d8c94f03a34ec3d5166fbd8a34ee6f76ca873a01
柒柒喵 <lingqiqi233@gmail.com>
2025-07-05 14:47:51 +0800

    adj: add core patch/flag secure android 16 support

---
+++ library/hook/src/main/java/com/sevtinge/hyperceiler/hook/module/app/SystemFramework/Phone/SystemFrameworkB.java
@@ -0,0 +1,42 @@
+/*
+ * This file is part of HyperCeiler.
+
+ * HyperCeiler is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+ * Copyright (C) 2023-2025 HyperCeiler Contributions
+ */
+package com.sevtinge.hyperceiler.hook.module.app.SystemFramework.Phone;
+
+import com.hchen.database.HookBase;
+import com.sevtinge.hyperceiler.hook.module.base.BaseModule;
+import com.sevtinge.hyperceiler.hook.module.hook.systemframework.FlagSecure;
+import com.sevtinge.hyperceiler.hook.module.hook.systemframework.corepatch.AllowUpdateSystemApp;
+import com.sevtinge.hyperceiler.hook.module.hook.systemframework.corepatch.BypassIsolationViolation;
+import com.sevtinge.hyperceiler.hook.module.hook.systemframework.corepatch.BypassSignCheckForT;
+
+@HookBase(targetPackage = "android", isPad = 2, targetSdk = 36)
+public class SystemFrameworkB extends BaseModule {
+
+    @Override
+    public void handleLoadPackage() {
+
+        // 核心破解
+        initHook(BypassSignCheckForT.INSTANCE, mPrefsMap.getBoolean("system_framework_core_patch_auth_creak") || mPrefsMap.getBoolean("system_framework_core_patch_disable_integrity"));
+        initHook(new BypassIsolationViolation(), mPrefsMap.getBoolean("system_framework_core_patch_bypass_isolation_violation"));
+        initHook(new AllowUpdateSystemApp(), mPrefsMap.getBoolean("system_framework_core_patch_allow_update_system_app"));
+
+        // 其它
+        initHook(new FlagSecure(), mPrefsMap.getBoolean("system_other_flag_secure"));
+    }
+}
