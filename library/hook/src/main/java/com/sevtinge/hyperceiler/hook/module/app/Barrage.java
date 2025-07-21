9de63e3f493318aa6270a82dc23cef6cab067d18
柒柒喵 <lingqiqi233@gmail.com>
2025-07-21 13:04:01 +0800

    feat: add Allow global use of barrage notifications ＆ Barrage notification not touchable

--- library/hook/src/main/java/com/sevtinge/hyperceiler/hook/module/app/Barrage.java
+++ library/hook/src/main/java/com/sevtinge/hyperceiler/hook/module/app/Barrage.java
@@ -21,13 +21,17 @@ package com.sevtinge.hyperceiler.hook.module.app;
 import com.hchen.database.HookBase;
 import com.sevtinge.hyperceiler.hook.module.base.BaseModule;
 import com.sevtinge.hyperceiler.hook.module.hook.barrage.AnyBarrage;
+import com.sevtinge.hyperceiler.hook.module.hook.barrage.BarrageNotTouchable;
 import com.sevtinge.hyperceiler.hook.module.hook.barrage.CustomBarrageLength;
+import com.sevtinge.hyperceiler.hook.module.hook.barrage.GlobalBarrage;
 
 @HookBase(targetPackage = "com.xiaomi.barrage")
 public class Barrage extends BaseModule {
     @Override
     public void handleLoadPackage() {
         initHook(AnyBarrage.INSTANCE, mPrefsMap.getBoolean("barrage_any_barrage"));
+        initHook(BarrageNotTouchable.INSTANCE, mPrefsMap.getBoolean("barrage_not_touchable"));
+        initHook(GlobalBarrage.INSTANCE, mPrefsMap.getBoolean("barrage_global_enable"));
         initHook(CustomBarrageLength.INSTANCE, mPrefsMap.getInt("barrage_custom_barrage_length", 36) != 36);
     }
 }
