25556b57c916c68122e993770b696b9407bf660b
Crypto Beaver <100139179+Crypt0Beaver@users.noreply.github.com>
2025-08-31 10:22:30 +0200

    Update LayoutRules.kt

--- library/hook/src/main/java/com/sevtinge/hyperceiler/hook/module/hook/home/layout/LayoutRules.kt
+++ library/hook/src/main/java/com/sevtinge/hyperceiler/hook/module/hook/home/layout/LayoutRules.kt
@@ -146,17 +146,17 @@ object LayoutRules : HomeBaseHook() {
                 -1
             }
 
-            val sWorkspacePaddingBottom = 0 as Int
-            /* val sWorkspacePaddingBottom = if (isSetWSPaddingBottomHook) {
+            // val sWorkspacePaddingBottom = 0 as Int
+            val sWorkspacePaddingBottom = if (isSetWSPaddingBottomHook) {
                 DisplayUtils.dp2px(
                     mPrefsMap.getInt(
                         "home_layout_workspace_padding_bottom",
                         0
                     ).toFloat()
-                )
+                ) as Int
             } else {
                 -1
-            } */
+            }
 
             val sWorkspaceCellSide = if (isSetWSPaddingSideHook) {
                 DisplayUtils.dp2px(
