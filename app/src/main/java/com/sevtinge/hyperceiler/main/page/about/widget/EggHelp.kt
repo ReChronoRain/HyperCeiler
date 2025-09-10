60841a0531615fc9ce2c58e29744de1c579e54cf
ghhccghk <2137610394@qq.com>
2025-09-10 19:13:29 +0800

    feat: 在关于页面添加Logo彩蛋
    - 在`VersionCard.java`中：
        - 为Logo视图添加了点击和长按监听器。
        - 点击时，会随机显示一条Toast消息。
        - 长按时，如果应用有通知权限，则会发送一个包含随机消息的通知，该通知在5秒后自动消失；若无权限，则仅显示Toast。
        - 新增了检查通知权限和创建通知渠道的方法。
    - 在`strings.xml`（默认和中文）中：
        - 添加了三个新的字符串资源，用作Logo彩蛋的随机消息。
    - 新增`EggHelp.kt`文件：
        - 包含`focusBuild`方法，用于构建发送通知时所需的Bundle数据。
        - 包含`buildIslandTemplate`私有方法，用于创建灵动岛通知的模板。
    
    Signed-off-by: ghhccghk <2137610394@qq.com>

---
+++ app/src/main/java/com/sevtinge/hyperceiler/main/page/about/widget/EggHelp.kt
@@ -0,0 +1,57 @@
+package com.sevtinge.hyperceiler.main.page.about.widget
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.Icon
+import android.os.Bundle
+import com.hyperfocus.api.FocusApi
+import com.hyperfocus.api.IslandApi
+import org.json.JSONObject
+
+object EggHelp {
+    fun focusBuild(string: String, context: Context): Bundle {
+
+        val icon =
+            Icon.createWithResource(context, com.sevtinge.hyperceiler.ui.R.drawable.ic_hyperceiler)
+                .apply { setTint(Color.WHITE) }
+        val darkIcon =
+            Icon.createWithResource(context, com.sevtinge.hyperceiler.ui.R.drawable.ic_hyperceiler)
+                .apply { setTint(Color.BLACK) }
+        val baseinfo = FocusApi.baseinfo(basetype = 1, title = string)
+
+
+        return FocusApi.sendFocus(
+            ticker = string,
+            aodTitle = string,
+            island = buildIslandTemplate(string),
+            aodPic = icon,
+            baseInfo = baseinfo,
+            updatable = true,
+            enableFloat = false,
+            timeout = 120,
+            picticker = icon,
+            pictickerdark = darkIcon
+        )
+
+    }
+
+    private fun buildIslandTemplate(title: String): JSONObject {
+        val picInfo = IslandApi.PicInfo(pic = "miui.focus.pic_ticker")
+        val left = IslandApi.ImageTextInfo(
+            textInfo = IslandApi.TextInfo(title = title),
+            picInfo = picInfo
+        )
+        val right = IslandApi.ImageTextInfo(
+            picInfo = picInfo,
+            type = 2
+        )
+        val bigIsland =
+            IslandApi.BigIslandArea(imageTextInfoLeft = left, imageTextInfoRight = right)
+        val smallIsland =
+            IslandApi.SmallIslandArea(picInfo = IslandApi.PicInfo(pic = "miui.focus.pic_ticker"))
+        return IslandApi.IslandTemplate(
+            bigIslandArea = bigIsland,
+            smallIslandArea = smallIsland
+        )
+    }
+}
--- app/src/main/java/com/sevtinge/hyperceiler/main/page/about/widget/VersionCard.java
+++ app/src/main/java/com/sevtinge/hyperceiler/main/page/about/widget/VersionCard.java
@@ -18,12 +18,16 @@
  */
 package com.sevtinge.hyperceiler.main.page.about.widget;
 
+import static androidx.core.content.ContextCompat.getSystemService;
 import static com.sevtinge.hyperceiler.common.utils.PersistConfig.isAprilFoolsThemeView;
 
 import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.BlendMode;
 import android.graphics.Canvas;
 import android.graphics.Paint;
@@ -43,9 +47,12 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 
 import com.sevtinge.hyperceiler.BuildConfig;
 import com.sevtinge.hyperceiler.R;
@@ -53,6 +60,8 @@ import com.sevtinge.hyperceiler.common.utils.SettingsFeatures;
 import com.sevtinge.hyperceiler.common.view.CubicEaseOutInterpolater;
 import com.sevtinge.hyperceiler.hook.utils.devicesdk.DisplayUtils;
 import com.sevtinge.hyperceiler.main.page.about.controller.LogoAnimationController;
+
+import java.util.Random;
 
 import fan.cardview.HyperCardView;
 import fan.core.utils.MiuiBlurUtils;
@@ -142,6 +151,44 @@ public class VersionCard extends FrameLayout implements View.OnClickListener {
             mHandler.sendEmptyMessageDelayed(0, 1500L);
         }
         setLogoBlur();
+        mLogoView.setOnClickListener(v -> {
+            String[] messages = getResources().getStringArray(R.array.logo_click_egg_messages);
+            int index = new Random().nextInt(messages.length);
+            String msg = messages[index];
+            Toast.makeText(getContext(), msg, Toast.LENGTH_SHORT).show();
+        });
+        mLogoView.setOnLongClickListener(v -> {
+            String[] messages = getResources().getStringArray(R.array.logo_click_egg_messages);
+            int index = new Random().nextInt(messages.length);
+            String msg = messages[index];
+
+            if (!hasNotificationPermission()) {
+                Toast.makeText(getContext(), msg, Toast.LENGTH_SHORT).show();
+                return true;
+            } else {
+                createNotificationChannel();
+                // 发送通知
+                NotificationManager notificationManager = getSystemService(getContext(), NotificationManager.class);
+                int notificationId = "logo_channel_id".hashCode();
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(getContext(), "logo_channel_id")
+                    .setSmallIcon(com.sevtinge.hyperceiler.ui.R.drawable.ic_hyperceiler)
+                    .setContentTitle("彩蛋触发通知")
+                    .setContentText(msg)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setAutoCancel(true);
+                builder.addExtras(EggHelp.INSTANCE.focusBuild(msg, getContext()));
+
+                notificationManager.notify(notificationId, builder.build());
+
+                // 5 秒后自动关闭
+                new Handler(Looper.getMainLooper()).postDelayed(() -> {
+                    notificationManager.cancel(notificationId);
+                }, 5000);
+
+
+                return true;
+            }
+        });
     }
 
     public void checkUpdate() {
@@ -291,5 +338,26 @@ public class VersionCard extends FrameLayout implements View.OnClickListener {
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
         mHandler.removeMessages(0);
+    }
+
+    private boolean hasNotificationPermission() {
+        return ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.POST_NOTIFICATIONS)
+            == PackageManager.PERMISSION_GRANTED;
+    }
+
+
+    private void createNotificationChannel() {
+        String channelId = "logo_channel_id";
+        String channelName = "彩蛋通知";
+        String channelDescription = "彩蛋触发通知";
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+        NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
+        channel.setDescription(channelDescription);
+
+        NotificationManager notificationManager = getSystemService(getContext(), NotificationManager.class);
+        if (notificationManager != null) {
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
