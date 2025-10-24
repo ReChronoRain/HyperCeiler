bc021b0f511798e64dba14c1f7b2ad1159e41e80
柒柒喵 <lingqiqi233@gmail.com>
2025-10-24 22:02:03 +0800

    app: update version to 2.6.163

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,7 +76,7 @@ android {
         minSdk = 35
         targetSdk = 36
         versionCode = getVersionCode()
-        versionName = "2.6.162"
+        versionName = "2.6.163"
 
         val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").apply {
             timeZone = TimeZone.getTimeZone("Asia/Shanghai")
