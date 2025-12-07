ccf93f31e7a1a4ae79d4ffea0e73f33be3216603
cedrixm <cedric-git@moriarchting.com>
2025-12-07 20:03:51 +0100

    Fix CornerSlide Module exception Adapt to match HOS v2 and v3 signatures

--- library/hook/src/main/java/com/sevtinge/hyperceiler/hook/module/rules/home/gesture/CornerSlide.java
+++ library/hook/src/main/java/com/sevtinge/hyperceiler/hook/module/rules/home/gesture/CornerSlide.java
@@ -31,16 +31,29 @@ import de.robv.android.xposed.XposedHelpers;
 public class CornerSlide extends HomeBaseHookNew {
     public int inDirection = 0;
 
+    private Class<?> canTriggerAssistantActionParam3;
+
     Context mContext;
+
+    @SuppressWarnings("unused")
+    @Version(isPad = false, min = 600000000)
+    private void initOS3Hook() {
+        canTriggerAssistantActionParam3 = long.class;
+        initBaseCore();
+    }
 
     @Override
     public void initBase() {
+        canTriggerAssistantActionParam3 = int.class;
+        initBaseCore();
+    }
+    public void initBaseCore() {
         findAndHookMethod("com.android.systemui.shared.recents.system.AssistManager",
             "isSupportGoogleAssist", int.class,
             MethodHook.returnConstant(true));
         Class<?> FsGestureAssistHelper = findClassIfExists("com.miui.home.recents.FsGestureAssistHelper");
         findAndHookMethod(FsGestureAssistHelper, "canTriggerAssistantAction",
-            float.class, float.class, long.class,
+            float.class, float.class, canTriggerAssistantActionParam3,
             new MethodHook() {
                 @Override
                 protected void before(MethodHookParam param) {
