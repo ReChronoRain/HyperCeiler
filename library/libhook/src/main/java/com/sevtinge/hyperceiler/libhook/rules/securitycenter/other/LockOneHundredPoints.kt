17b346075c2bea8a85de4f295c7be5f1641ded23
柒柒喵 <lingqiqi233@gmail.com>
2026-02-01 01:02:50 +0800

    fix: LockOneHundredPoints hook failed on new verion

--- library/libhook/src/main/java/com/sevtinge/hyperceiler/libhook/rules/securitycenter/other/LockOneHundredPoints.kt
+++ library/libhook/src/main/java/com/sevtinge/hyperceiler/libhook/rules/securitycenter/other/LockOneHundredPoints.kt
@@ -20,9 +20,7 @@ package com.sevtinge.hyperceiler.libhook.rules.securitycenter.other
 
 import android.view.View
 import com.sevtinge.hyperceiler.libhook.base.BaseHook
-import com.sevtinge.hyperceiler.libhook.utils.api.DeviceHelper.Miui.isPad
 import com.sevtinge.hyperceiler.libhook.utils.hookapi.dexkit.DexKit
-import com.sevtinge.hyperceiler.libhook.utils.hookapi.tool.AppsTool
 import com.sevtinge.hyperceiler.libhook.utils.log.XposedLog
 import io.github.kyuubiran.ezxhelper.core.finder.MethodFinder.`-Static`.methodFinder
 import io.github.kyuubiran.ezxhelper.core.util.ClassUtil.loadClass
@@ -31,7 +29,7 @@ import org.luckypray.dexkit.query.enums.StringMatchType
 import java.lang.reflect.Method
 
 object LockOneHundredPoints : BaseHook() {
-    private val score by lazy<Method> {
+    private val scoreOld by lazy<Method> {
         DexKit.findMember("LockOneHundredPoints1") {
             it.findClass {
                 matcher {
@@ -46,7 +44,7 @@ object LockOneHundredPoints : BaseHook() {
         }
     }
 
-    private val scoreOld by lazy<Method> {
+    private val score by lazy<Method> {
         DexKit.findMember("LockOneHundredPoints2") {
             it.findClass {
                 matcher {
@@ -54,7 +52,7 @@ object LockOneHundredPoints : BaseHook() {
                 }
             }.findMethod {
                 matcher {
-                    usingNumbers(41, 100)
+                    usingNumbers(41, 100, 0)
                     returnType = "int"
                 }
             }.single()
@@ -83,6 +81,22 @@ object LockOneHundredPoints : BaseHook() {
         }
     }
 
+    private val addRiskApp by lazy<Method> {
+        DexKit.findMember("AddRiskApp") {
+            it.findClass {
+                matcher {
+                    className = "com.miui.securityscan.scanner.ScoreManager"
+                }
+            }.findMethod {
+                matcher {
+                    addInvoke("Landroid/text/TextUtils;->isEmpty(Ljava/lang/CharSequence;)Z")
+                    paramCount = 1
+                    returnType = "void"
+                }
+            }.single()
+        }
+    }
+
     override fun init() {
         loadClass("com.miui.securityscan.ui.main.MainContentFrame").methodFinder()
             .filterByName("onClick")
@@ -92,20 +106,17 @@ object LockOneHundredPoints : BaseHook() {
             }
 
         runCatching {
-            XposedLog.d(TAG, lpparam.packageName, "LockOneHundredPoints method is $score")
-            XposedLog.d(TAG, lpparam.packageName, "LockOneHundredPoints 3 method is $score3")
-
-            if ((AppsTool.getPackageVersionCode(lpparam) <= 40001000 && !isPad) || (AppsTool.getPackageVersionCode(lpparam) <= 40011000 && isPad)) {
-                XposedLog.d(TAG, lpparam.packageName, "LockOneHundredPoints old method is $scoreOld")
-                scoreOld.createHook {
-                    returnConstant(0)
-                }
+            scoreOld.createHook {
+                returnConstant(0)
             }
             score.createHook {
-                returnConstant(0)
+                returnConstant(100)
             }
             score3.createHook {
                 returnConstant(true)
+            }
+            addRiskApp.createHook {
+                returnConstant(null)
             }
         }.onFailure {
             XposedLog.e(TAG, lpparam.packageName, "LockOneHundredPoints hook Failed: $it")
