1a7745a61c30f1cce418664848631038b900279a
ghhccghk <2137610394@qq.com>
2026-03-22 22:09:41 +0800

    feat: 新增背屏禁止返回桌面功能并优化设置布局
    - **新增功能**: 在 `FuckSubScreenWhiteList` 中新增 Hook 逻辑，支持禁止背屏在息屏或关闭时自动返回桌面。
    - **界面优化**: 在 `framework_display.xml` 中新增“背屏”设置分类（PreferenceCategory），并将背屏白名单与新功能选项归类。
    - **资源更新**: 补充了中英文对应的字符串资源 `system_framework_fuck_subscreen_not_go_to_home` 及 `subscreen`。
    
    Signed-off-by: ghhccghk <2137610394@qq.com>

--- library/libhook/src/main/java/com/sevtinge/hyperceiler/libhook/rules/systemframework/display/FuckSubScreenWhiteList.kt
+++ library/libhook/src/main/java/com/sevtinge/hyperceiler/libhook/rules/systemframework/display/FuckSubScreenWhiteList.kt
@@ -1,9 +1,12 @@
 package com.sevtinge.hyperceiler.libhook.rules.systemframework.display
 
+import com.sevtinge.hyperceiler.common.utils.PrefsBridge
 import com.sevtinge.hyperceiler.libhook.base.BaseHook
 import com.sevtinge.hyperceiler.libhook.utils.hookapi.tool.hookAllMethods
 
+/** 绕过背屏白名单*/
 object FuckSubScreenWhiteList : BaseHook() {
+
     override fun init() {
         val asI = findClass("com.android.server.wm.ActivityStarterImpl")
         asI.hookAllMethods("isShouldShowOnRearDisplay") {
@@ -15,6 +18,17 @@ object FuckSubScreenWhiteList : BaseHook() {
             before {
                 returnConstant(true)
             }
+        }
+        /** 禁止锁屏返回桌面 */
+        val notGotoHome = PrefsBridge.getBoolean("system_framework_fuck_subscreen_not_go_to_home")
+
+        if (notGotoHome) {
+            asI.hookAllMethods("handlerTransitionFinished") {
+                before { param ->
+                    param.args[4] = false
+                }
+            }
+
         }
 
     }
