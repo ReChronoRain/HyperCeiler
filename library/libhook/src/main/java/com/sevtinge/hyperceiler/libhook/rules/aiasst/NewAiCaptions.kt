ecccb788e0abff6548b760ac8c9f942629159ad4
柒柒喵 <lingqiqi233@gmail.com>
2026-04-02 15:04:16 +0800

    fix: aiasst hooks supports versions 5.12.1.40 and later

--- library/libhook/src/main/java/com/sevtinge/hyperceiler/libhook/rules/aiasst/NewAiCaptions.kt
+++ library/libhook/src/main/java/com/sevtinge/hyperceiler/libhook/rules/aiasst/NewAiCaptions.kt
@@ -19,16 +19,24 @@
 package com.sevtinge.hyperceiler.libhook.rules.aiasst
 
 import com.sevtinge.hyperceiler.libhook.base.BaseHook
+import com.sevtinge.hyperceiler.libhook.utils.hookapi.tool.AppsTool.getPackageVersionCode
 import io.github.kyuubiran.ezxhelper.core.finder.MethodFinder.`-Static`.methodFinder
 import io.github.kyuubiran.ezxhelper.xposed.dsl.HookFactory.`-Static`.createHook
+import io.github.kyuubiran.ezxhelper.xposed.dsl.HookFactory.`-Static`.createHooks
 import java.lang.reflect.Method
+import java.lang.reflect.Modifier
 
 object NewAiCaptions : BaseHook() {
     override fun useDexKit() = true
 
     override fun initDexKit(): Boolean {
-        getMethod
-        getMethodNew
+        getMethodNewList
+        if (getMethodNewList.isEmpty()) {
+            getMethodNew
+            if (getMethodNew == null) {
+                getMethod
+            }
+        }
         return true
     }
     private val mSupportAiSubtitlesUtils by lazy {
@@ -39,7 +47,7 @@ object NewAiCaptions : BaseHook() {
         requiredMember("AiCaptionsModel") {
            it.findMethod {
                matcher {
-                   // SupportAiSubtitlesUtils
+                   // 5.8.0.20-
                    addEqString("SupportAiSubtitlesUtils")
                    addUsingField("Landroid/os/Build;->DEVICE:Ljava/lang/String;")
 
@@ -49,15 +57,15 @@ object NewAiCaptions : BaseHook() {
         }
     }
 
-    private val getMethodNew by lazy<Method> {
-        requiredMember("AiCaptionsModelNew") {
+    private val getMethodNew by lazy {
+        optionalMember("AiCaptionsModelNew") {
             it.findClass {
                 matcher {
                     addEqString("SupportAiSubtitlesUtils")
                 }
             }.findMethod {
                 matcher {
-                    // SupportAiSubtitlesUtils
+                    // 5.9.0.40+
                     addInvoke {
                         addUsingField("Landroid/os/Build;->DEVICE:Ljava/lang/String;")
                         paramCount = 0
@@ -66,19 +74,51 @@ object NewAiCaptions : BaseHook() {
                     addInvoke("Ljava/util/Set;->iterator()Ljava/util/Iterator;")
                 }
             }.single()
+        } as? Method
+    }
+
+    private val getMethodNewList by lazy<List<Method>> {
+        optionalMemberList("AiCaptionsModelNewList") {
+            it.findClass {
+                matcher {
+                    addEqString("SupportAiSubtitlesUtils")
+                }
+            }.findMethod {
+                matcher {
+                    // 5.12.1.40 (实际应在 5.11.0.40+ 就改了)
+                    // public static java.lang.Boolean m5.a2.m()
+                    // public static java.lang.Boolean m5.a2.r()
+                    addInvoke {
+                        addInvoke {
+                            addUsingField("Landroid/os/Build;->DEVICE:Ljava/lang/String;")
+                            paramCount = 0
+                        }
+                        modifiers = Modifier.PUBLIC
+                        paramCount = 1
+                    }
+                    modifiers = Modifier.PUBLIC
+                    returnType = "java.lang.Boolean"
+                }
+            }
         }
     }
 
-
+    val is1140AICaptions by lazy {
+        getPackageVersionCode(lpparam) >= 540110140
+    }
 
     override fun init() {
-        if (mSupportAiSubtitlesUtils == null) {
+        if (is1140AICaptions) {
+            getMethodNewList.createHooks {
+                returnConstant(true)
+            }
+        } else if (mSupportAiSubtitlesUtils == null) {
             runCatching {
                 getMethod.createHook {
                     returnConstant(true)
                 }
             }.onFailure {
-                getMethodNew.createHook {
+                getMethodNew?.createHook {
                     returnConstant(true)
                 }
             }
