c1f2a01f327565d4adba2be07648f41185015d1c
柒柒喵 <lingqiqi233@gmail.com>
2026-04-09 13:58:08 +0800

    只针对 HyperOS2 处理，HyperOS3 基线来到 Android 15 qpr2，无需处理

--- library/libhook/src/main/java/com/sevtinge/hyperceiler/libhook/rules/systemframework/corepatch/DowngradeCheckPatch.java
+++ library/libhook/src/main/java/com/sevtinge/hyperceiler/libhook/rules/systemframework/corepatch/DowngradeCheckPatch.java
@@ -1,58 +1,13 @@
 package com.sevtinge.hyperceiler.libhook.rules.systemframework.corepatch;
 
-import static com.sevtinge.hyperceiler.libhook.utils.hookapi.tool.EzxHelpUtils.findClassIfExists;
-import static com.sevtinge.hyperceiler.libhook.utils.hookapi.tool.EzxHelpUtils.findMethodExactIfExists;
-import static com.sevtinge.hyperceiler.libhook.utils.hookapi.tool.EzxHelpUtils.hookMethod;
-
 import com.sevtinge.hyperceiler.common.log.XposedLog;
 
 import io.github.libxposed.api.XposedModuleInterface;
-
-import android.os.Build;
 
 public class DowngradeCheckPatch extends CorePatchHelper {
     private final String TAG = "DowngradeCheckPatch";
 
     public void init(XposedModuleInterface.SystemServerStartingParam lpparam) {
-        // Android 11 / 12
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
-            try {
-                var pmService =
-                    findClassIfExists("com.android.server.pm.PackageManagerService", lpparam.getClassLoader());
-                if (pmService != null) {
-                    var checkDowngrade = findMethodExactIfExists(pmService, "checkDowngrade",
-                        "com.android.server.pm.parsing.pkg.AndroidPackage",
-                        "android.content.pm.PackageInfoLite");
-                    if (checkDowngrade != null) {
-                        // 允许降级
-                        hookMethod(checkDowngrade, new ReturnConstant("prefs_key_system_framework_core_patch_downgr", null));
-                    }
-                }
-            } catch (Throwable t) {
-                XposedLog.e(TAG, "system", "Android 11+ hook failed, crash: " + t);
-            }
-            return;
-        }
-
-        Class<?> PackageManagerServiceUtils =
-            findClassIfExists("com.android.server.pm.PackageManagerServiceUtils",
-                lpparam.getClassLoader());
-
-        // Android 15+
-        Throwable exceptionEarlyAndroid15 = null;
-        try {
-            var checkDowngradeAlt = findMethodExactIfExists(PackageManagerServiceUtils,
-                "checkDowngrade",
-                "com.android.server.pm.PackageSetting",
-                "android.content.pm.PackageInfoLite");
-            if (checkDowngradeAlt != null) {
-                hookMethod(checkDowngradeAlt, new ReturnConstant("prefs_key_system_framework_core_patch_downgr", null));
-                return;
-            }
-        } catch (Throwable t) {
-            exceptionEarlyAndroid15 = t;
-        }
-
         // Android 13+
         // ee11a9c (Rename AndroidPackageApi to AndroidPackage)
         try {
@@ -61,12 +16,8 @@ public class DowngradeCheckPatch extends CorePatchHelper {
                 "com.android.server.pm.pkg.AndroidPackage",
                 "android.content.pm.PackageInfoLite",
                 new ReturnConstant("prefs_key_system_framework_core_patch_downgr", null));
-        } catch (Throwable t) {
-            if (exceptionEarlyAndroid15 != null) {
-                XposedLog.e(TAG, "system", "Android 15+ hook failed, crash: " + t);
-            }
-            XposedLog.e(TAG, "system", "Android 13+ hook failed, crash: " + t);
+        } catch (Throwable e) {
+            XposedLog.e(TAG, "system", "Android 13+ hook failed, crash: " + e);
         }
-
     }
 }
