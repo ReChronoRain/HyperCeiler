5fbc412c9c446667a8a9ed8b15590def22d36eae
adlyq <45322943+Adlyq@users.noreply.github.com>
2026-05-21 17:57:54 +0800

    style: make codacy-production happy

--- library/libhook/src/main/java/com/sevtinge/hyperceiler/libhook/rules/phrase/UnlockImeClipboardFix.kt
+++ library/libhook/src/main/java/com/sevtinge/hyperceiler/libhook/rules/phrase/UnlockImeClipboardFix.kt
@@ -14,30 +14,27 @@ class UnlockImeClipboardFix : BaseHook() {
         for (field in inputProviderClz.declaredFields) {
             val modifiers = field.modifiers
 
-            if (
-                Modifier.isPublic(modifiers) &&
-                Modifier.isStatic(modifiers) &&
-                Modifier.isFinal(modifiers) &&
-                field.type == List::class.java
-            ) {
-                field.isAccessible = true
+            if (!Modifier.isPublic(modifiers)) continue
+            if (!Modifier.isStatic(modifiers)) continue
+            if (!Modifier.isFinal(modifiers)) continue
+            if (field.type != List::class.java) continue
+            field.isAccessible = true
 
-                val listObj = field.get(null) as? List<*> ?: continue
-                if (listObj.isEmpty() || listObj[0] !is String) continue
+            val listObj = field.get(null) as? List<*> ?: continue
+            if (listObj.isEmpty() || listObj[0] !is String) continue
 
-                @Suppress("UNCHECKED_CAST")
-                val whiteList = listObj as List<String>
+            @Suppress("UNCHECKED_CAST")
+            val whiteList = listObj as List<String>
 
-                if (whiteList.contains("com.xiaomi.type")) {
-                    val toAdd = target - whiteList.toSet()
+            if (whiteList.contains("com.xiaomi.type")) {
+                val toAdd = target - whiteList.toSet()
 
-                    if (toAdd.isNotEmpty()) {
-                        val newList = whiteList.toMutableList().apply { addAll(toAdd) }
-                        field.set(null, Collections.unmodifiableList(newList))
-                    }
-
-                    break
+                if (toAdd.isNotEmpty()) {
+                    val newList = whiteList.toMutableList().apply { addAll(toAdd) }
+                    field.set(null, Collections.unmodifiableList(newList))
                 }
+
+                break
             }
         }
     }
