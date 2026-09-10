72188fa388373da408a5725a6242beb93fb27b88
sekaiacg <sekaiacg@gmail.com>
2026-09-10 19:06:15 +0800

    feat(powerkeeper): add lock thermal max fps

---
+++ library/libhook/src/main/java/com/sevtinge/hyperceiler/libhook/rules/powerkeeper/LockThermalMaxFps.kt
@@ -0,0 +1,31 @@
+/*
+  * This file is part of HyperCeiler.
+
+  * HyperCeiler is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU Affero General Public License as
+  * published by the Free Software Foundation, either version 3 of the
+  * License.
+
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU Affero General Public License for more details.
+
+  * You should have received a copy of the GNU Affero General Public License
+  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+  * Copyright (C) 2023-2026 HyperCeiler Contributions
+*/
+package com.sevtinge.hyperceiler.libhook.rules.powerkeeper
+
+import com.sevtinge.hyperceiler.libhook.base.BaseHook
+import io.github.lingqiqi5211.ezhooktool.core.findAllMethods
+import io.github.lingqiqi5211.ezhooktool.xposed.dsl.createHook
+
+object LockThermalMaxFps : BaseHook() {
+    override fun init() {
+        findClass("com.miui.powerkeeper.feedbackcontrol.ThermalManager").findAllMethods { name("displayControl"); paramCount(1) }.single().createHook {
+                returnConstant(null)
+            }
+    }
+}
